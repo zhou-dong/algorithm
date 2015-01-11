df0c0c6ff191d8cfbeeeeaf49837d922cf98eb4a
zhou-dong <82224165@qq.com>
2015-01-10 22:08:19 -0600

    insertion sort

--- algorithm/src/org/dzhou/research/algorithm/InsertionSort.java
+++ algorithm/src/org/dzhou/research/algorithm/InsertionSort.java
@@ -2,6 +2,38 @@ package org.dzhou.research.algorithm;
 
 public class InsertionSort {
 
-	
-	
+	public static void sort(int[] input) {
+		for (int i = 1; i < input.length; i++)
+			swap(input, i, locate(input, i));
+	}
+
+	private static int locate(int[] input, int i) {
+		for (int result = i; result > 0; result--)
+			if (input[result - 1] <= input[i])
+				return result;
+		return 0;
+	}
+
+	private static void swap(int[] input, int from, int to) {
+		if (from == to)
+			return;
+		int temp = input[from];
+		for (int i = from; i >= to; i--)
+			if (i != to)
+				input[i] = input[i - 1];
+			else
+				input[to] = temp;
+	}
+
+	public static void main(String args[]) {
+		int[] input = { 6, 5, 3, 7, 8, 1, 2, 4 };
+		sort(input);
+		print(input);
+	}
+
+	private static void print(int[] input) {
+		for (int i : input)
+			System.out.println(i);
+	}
+
 }
