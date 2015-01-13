7f089a190bb999543ff77ac691252d358ff09b3a
zhou-dong <82224165@qq.com>
2015-01-13 02:10:08 -0600

    no finish data structure

---
+++ algorithm/src/org/dzhou/research/datastructure/AvlTree.java
@@ -0,0 +1,10 @@
+package org.dzhou.research.datastructure;
+
+public class AvlTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ algorithm/src/org/dzhou/research/datastructure/RedBlackTree.java
@@ -0,0 +1,10 @@
+package org.dzhou.research.datastructure;
+
+public class RedBlackTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
