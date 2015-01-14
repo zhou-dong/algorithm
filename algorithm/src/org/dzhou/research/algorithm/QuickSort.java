34f370dd880f2eea0e685212b364e7d2816354d7
zhou-dong <82224165@qq.com>
2015-01-14 15:04:42 -0600

    just back up

--- algorithm/src/org/dzhou/research/algorithm/QuickSort.java
+++ algorithm/src/org/dzhou/research/algorithm/QuickSort.java
@@ -2,27 +2,80 @@ package org.dzhou.research.algorithm;
 
 public class QuickSort {
 
+	private static int[] array = null;
+
 	private static void sort(int[] input) {
-		int pivot = input[0];
-		int i = 1;
-		int j = input.length - 1;
-		while (i != j) {
-			if (input[j] > pivot)
-				j--;
-			if (input[i] < pivot)
-				i++;
+		int pivot = array[0];
+		int pivotIndex = 0;
+		int frontIndex = 1;
+		int backIndex = array.length - 1;
+
+		if (backIndex == frontIndex) {
+
 		}
-		swap(input, i, j);
+
 	}
 
-	private static void swap(int[] input, int i, int j) {
-		int temp = input[i];
-		input[i] = input[j];
-		input[j] = temp;
+	private static void recursiveSort(int pivotIndex, int frontIndex,
+			int backIndex) {
+		if (frontIndex == backIndex)
+			return;
+		int pivotValue = array[pivotIndex];
+		while (true) {
+			int backValue = array[backIndex];
+			int frontValue = array[frontIndex];
+			if (backIndex == frontIndex) {
+				if (frontValue < pivotValue)
+					swap(pivotIndex, frontIndex);
+				break;
+			}
+			if (backValue >= pivotValue)
+				backIndex--;
+			else
+				break;
+		}
+		while (true) {
+			int frontValue = array[frontIndex];
+			int backValue = array[backIndex];
+			if (frontIndex == backIndex) {
+				if (backValue < pivotValue)
+					swap(pivotIndex, backIndex);
+				break;
+			}
+			if (frontValue <= pivotValue)
+				frontIndex++;
+			else
+				break;
+		}
+		if (backIndex != frontIndex)
+			swap(backIndex, frontIndex);
+	}
+
+	private static void leftRecursiveSort(int pivotIndex, int frontIndex,
+			int backIndex) {
+		int nextPivotIndex = frontIndex;
+		int nextFrontIndex = frontIndex + 1;
+		int nextBackIndex = pivotIndex - 1;
+		recursiveSort(nextPivotIndex, nextFrontIndex, nextBackIndex);
+	}
+
+	private static void rightRecursiveSort(int pivotIndex, int frontIndex,
+			int backIndex) {
+		int nextRightFrontIndex = pivotIndex + 1;
+		int nextRightBackIndex = backIndex;
+
+	}
+
+	private static void swap(int i, int j) {
+		if (i == j)
+			return;
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 
 	public static void main(String args[]) {
-		int[] input = { 3, 7, 8, 5, 2, 1, 9, 5, 4 };
+		int[] input = { 6, 8, 5, 3, 7, 9, 1, 4, 2 };
 		sort(input);
 		print(input);
 	}
