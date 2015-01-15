689501873f2666805ecfdc94f729ba189125d0d4
zhou-dong <82224165@qq.com>
2015-01-15 00:56:53 -0600

    quick sort

--- algorithm/src/org/dzhou/research/algorithm/QuickSort.java
+++ algorithm/src/org/dzhou/research/algorithm/QuickSort.java
@@ -2,90 +2,51 @@ package org.dzhou.research.algorithm;
 
 public class QuickSort {
 
-	private static int[] array = null;
+	private static int[] array;
 
-	private static void sort(int[] input) {
+	public static void sort(int[] input) {
 		array = input;
-		recursiveSort(0, 1, array.length - 1);
+		quicksort(0, array.length - 1);
 	}
 
-	private static void recursiveSort(int pivotIndex, int frontIndex,
-			int backIndex) {
-		if (frontIndex == backIndex)
-			return;
-		if (frontIndex < 0 || backIndex < 0)
-			return;
-		if (backIndex == array.length || frontIndex == array.length)
-			return;
-		int pivotValue = array[pivotIndex];
-		while (true) {
-			if (backIndex < 0)
-				return;
-			int backValue = array[backIndex];
-			int frontValue = array[frontIndex];
-			if (backIndex == frontIndex) {
-				if (frontValue < pivotValue)
-					swap(pivotIndex, frontIndex);
-				break;
+	private static void quicksort(int front, int tail) {
+		int i = front;
+		int j = tail;
+		int pivot = array[getPivot(front, tail)];
+		while (i <= j) {
+			while (array[i] < pivot)
+				i++;
+			while (array[j] > pivot)
+				j--;
+			if (i <= j) {
+				swap(i, j);
+				i++;
+				j--;
 			}
-			if (backValue >= pivotValue)
-				backIndex--;
-			else
-				break;
 		}
-		while (true) {
-			if (frontIndex == pivotIndex)
-				return;
-			int frontValue = array[frontIndex];
-			int backValue = array[backIndex];
-			if (frontIndex == backIndex) {
-				if (backValue < pivotValue)
-					swap(pivotIndex, backIndex);
-				break;
-			}
-			if (frontValue <= pivotValue)
-				frontIndex++;
-			else
-				break;
-		}
-		if (backIndex != frontIndex)
-			swap(backIndex, frontIndex);
-		leftRecursiveSort(pivotIndex, frontIndex, backIndex);
-		rightRecursiveSort(pivotIndex, frontIndex, backIndex);
+		if (front < j)
+			quicksort(front, j);
+		if (i < tail)
+			quicksort(i, tail);
 	}
 
-	private static void leftRecursiveSort(int pivotIndex, int frontIndex,
-			int backIndex) {
-		int nextPivotIndex = frontIndex;
-		int nextFrontIndex = frontIndex + 1;
-		int nextBackIndex = pivotIndex - 1;
-		recursiveSort(nextPivotIndex, nextFrontIndex, nextBackIndex);
-	}
-
-	private static void rightRecursiveSort(int pivotIndex, int frontIndex,
-			int backIndex) {
-		int nextPivotIndex = pivotIndex + 1;
-		int nextFrontIndex = pivotIndex + 2;
-		int nextBackIndex = backIndex;
-		recursiveSort(nextPivotIndex, nextFrontIndex, nextBackIndex);
+	private static int getPivot(int front, int tail) {
+		return (tail + front) / 2;
 	}
 
 	private static void swap(int i, int j) {
-		if (i == j)
-			return;
 		int temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
 	}
 
-	public static void main(String args[]) {
-		int[] input = { 6, 8, 5, 3, 7, 9, 1, 4, 2 };
-		sort(input);
+	public static void main(String[] args) {
+		sort(new int[] { 6, 2, 3, 1, 8, 7, 5, 4, 9, 2, 4 });
 		print();
 	}
 
 	private static void print() {
 		for (int i : array)
-			System.out.println(i + " ");
+			System.out.print(i + " ");
 	}
 }
