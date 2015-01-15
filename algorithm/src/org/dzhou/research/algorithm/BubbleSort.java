5fa05b1733d826605b12994212f409dc19ac0669
zhou-dong <82224165@qq.com>
2015-01-15 01:02:34 -0600

    update bubble sort

--- algorithm/src/org/dzhou/research/algorithm/BubbleSort.java
+++ algorithm/src/org/dzhou/research/algorithm/BubbleSort.java
@@ -2,32 +2,34 @@ package org.dzhou.research.algorithm;
 
 public class BubbleSort {
 
+	private static int[] array = null;
+
 	public static void sort(int[] input) {
-		for (int i = input.length - 1; i > 0; i--)
+		array = input;
+		for (int i = array.length - 1; i > 0; i--)
 			for (int j = 0; j < i; j++)
-				if (input[j] > input[j + 1])
-					swapNext(input, j);
+				if (array[j] > array[j + 1])
+					swapNext(j);
 	}
 
-	private static void swapNext(int input[], int i) {
-		swap(input, i, i + 1);
+	private static void swapNext(int i) {
+		swap(i, i + 1);
 	}
 
-	private static void swap(int[] input, int i, int j) {
-		int temp = input[i];
-		input[i] = input[j];
-		input[j] = temp;
+	private static void swap(int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 
 	public static void main(String[] args) {
-		int[] input = { 8, 7, 15, 9, 5, 4, 6, 3, 5, 1, 6, 2, 0 };
-		sort(input);
-		print(input);
+		sort(new int[] { 8, 7, 15, 9, 5, 4, 6, 3, 5, 1, 6, 2, 0 });
+		print();
 	}
 
-	private static void print(int[] input) {
-		for (int i : input)
-			System.out.println(i);
+	private static void print() {
+		for (int i : array)
+			System.out.print(i + " ");
 	}
 
 }
