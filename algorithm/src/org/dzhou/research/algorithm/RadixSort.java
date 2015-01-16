76919b5e816eda1b67f2b947f75d850f907bb672
zhou-dong <82224165@qq.com>
2015-01-16 14:04:10 -0600

    radix sort

--- algorithm/src/org/dzhou/research/algorithm/RadixSort.java
+++ algorithm/src/org/dzhou/research/algorithm/RadixSort.java
@@ -1,43 +1,45 @@
 package org.dzhou.research.algorithm;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class RadixSort {
 
 	@SuppressWarnings("unchecked")
-	private static List<Integer>[] buckets = new LinkedList[10];
+	private static List<Integer>[] buckets = new ArrayList[10];
 	private static int[] array = null;
-
-	public static void main(String[] args) {
-		sort(new int[] { 312, 427, 632, 210, 127, 236, 982, 531 });
-
-		System.out.println(getMaxNumber());
-
-		System.out.println(getSortTime());
-
-		System.out.println("312%10: " + (312 / 1) % 10);
-		System.out.println("312%100: " + (312 / 10) % 10);
-		System.out.println("312%100: " + (312 / 100) % 10);
-
-	}
 
 	public static int[] sort(int[] input) {
 		array = input;
 		int sortTime = getSortTime();
 		int index = 1;
 		for (int i = 0; i < sortTime; i++) {
-			index *= 10;
+			initBuckets();
 			for (int j : array)
-				buckets[j / index].add(j);
-
+				buckets[remainder(j, index)].add(j);
+			fromBucketsToArray();
+			index *= 10;
 		}
 		return array;
 	}
 
-	private static void insertIntoArrayFromBuckets() {
+	private static void initBuckets() {
+		for (int i = 0; i < buckets.length; i++)
+			buckets[i] = new ArrayList<Integer>();
+	}
 
+	private static int remainder(int number, int index) {
+		return (number / index) % 10;
+	}
+
+	private static void fromBucketsToArray() {
+		int index = 0;
+		for (List<Integer> bucket : buckets) {
+			if (bucket.size() == 0)
+				continue;
+			for (Integer i : bucket)
+				array[index++] = i;
+		}
 	}
 
 	private static int getSortTime() {
@@ -46,7 +48,7 @@ public class RadixSort {
 		for (int i = 0; i < Integer.MAX_VALUE; i++) {
 			index *= 10;
 			if (maxNumber / index == 0)
-				return i;
+				return i + 1;
 		}
 		return 0;
 	}
@@ -60,8 +62,15 @@ public class RadixSort {
 
 	}
 
+	public static void main(String[] args) {
+		int[] result = sort(new int[] { 312, 427, 632, 210, 127, 236, 982, 531,
+				9, 21 });
+		print(result);
+	}
+
 	private static void print(int[] input) {
 		for (int i : input)
-			System.out.println(i + " ");
+			System.out.print(i + " ");
 	}
+
 }
