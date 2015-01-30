aa54ec801e9f30981ef81943af37a8ef2c4b4438
zhou-dong <82224165@qq.com>
2015-01-30 09:57:44 -0600

    add decision tree and priority queue

---
+++ algorithm/src/org/dzhou/research/algorithm/struct/PriorityQueue.java
@@ -0,0 +1,10 @@
+package org.dzhou.research.algorithm.struct;
+
+public class PriorityQueue {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world!");
+	}
+
+}
---
+++ algorithm/src/org/dzhou/research/algorithm/tree/DecisionTree.java
@@ -0,0 +1,11 @@
+package org.dzhou.research.algorithm.tree;
+
+public class DecisionTree {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world~");
+
+	}
+
+}
