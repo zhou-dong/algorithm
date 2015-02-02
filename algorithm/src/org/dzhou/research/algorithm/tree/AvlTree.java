85bf3970bec92d1bef083849bfaf3cb87d373d6c
zhou-dong <82224165@qq.com>
2015-02-02 10:03:40 -0600

    prepared of avl tree

--- algorithm/src/org/dzhou/research/algorithm/tree/AvlTree.java
+++ algorithm/src/org/dzhou/research/algorithm/tree/AvlTree.java
@@ -2,7 +2,22 @@ package org.dzhou.research.algorithm.tree;
 
 public class AvlTree {
 
-	private static void insert() {
+	void leftRotate() {
+
+	}
+
+	void rightRotate() {
+
+	}
+
+	void insert() {
+
+	}
+
+	void swap(int i, int j) {
+		int temp = i;
+		i = j;
+		j = temp;
 	}
 
 	public static void main(String[] args) {
--- algorithm/src/org/dzhou/research/algorithm/tree/RedBlackTree.java
+++ algorithm/src/org/dzhou/research/algorithm/tree/RedBlackTree.java
@@ -1,6 +1,21 @@
 package org.dzhou.research.algorithm.tree;
 
+/* Properties:
+ * 1. Every node is either RED or BLACK.
+ * 2. Every nil node is BLACK.
+ * 3. Every RED node has two BLACK child nodes.(Red node can not have red child node).
+ * 4. Every path from node x down to the leaf node has the same number of BLACK nodes.
+ * 5. The root node is always BLACK.
+ */
 public class RedBlackTree {
+
+	void leftRotate() {
+
+	}
+
+	void rightRotate() {
+
+	}
 
 	private static void insert() {
 	}
