c99ba8b9592a743315c81de79eb246430d4c9fc8
zhou-dong <82224165@qq.com>
2015-02-03 21:48:28 -0600

    finish avl tree

--- algorithm/src/org/dzhou/research/algorithm/tree/AvlTree.java
+++ algorithm/src/org/dzhou/research/algorithm/tree/AvlTree.java
@@ -1,11 +1,17 @@
 package org.dzhou.research.algorithm.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Implementation of AVL tree
  * 
  * Base on "Justin Ethier" the implement of AvlTree and code on:
  * https://github.com/justinethier/AVL-Tree/blob/master/AvlTree.java
  * 
+ * This class only have insert method.
+ * 
+ * Checked result by preTraversal, inorderTraversal and postTraversal
  */
 public class AvlTree<T extends Comparable<? super T>> {
 
@@ -26,6 +32,17 @@ public class AvlTree<T extends Comparable<? super T>> {
 			this.rightChild = rightChild;
 		}
 
+	}
+
+	public AvlNode<T> root;
+
+	public boolean insert(T element) {
+		try {
+			root = insert(element, root);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	public AvlNode<T> insert(T element, AvlNode<T> node) throws Exception {
@@ -116,10 +133,53 @@ public class AvlTree<T extends Comparable<? super T>> {
 		return node == null ? -1 : node.height;
 	}
 
+	// <root> <left> <right>
+	public void preTraversal(AvlNode<T> node, List<T> list) {
+		if (node == null || node.element == null)
+			return;
+		list.add(node.element);
+		preTraversal(node.leftChild, list);
+		preTraversal(node.rightChild, list);
+	}
+
+	// <left> <root> <right>
+	public void inorderTraversal(AvlNode<T> node, List<T> list) {
+		if (node == null || node.element == null)
+			return;
+		inorderTraversal(node.leftChild, list);
+		list.add(node.element);
+		inorderTraversal(node.rightChild, list);
+	}
+
+	// <left> <right> <root>
+	public void postTraversal(AvlNode<T> node, List<T> list) {
+		if (node == null || node.element == null)
+			return;
+		postTraversal(node.leftChild, list);
+		postTraversal(node.rightChild, list);
+		list.add(node.element);
+	}
+
 	public static void main(String[] args) {
+		AvlTree<Integer> tree = new AvlTree<>();
 		int[] test = { 6, 8, 7, 4, 5, 2, 1, 9, 10 };
 		for (int i : test)
+			tree.insert(i);
+		List<Integer> list = new ArrayList<Integer>();
+		tree.preTraversal(tree.root, list);
+		printList(list);
+		list = new ArrayList<Integer>();
+		tree.inorderTraversal(tree.root, list);
+		printList(list);
+		list = new ArrayList<Integer>();
+		tree.postTraversal(tree.root, list);
+		printList(list);
+	}
+
+	public static void printList(List<Integer> list) {
+		for (int i : list)
 			System.out.print(i + " ");
+		System.out.println(" ");
 	}
 
 }
