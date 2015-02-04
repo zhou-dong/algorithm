02b4edd52d4e0d46913e719ccab1205c70b22a10
zhou-dong <82224165@qq.com>
2015-02-04 11:24:31 -0600

    add b tree

---
+++ algorithm/src/org/dzhou/research/algorithm/tree/BTree.java
@@ -0,0 +1,13 @@
+package org.dzhou.research.algorithm.tree;
+
+public class BTree {
+
+	private void todo() {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
