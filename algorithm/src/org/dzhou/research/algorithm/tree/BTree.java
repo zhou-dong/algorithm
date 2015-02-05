ea5184562b8aaed2501477698e42dc503f96c29d
zhou-dong <82224165@qq.com>
2015-02-05 15:31:09 -0600

    back b tree

--- algorithm/src/org/dzhou/research/algorithm/tree/BTree.java
+++ algorithm/src/org/dzhou/research/algorithm/tree/BTree.java
@@ -1,6 +1,18 @@
 package org.dzhou.research.algorithm.tree;
 
-public class BTree {
+public class BTree<T extends Comparable<? super T>> {
+
+	private static final int NODE_SIZE = 2;
+
+	protected static class Node<T> {
+
+		protected int[] keys = new int[2 * NODE_SIZE - 1];
+	}
+
+	public Node<T> root;
+
+	protected void spiltChildNode() {
+	}
 
 	private void todo() {
 	}
