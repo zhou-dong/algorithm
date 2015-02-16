da4c65eb061d4fb1767a15e54607b696ca6ebf1b
zhou-dong <82224165@qq.com>
2015-02-15 23:10:34 -0600

    not finished

--- algorithm/src/org/dzhou/research/algorithm/struct/Stack.java
+++ algorithm/src/org/dzhou/research/algorithm/struct/Stack.java
@@ -1,13 +1,50 @@
 package org.dzhou.research.algorithm.struct;
 
+/**
+ * not finished
+ */
 public class Stack {
 
-	private void todo() {
+	private int[] array;
+	private int size;
+	private int topOfStack;
+
+	public Stack(int size) {
+		this.size = size;
+		array = new int[size];
+	}
+
+	public void push(int input) {
+		if (topOfStack + 1 < size)
+			array[topOfStack++] = input;
+		else
+			System.out.println("stack is full");
+	}
+
+	public int pop() {
+		if (topOfStack >= 0)
+			return array[topOfStack--];
+		else
+			return -1;
+	}
+
+	public int peek() {
+		return array[topOfStack];
+	}
+
+	public void display() {
+		for (int i : array)
+			System.out.print(i + " ");
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Stack stack = new Stack(20);
+		int[] test = { 7, 5, 9, 3, 4, 1, 2, 6, 3 };
+		for (int i : test)
+			stack.push(i);
+		stack.display();
+		for (int i = 0; i < test.length; i++)
+			System.out.println(stack.pop());
 	}
 
 }
