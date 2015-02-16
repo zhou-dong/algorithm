e2945ba631bf2c07b26ba57458a5c89043fad16d
zhou-dong <82224165@qq.com>
2015-02-16 00:46:02 -0600

    not finished

--- algorithm/src/org/dzhou/research/algorithm/struct/Queue.java
+++ algorithm/src/org/dzhou/research/algorithm/struct/Queue.java
@@ -1,12 +1,38 @@
 package org.dzhou.research.algorithm.struct;
 
+/**
+ * not finished, could by improved by:
+ * 
+ * 1. recursive the array.
+ * 
+ * 2. implement by linked list
+ */
 public class Queue {
 
-	private void todo() {
+	private int[] array;
+	private int front = -1;
+	private int rare = -1;
+	private int size = 0;
+
+	Queue(int size) {
+		this.size = size;
+		array = new int[size];
 	}
 
-	public static void main(String args[]) {
+	public void add(int input) {
+		if (rare + 1 == size)
+			System.out.println("it is full");
+		else
+			array[rare++] = input;
+	}
 
+	public int pop() {
+		if (front > -1)
+			return array[front--];
+		else {
+			System.out.println("it is empty");
+			return -1;
+		}
 	}
 
 }
