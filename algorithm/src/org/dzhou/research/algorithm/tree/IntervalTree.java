1b2ca5a6a7d568e158fcdc16a0ebbd66ccbac8d1
zhou-dong <82224165@qq.com>
2015-02-19 22:37:18 -0600

    no finish

--- algorithm/src/org/dzhou/research/algorithm/tree/IntervalTree.java
+++ algorithm/src/org/dzhou/research/algorithm/tree/IntervalTree.java
@@ -2,6 +2,8 @@ package org.dzhou.research.algorithm.tree;
 
 /**
  * 
+ * Not finish
+ * 
  * 比较两个区间中的值
  * 
  * Also have the name of Interval Tree
@@ -21,22 +23,86 @@ package org.dzhou.research.algorithm.tree;
  */
 public class IntervalTree<Key extends Comparable<Key>, Value> {
 
+	private Node root;
+
+	public class Node {
+
+		Key low;
+		Key high;
+		Key max;
+		Value value;
+		Node left;
+		Node right;
+
+		public Node(Key low, Key high, Key max, Value value, Node left,
+				Node right) {
+			this.low = low;
+			this.high = high;
+			this.max = max;
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+
+	}
+
 	public void put(Key low, Key high, Value val) {
+		Node node = root;
+		while (node != null) {
+			node.max = (high.compareTo(node.max) > 0) ? high : node.max;
+			if (low.compareTo(node.low) < 0) {// left
+				if (node.left == null) {
+					node.left = new Node(low, high, high, val, null, null);
+					return;
+				}
+				node = node.left;
+			} else {// right
+				if (node.right == null) {
+					node.right = new Node(low, high, high, val, null, null);
+					return;
+				}
+				node = node.right;
+			}
+		}
+		root = new Node(low, high, high, val, null, null);
+	}
+
+	public boolean isOverlap(Key low, Key high) {
+		Node node = root;
+		while (node != null) {
+			if (iSIntersected(low, high, node))
+				return true;
+			if (goLeft(low, high, node))
+				node = node.left;
+			else
+				node = node.right;
+		}
+		return false;
+	}
+
+	private boolean iSIntersected(Key low, Key high, Node node) {
+		return low.compareTo(node.high) < 0 && high.compareTo(low) > 0;
+	}
+
+	private boolean goLeft(Key low, Key high, Node node) {
+		return node != null && node.max.compareTo(low) > 0;
 	}
 
 	public void get(Key low, Key gigh) {
+		// TODO
 	}
 
 	public void delete(Key low, Key high) {
+		// TODO
 	}
 
 	public Iterable<Value> intersects(Key low, Key high) {
+		// TODO
 		return null;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO
 	}
 
 }
