8517c99c3a74a1c5e452b8c1adc53c7ed5946144
zhou-dong <82224165@qq.com>
2015-02-25 15:27:59 -0600

    dynamic programming

--- algorithm/src/org/dzhou/research/algorithm/graph/path/shortest/FloydWarshall.java
+++ algorithm/src/org/dzhou/research/algorithm/graph/path/shortest/FloydWarshall.java
@@ -1,10 +1,52 @@
 package org.dzhou.research.algorithm.graph.path.shortest;
 
+/**
+ * Dynamic Programming
+ * 
+ * @author DONG ZHOU
+ */
 public class FloydWarshall {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int max = Integer.MAX_VALUE;
+	private int[] row0 = { 0, 2, 6, 4 };
+	private int[] row1 = { max, 0, 3, max };
+	private int[] row2 = { 7, max, 0, 1 };
+	private int[] row3 = { 5, max, 12, 0 };
+	private int[][] map = { row0, row1, row2, row3 };
 
+	public void execute() {
+		for (int k = 0; k < map.length; k++)
+			for (int i = 0; i < map.length; i++)
+				for (int j = 0; j < map.length; j++)
+					updateDistance(k, i, j);
+	}
+
+	private void updateDistance(int k, int i, int j) {
+		int transferDistance = transferDinstance(i, j, k);
+		if (map[i][j] > transferDistance)
+			map[i][j] = transferDistance;
+	}
+
+	private int transferDinstance(int i, int j, int transferPoint) {
+		if (map[i][transferPoint] == max)
+			return max;
+		if (map[transferPoint][j] == max)
+			return max;
+		return map[i][transferPoint] + map[transferPoint][j];
+	}
+
+	public void display() {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++)
+				System.out.print(map[i][j] + " ");
+			System.out.println("");
+		}
+	}
+
+	public static void main(String[] args) {
+		FloydWarshall instance = new FloydWarshall();
+		instance.execute();
+		instance.display();
 	}
 
 }
