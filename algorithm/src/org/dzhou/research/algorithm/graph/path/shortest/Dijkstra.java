99c00336601537c4374dcdb51b1b0f0fb9b96688
zhou-dong <82224165@qq.com>
2015-02-26 14:31:08 -0600

    Dijkstra algorithem

--- algorithm/src/org/dzhou/research/algorithm/graph/path/shortest/Dijkstra.java
+++ algorithm/src/org/dzhou/research/algorithm/graph/path/shortest/Dijkstra.java
@@ -17,28 +17,27 @@ public class Dijkstra {
 	private int[] row5 = { max, max, max, max, max, 0 };
 
 	private int[][] map = { row0, row1, row2, row3, row4, row5 };
-	private int[] distance = row0.clone();
+	private int[] dis = row0.clone();
 	private int[] book = new int[6];
 
-	private int shortest = 0;
-
 	public void execute() {
+		int nearest = 0;
 		for (int i = 0; i < map.length; i++) {
 			int min = max;
 			for (int j = 0; j < map.length; j++) {
 				if (isBooked(j))
 					continue;
-				if (distance[j] >= min)
+				if (dis[j] >= min)
 					continue;
-				min = distance[j];
-				shortest = j;
+				min = dis[j];
+				nearest = j;
 			}
-			book(shortest);
+			book(nearest);
 			for (int j = 0; j < map.length; j++) {
-				if (map[shortest][j] == max)
+				if (map[nearest][j] == max)
 					continue;
-				if (distance[j] > distance[shortest] + map[shortest][j])
-					distance[j] = distance[shortest] + map[shortest][j];
+				if (dis[j] > dis[nearest] + map[nearest][j])
+					dis[j] = dis[nearest] + map[nearest][j];
 			}
 		}
 	}
@@ -52,7 +51,7 @@ public class Dijkstra {
 	}
 
 	public void display() {
-		for (int i : distance)
+		for (int i : dis)
 			System.out.print(i + " ");
 	}
 
