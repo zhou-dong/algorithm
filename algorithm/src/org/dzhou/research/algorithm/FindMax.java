982f8bd3a3d7c0d284f8872bff3b6476d12887b1
zhou-dong <82224165@qq.com>
2015-02-28 20:06:23 -0600

    update name

--- algorithm/src/org/dzhou/research/algorithm/FindMax.java
+++ algorithm/src/org/dzhou/research/algorithm/FindMax.java
@@ -3,20 +3,20 @@ package org.dzhou.research.algorithm;
 /**
  * @author DONG ZHOU
  */
-public class FindSmallestNumber {
+public class FindMax {
 
-	private int[] array = { -1, 1, 3, 5, 6, 9, 10, 45, 28, 6 };
+	private int[] array = { 1, 3, 5, 6, 9, 10, 45, 28, 6 };
 
 	public int execute() {
 		int result = array[0];
 		for (int i : array)
-			if (i < result)
+			if (i > result)
 				result = i;
 		return result;
 	}
 
 	public static void main(String[] args) {
-		FindSmallestNumber instance = new FindSmallestNumber();
+		FindMax instance = new FindMax();
 		System.out.println(instance.execute());
 	}
 
