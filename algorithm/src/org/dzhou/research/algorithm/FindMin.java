c7cf3da3cb830fc3b33fa1ca8b8d5e5eeb5939cc
zhou-dong <82224165@qq.com>
2015-02-28 22:00:17 -0600

    optimize find minimum

--- algorithm/src/org/dzhou/research/algorithm/FindMin.java
+++ algorithm/src/org/dzhou/research/algorithm/FindMin.java
@@ -9,7 +9,7 @@ public class FindMin {
 
 	public int execute() {
 		int result = array[0];
-		for (int i = 0; i < array.length; i++)
+		for (int i = 1; i < array.length; i++)
 			if (array[i] < result)
 				result = array[i];
 		return result;
