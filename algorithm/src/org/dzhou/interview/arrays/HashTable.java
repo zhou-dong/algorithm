95808b0397448820e81a95d31b9384d8a27e8c28
zhou-dong <82224165@qq.com>
2015-12-23 16:13:55 -0600

    update

--- algorithm/src/org/dzhou/interview/arrays/HashTable.java
+++ algorithm/src/org/dzhou/interview/arrays/HashTable.java
@@ -6,12 +6,37 @@ package org.dzhou.interview.arrays;
  * @author DONG ZHOU
  *
  */
-public class HashTable {
+public class HashTable<K, V> {
+
+	static final int CAPACITY = 1 << 4;
+
+	public static class Entity<K, V> {
+		int hash;
+		K key = null;
+		V value = null;
+
+		public Entity(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
+
+	Entity<K, V>[] table = null;
+
+	@SuppressWarnings("unchecked")
+	public HashTable() {
+		table = (Entity<K, V>[]) new Entity[CAPACITY];
+	}
+
+	static final int hash(Object key) {
+		return (key == null) ? 0 : (key.hashCode()) % CAPACITY;
+	}
+
+	public void put(K key, V value) {
+
+	}
 
 	public static void main(String[] args) {
-
-		
-		
 	}
 
 }
