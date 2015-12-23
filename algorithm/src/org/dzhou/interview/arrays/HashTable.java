9ed115f7040a0f60c9b12cc10d4ff11aa234969a
zhou-dong <82224165@qq.com>
2015-12-23 15:29:52 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/arrays/HashTable.java
@@ -0,0 +1,17 @@
+package org.dzhou.interview.arrays;
+
+/**
+ * Use an array of linked lists and hash code function to insert a key or value
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class HashTable {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
