33a85b902c58e5d68416c8062f3a54e16709d4e3
zhou-dong <82224165@qq.com>
2015-12-25 14:51:50 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/arrays/ChechOneAwayDistance.java
@@ -0,0 +1,63 @@
+package org.dzhou.interview.arrays;
+
+/**
+ * There are three types of edits that can be performed on strings: insert a
+ * character, remove a character, or replace a character. Given two strings,
+ * write a function to check if they are one edit(or zero edits) away.
+ * 
+ * @author DONG ZHOU
+ *
+ *         Replacement:
+ *
+ *         Insertion:
+ *
+ *         Removal:
+ *
+ */
+public class ChechOneAwayDistance {
+
+	public static boolean isOneAway(String first, String second) {
+		if (first.length() == second.length())
+			return isOneEditReplace(first, second);
+		else if (first.length() + 1 == second.length())
+			return isOneEditInsert(first, second);
+		else if (first.length() - 1 == second.length())
+			return isOneEditInsert(second, first);
+		return false;
+	}
+
+	private static boolean isOneEditReplace(String first, String second) {
+		boolean foundDifference = false;
+		for (int i = 0; i < first.length(); i++) {
+			if (first.charAt(i) != second.charAt(i)) {
+				if (foundDifference)
+					return false;
+				foundDifference = true;
+			}
+		}
+		return true;
+	}
+
+	private static boolean isOneEditInsert(String first, String second) {
+		int firstIndex = 0;
+		int secondIndex = 0;
+		while (firstIndex < first.length() && secondIndex < second.length()) {
+			if (first.charAt(firstIndex) != second.charAt(secondIndex)) {
+				if (firstIndex != secondIndex)
+					return false;
+				secondIndex++;
+			} else {
+				firstIndex++;
+				secondIndex++;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isOneAway("pale", "ple"));
+		System.out.println(isOneAway("pales", "pale"));
+		System.out.println(isOneAway("pale", "bale"));
+		System.out.println(isOneAway("pale", "ble"));
+	}
+}
