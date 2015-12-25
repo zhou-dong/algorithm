d543a4e0e1941e250c8513201706c41895eb5afc
zhou-dong <82224165@qq.com>
2015-12-25 15:38:19 -0600

    not finish

---
+++ algorithm/src/org/dzhou/interview/arrays/StringCompression.java
@@ -0,0 +1,15 @@
+package org.dzhou.interview.arrays;
+
+/**
+ * Implement a method to perform basic string compression using the counts or
+ * repeated characters. For example, the string aabbcccccaaa would become
+ * a2b2c5a3. If the "compressed" string would not become smaller than the
+ * original string, your method should return the original string. You can
+ * assume the string has only upper case and lower case letters (a-z).
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class StringCompression {
+
+}
