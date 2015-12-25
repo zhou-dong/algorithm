8bb20521e5b43c3c370def9a889c89a133944a92
zhou-dong <82224165@qq.com>
2015-12-25 13:04:18 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/arrays/PalindromePermutation.java
@@ -0,0 +1,21 @@
+package org.dzhou.interview.arrays;
+
+/**
+ * Given a string, write a function to check if it is a permutation of a
+ * palindrome. A palindrome is a word or phase that is the some forwards and
+ * backwards. A palindrome is a rearrangement of letters. The palindrome does
+ * not need to be limited to just dictionary words.
+ * 
+ * @author DONG ZHOU
+ *
+ *         If there is no odd or one odd element that will be palindrome of
+ *         permutation.
+ */
+public class PalindromePermutation {
+
+	public boolean isPermutationOfPalindrome(String phrase) {
+
+		return false;
+	}
+
+}
