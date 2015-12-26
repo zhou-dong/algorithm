9900588cb3cfe8ecd7d25490447693487be51e61
zhou-dong <82224165@qq.com>
2015-12-25 18:32:50 -0600

    check lenth first

--- algorithm/src/org/dzhou/interview/arrays/StringCompression.java
+++ algorithm/src/org/dzhou/interview/arrays/StringCompression.java
@@ -12,4 +12,60 @@ package org.dzhou.interview.arrays;
  */
 public class StringCompression {
 
+	public static String compress(String input) {
+		StringBuffer compressed = new StringBuffer();
+		int countConsecutive = 0;
+		for (int i = 0; i < input.length(); i++) {
+			countConsecutive++;
+			if ((i + 1) >= input.length() || input.charAt(i) != input.charAt(i + 1)) {
+				compressed.append(input.charAt(i));
+				compressed.append(countConsecutive);
+				countConsecutive = 0;
+			}
+		}
+		return compressed.length() < input.length() ? compressed.toString() : input;
+	}
+
+	// optimize by int[] with could be save space
+	public static String compress2(String input) {
+		int compressedLength = countCompression(input);
+		if (compressedLength >= input.length())
+			return input;
+		StringBuffer compressed = new StringBuffer();
+		int countConsecutive = 0;
+		for (int i = 0; i < input.length(); i++) {
+			countConsecutive++;
+			if ((i + 1) >= input.length() || input.charAt(i) != input.charAt(i + 1)) {
+				compressed.append(input.charAt(i));
+				compressed.append(countConsecutive);
+				countConsecutive = 0;
+			}
+		}
+		return compressed.toString();
+	}
+
+	private static int countCompression(String input) {
+		int compressedLength = 0;
+		int countConsecutive = 0;
+		for (int i = 0; i < input.length(); i++) {
+			countConsecutive++;
+			if ((i + 1) >= input.length() || input.charAt(i) != input.charAt(i + 1)) {
+				compressedLength += 1 + String.valueOf(countConsecutive).length();
+				countConsecutive = 0;
+			}
+		}
+		return compressedLength;
+	}
+
+	public static void main(String[] args) {
+		String test = "aabbccccccccccccccccccccccccccaaa";
+		String testResult = "a2b2c5a3";
+		String result = StringCompression.compress(test);
+		System.out.println(testResult.equals(result));
+		System.out.println(result);
+
+		String result2 = StringCompression.compress2(test);
+		System.out.println(testResult.equals(result2));
+		System.out.println(result2);
+	}
 }
