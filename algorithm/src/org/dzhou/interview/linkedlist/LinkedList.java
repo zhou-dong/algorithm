1acf438df75a9dd23385dde2b18300b5c94b3496
zhou-dong <82224165@qq.com>
2015-12-28 17:26:01 -0600

    update

--- algorithm/src/org/dzhou/interview/linkedlist/LinkedList.java
+++ algorithm/src/org/dzhou/interview/linkedlist/LinkedList.java
@@ -37,27 +37,16 @@ public class LinkedList {
 	}
 
 	public static void main(String[] args) {
-
+		// initial head
 		Node head = new Node("head");
-
-<<<<<<< HEAD
+		// append elements to tail
 		for (int i = 0; i < 100; i++)
 			head.appendToTail(i * 100);
-
+		// print all the elements
 		while (head != null) {
 			System.out.println(head.data);
-=======
-		for (int i = 0; i < 100; i++) {
-			head.appendToTail(i * 100);
-		}
-
-		while (head != null) {
-			Object data = head.data;
-			System.out.println(data);
->>>>>>> 9e79c8edf6629c48d492b1ccf5fd5346684a4a8f
 			head = head.next;
 		}
-
 	}
 
 }
