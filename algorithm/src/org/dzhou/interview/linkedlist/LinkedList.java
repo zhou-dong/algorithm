38ee12b4aa304e647ad5f49ae11a5456d8a98538
zhou-dong <82224165@qq.com>
2015-12-28 23:55:49 -0600

    update

--- algorithm/src/org/dzhou/interview/linkedlist/LinkedList.java
+++ algorithm/src/org/dzhou/interview/linkedlist/LinkedList.java
@@ -2,60 +2,7 @@ package org.dzhou.interview.linkedlist;
 
 import java.util.HashSet;
 
-/**
- * 
- * Simple implement of Linked List
- * 
- * @author DONG ZHOU
- *
- */
 public class LinkedList {
-
-	/**
-	 * When use this kind of method to implement Linked List, should careful of
-	 * if multiple objects need a reference to the linked list, if the head of
-	 * linked list has changed.
-	 * 
-	 * @author DONG ZHOU
-	 *
-	 */
-
-	static class Node {
-
-		int data;
-		Node next = null;
-
-		public Node(int data) {
-			this.data = data;
-		}
-
-		void appendToTail(int data) {
-			Node tmp = this;
-			while (tmp.next != null)
-				tmp = tmp.next;
-			tmp.next = new Node(data);
-		}
-
-	}
-
-	public static Node deleteNode(Node head, int data) {
-
-		if (head.data == data)
-			return head.next;
-
-		Node tmp = head;
-
-		while (tmp.next != null) {
-			if (tmp.next.data == data) {
-				tmp.next = tmp.next.next;
-				return head;
-			}
-			tmp = tmp.next;
-		}
-
-		return head;
-
-	}
 
 	public static void deleteDups(Node head) {
 		HashSet<Object> set = new HashSet<>();
@@ -93,8 +40,9 @@ public class LinkedList {
 		for (int i = 0; i < 20; i++)
 			head.appendToTail(i);
 		print(head);
-		head = deleteNode(head, 99);
-		head = deleteNode(head, -1);
+		head = Node.deleteNode(head, 19);
+		head = Node.deleteNode(head, 18);
+		head = Node.deleteNode(head, -1);
 		print(head);
 		// append duplicate data
 		for (int i = 0; i < 10; i++)
---
+++ algorithm/src/org/dzhou/interview/linkedlist/Node.java
@@ -0,0 +1,44 @@
+package org.dzhou.interview.linkedlist;
+
+/**
+ * 
+ * Simple implement of LinkedList.
+ * 
+ * When use this kind of method to implement Linked List, should careful of if
+ * multiple objects need a reference to the linked list, if the head of linked
+ * list has changed.
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class Node {
+
+	int data;
+	Node next = null;
+
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public void appendToTail(int data) {
+		Node node = this;
+		while (node.next != null)
+			node = node.next;
+		node.next = new Node(data);
+	}
+
+	public static Node deleteNode(Node head, int data) {
+		if (head.data == data)
+			return head.next;
+		Node runner = head;
+		while (runner.next != null) {
+			if (runner.next.data == data) {
+				runner.next = runner.next.next;
+				return head;
+			}
+			runner = runner.next;
+		}
+		return head;
+	}
+
+}
