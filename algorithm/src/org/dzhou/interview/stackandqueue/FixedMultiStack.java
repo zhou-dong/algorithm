261ecd0951e6394b31a82abb4b0f131c338b1b37
zhou-dong <82224165@qq.com>
2016-01-03 15:08:32 -0600

    update

--- algorithm/src/org/dzhou/interview/stackandqueue/FixedMultiStack.java
+++ algorithm/src/org/dzhou/interview/stackandqueue/FixedMultiStack.java
@@ -1,5 +1,7 @@
 package org.dzhou.interview.stackandqueue;
 
+import java.util.EmptyStackException;
+
 /**
  * Practice of "cracking the code interview"
  * 
@@ -10,4 +12,55 @@ package org.dzhou.interview.stackandqueue;
  */
 public class FixedMultiStack {
 
+	public class FullStackException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
+
+	private int numberOfStacks = 3;
+	private int stackCapacity;
+	private int[] values;
+	private int[] sizes;
+
+	public FixedMultiStack(int stackSize) {
+		this.stackCapacity = stackSize;
+		sizes = new int[numberOfStacks];
+		values = new int[stackSize * numberOfStacks];
+	}
+
+	public void push(int stackNum, int value) {
+		if (isFull(stackNum))
+			throw new FullStackException();
+		sizes[stackNum]++;
+		values[indexOfTop(stackNum)] = value;
+	}
+
+	public int pop(int stackNum) {
+		if (isEmpty(stackNum))
+			throw new EmptyStackException();
+		sizes[stackNum]--;
+		int topIndex = indexOfTop(stackNum);
+		int value = values[topIndex];
+		values[topIndex] = 0;
+		return value;
+	}
+
+	public int peek(int stackNum) {
+		if (isEmpty(stackNum))
+			throw new EmptyStackException();
+		return values[indexOfTop(stackNum)];
+	}
+
+	private boolean isEmpty(int stackNum) {
+		return sizes[stackNum] == 0;
+	}
+
+	private boolean isFull(int stackNum) {
+		return sizes[stackNum] == stackCapacity;
+	}
+
+	private int indexOfTop(int stackNum) {
+		int offset = stackNum * stackCapacity;
+		int size = sizes[stackNum];
+		return offset + size - 1;
+	}
 }
