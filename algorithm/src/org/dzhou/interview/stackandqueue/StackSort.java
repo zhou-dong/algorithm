f82eeaad69e56c582755270910c29dbd714dc9c3
zhou-dong <82224165@qq.com>
2016-01-03 20:13:14 -0600

    updaste

---
+++ algorithm/src/org/dzhou/interview/stackandqueue/StackSort.java
@@ -0,0 +1,31 @@
+package org.dzhou.interview.stackandqueue;
+
+import java.util.Comparator;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * Write a program to sort a stack such that the smallest items are on the top.
+ * You can use an additional temporary stack, but you may not copy the elements
+ * into any other data structure (such as an array). The stack support the
+ * following operations: push, pop, peek and isEmpty.
+ * 
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class StackSort {
+
+	public static Stack<Integer> sort(Stack<Integer> stack) {
+		Stack<Integer> result = new Stack<>();
+		while (!stack.isEmpty()) {
+			Integer tmp = stack.pop();
+			while (!result.isEmpty() && result.peek() > tmp) {
+				stack.push(result.pop());
+			}
+			result.push(tmp);
+		}
+		return result;
+	}
+
+}
