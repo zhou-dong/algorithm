8154cd0fe988b6b7d558a5acc7db813834fcc441
zhou-dong <82224165@qq.com>
2016-01-07 00:32:40 -0600

    update

--- algorithm/src/org/dzhou/interview/treeandgraph/HasRouterOrNot.java
+++ algorithm/src/org/dzhou/interview/treeandgraph/HasRouterOrNot.java
@@ -1,5 +1,9 @@
 package org.dzhou.interview.treeandgraph;
 
+import java.util.LinkedList;
+
+import org.dzhou.interview.treeandgraph.Node.State;
+
 /**
  * Practice of "cracking the code interview"
  * 
@@ -11,13 +15,55 @@ package org.dzhou.interview.treeandgraph;
  */
 public class HasRouterOrNot {
 
-	boolean search() {
-
+	public static boolean search(Graph graph, Node start, Node end) {
+		if (start == end)
+			return true;
+		initNodesStateToUnvisited(graph);
+		LinkedList<Node> queue = new LinkedList<>();
+		addToQueue(queue, start);
+		Node node;
+		while (!queue.isEmpty()) {
+			node = queue.removeFirst();
+			if (node != null)
+				for (Node adjacent : node.children) {
+					if (adjacent.state != State.Unvisited)
+						continue;
+					if (adjacent == end)
+						return true;
+					else 
+						addToQueue(queue, adjacent);
+				}
+			node.state = State.Visited;
+		}
 		return false;
 	}
 
-}
+	public static boolean search2(Graph graph, Node start, Node end) {
+		initNodesStateToUnvisited(graph);
+		LinkedList<Node> queue = new LinkedList<>();
+		addToQueue(queue, start);
+		Node node;
+		while (true) {
+			node = queue.removeFirst();
+			if (node == null)
+				return false;
+			if (node == end)
+				return true;
+			for (Node adjacent : node.children)
+				if (adjacent.state == State.Unvisited)
+					addToQueue(queue, adjacent);
+			node.state = State.Visited;
+		}
+	}
 
-enum State {
-	Unvisited, Visited, Visiting
+	private static void initNodesStateToUnvisited(Graph graph) {
+		for (Node node : graph.nodes)
+			node.state = State.Unvisited;
+	}
+
+	private static void addToQueue(LinkedList<Node> queue, Node node) {
+		node.state = State.Visiting;
+		queue.add(node);
+	}
+
 }
--- algorithm/src/org/dzhou/interview/treeandgraph/Node.java
+++ algorithm/src/org/dzhou/interview/treeandgraph/Node.java
@@ -2,6 +2,11 @@ package org.dzhou.interview.treeandgraph;
 
 public class Node {
 
+	public enum State {
+		Unvisited, Visited, Visiting
+	}
+
+	public State state;
 	public String name;
 	public Node[] children;
 
