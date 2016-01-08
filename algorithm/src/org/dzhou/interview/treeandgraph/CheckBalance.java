33a473e6468815e8616d9160afd2be8f2a1ee91c
zhou-dong <82224165@qq.com>
2016-01-08 12:10:53 -0600

    update

--- algorithm/src/org/dzhou/interview/treeandgraph/CheckBalance.java
+++ algorithm/src/org/dzhou/interview/treeandgraph/CheckBalance.java
@@ -28,4 +28,24 @@ public class CheckBalance {
 			return isBalanced(root.left) && isBalanced(root.right);
 	}
 
+	public int getHeight2(TreeNode<?> root) {
+		if (root == null)
+			return -1;
+		int leftHeight = getHeight2(root.left);
+		if (leftHeight == Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+		int rightHeight = getHeight2(root.right);
+		if (rightHeight == Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+		int heightDiff = leftHeight - rightHeight;
+		if (Math.abs(heightDiff) > 1)
+			return Integer.MIN_VALUE;
+		else
+			return Math.max(leftHeight, rightHeight) + 1;
+	}
+
+	public boolean isBalanced2(TreeNode<?> root) {
+		return getHeight2(root) != Integer.MIN_VALUE;
+	}
+
 }
