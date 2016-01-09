d14fbbeed2b7e0da1e869bccb0f05ba7d74a32f8
zhou-dong <82224165@qq.com>
2016-01-08 23:59:51 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/treeandgraph/ValidateBST.java
@@ -0,0 +1,13 @@
+package org.dzhou.interview.treeandgraph;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * Implement a function to check if a binary tree is a binary search tree.
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class ValidateBST {
+
+}
