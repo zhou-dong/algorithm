acd611e962f2f277f9fcc93a3fd7ad27be9d364a
zhou-dong <82224165@qq.com>
2016-01-10 19:09:45 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/treeandgraph/Traversal.java
@@ -0,0 +1,40 @@
+package org.dzhou.interview.treeandgraph;
+
+/**
+ *
+ * Practice Traversal
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class Traversal {
+
+	public void inOrderTraversal(TreeNode<?> node) {
+		if (node == null)
+			return;
+		inOrderTraversal(node.left);
+		visit(node);
+		inOrderTraversal(node.right);
+	}
+
+	public void preOrderTraversal(TreeNode<?> node) {
+		if (node == null)
+			return;
+		visit(node);
+		preOrderTraversal(node.left);
+		preOrderTraversal(node.right);
+	}
+
+	public void postOrderTraversal(TreeNode<?> node) {
+		if (node == null)
+			return;
+		postOrderTraversal(node.left);
+		postOrderTraversal(node.right);
+		visit(node);
+	}
+
+	public void visit(TreeNode<?> node) {
+
+	}
+
+}
