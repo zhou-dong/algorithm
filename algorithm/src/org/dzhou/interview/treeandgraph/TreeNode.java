335e57daa6102337add506f266ece0e571c0824b
zhou-dong <82224165@qq.com>
2016-01-12 14:53:04 -0600

    update

--- algorithm/src/org/dzhou/interview/treeandgraph/TreeNode.java
+++ algorithm/src/org/dzhou/interview/treeandgraph/TreeNode.java
@@ -6,9 +6,24 @@ public class TreeNode<T> {
 		this.data = data;
 	}
 
+	public TreeNode(T data, TreeNode<T> parent) {
+		this.data = data;
+		this.parent = parent;
+	}
+
 	public T data;
 	public TreeNode<T> left;
 	public TreeNode<T> right;
 	public TreeNode<T> parent;
+
+	public TreeNode<T> addLeftChild(T data) {
+		this.left = new TreeNode<T>(data, this);
+		return this.left;
+	}
+
+	public TreeNode<T> addRightChild(T data) {
+		this.right = new TreeNode<T>(data, this);
+		return this.right;
+	}
 
 }
