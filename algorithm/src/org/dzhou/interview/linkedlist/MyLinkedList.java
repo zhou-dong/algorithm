bb989115fb7afbf4d8b031e72c78e902c156b246
zhou-dong <82224165@qq.com>
2016-01-11 23:41:12 -0600

    update

--- algorithm/src/org/dzhou/interview/linkedlist/MyLinkedList.java
+++ algorithm/src/org/dzhou/interview/linkedlist/MyLinkedList.java
@@ -19,6 +19,24 @@ public class MyLinkedList<E> {
 		tail.prev = head;
 	}
 
+	public void addFront(E data) {
+		ListNode<E> node = new ListNode<E>(data);
+		node.next = head.next;
+		node.prev = head;
+		node.next.prev = node;
+		head.next = node;
+		size++;
+	}
+
+	public void addEnd(E data) {
+		ListNode<E> node = new ListNode<E>(data);
+		node.prev = tail.prev;
+		node.next = tail;
+		node.prev.next = node;
+		tail.prev = node;
+		size++;
+	}
+
 	public int getSize() {
 		return size;
 	}
