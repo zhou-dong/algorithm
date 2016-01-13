472a06ce7b226d8421f662035264b318227f255d
zhou-dong <82224165@qq.com>
2016-01-13 09:41:05 -0600

    update

--- algorithm/src/org/dzhou/interview/treeandgraph/BinarySearchTreeImplement.java
+++ algorithm/src/org/dzhou/interview/treeandgraph/BinarySearchTreeImplement.java
@@ -1,11 +1,12 @@
 package org.dzhou.interview.treeandgraph;
 
 /**
+ * Implement of Binary Search Tree
  * 
  * @author DONG ZHOU
  *
  */
-public class BinarySearchTreeImplement<T extends Comparable<T>> {
+public class BinarySearchTreeImplement<T extends Comparable<? super T>> {
 
 	public TreeNode<T> root;
 
@@ -21,6 +22,32 @@ public class BinarySearchTreeImplement<T extends Comparable<T>> {
 				current = current.getRight();
 		}
 		return false;
+	}
+
+	public boolean insert(T toInsert) {
+		TreeNode<T> current = root;
+		TreeNode<T> parent = null;
+		int compare = 0;
+		while (current != null) {
+			parent = current;
+			compare = current.getData().compareTo(toInsert);
+			if (compare == 0)
+				return false;
+			else if (compare < 0)
+				current = current.getLeft();
+			else
+				current = current.getRight();
+		}
+		if (compare < 0)
+			parent.addLeftChild(toInsert);
+		else
+			parent.addRightChild(toInsert);
+		return true;
+	}
+
+	public T remove(T toRemove) {
+
+		return toRemove;
 	}
 
 }
