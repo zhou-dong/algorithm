5a2087755c71912e659577238777f87a8c102947
zhou-dong <82224165@qq.com>
2016-01-15 23:19:39 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/treeandgraph/map/Graph.java
@@ -0,0 +1,17 @@
+package org.dzhou.interview.treeandgraph.map;
+
+import java.util.Map;
+
+public class Graph {
+
+	private Map<Location, Node> graph;
+
+	public Map<Location, Node> getGraph() {
+		return graph;
+	}
+
+	public void setGraph(Map<Location, Node> graph) {
+		this.graph = graph;
+	}
+
+}
