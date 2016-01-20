b50453aad413e4e0d5991b1362283239d47eeeee
zhou-dong <82224165@qq.com>
2016-01-20 08:51:46 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/treeandgraph/CommonAncestor.java
@@ -0,0 +1,14 @@
+package org.dzhou.interview.treeandgraph;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * Design an algorithm and write code find the first common ancestor of two
+ * nodes in binary tree. Avoid storing additional nodes in a data structure.
+ * NOTE: This is not necessarily a binary search tree.
+ * 
+ * @author DONG ZHOU
+ */
+public class CommonAncestor {
+
+}
