c9948ab2f703de9aec5cb5e3f1e4725d6c0d09d8
zhou-dong <82224165@qq.com>
2016-01-21 09:35:04 -0600

    update

--- algorithm/src/org/dzhou/interview/treeandgraph/CommonAncestor4.java
+++ algorithm/src/org/dzhou/interview/treeandgraph/CommonAncestor4.java
@@ -23,4 +23,30 @@ public class CommonAncestor4<T> {
 		}
 	}
 
+	Result commonAncestorHelper(TreeNode<T> root, TreeNode<T> p, TreeNode<T> q) {
+		if (root == null)
+			return new Result(null, false);
+		if (root == p && root == q)
+			return new Result(root, true);
+		Result rx = commonAncestorHelper(root.getLeft(), p, q);
+		if (rx.isAncestor)
+			return rx;
+		Result ry = commonAncestorHelper(root.getRight(), p, q);
+		if (ry.isAncestor)
+			return ry;
+		if (rx.node != null && ry.node != null)
+			return new Result(root, true);
+		else if (root == p || root == q) {
+			boolean isAncesotr = rx.node != null || ry.node != null;
+			return new Result(root, isAncesotr);
+		} else
+			return new Result(rx.node != null ? rx.node : ry.node, false);
+	}
+
+	TreeNode<T> commonAncestor(TreeNode<T> root, TreeNode<T> p, TreeNode<T> q) {
+		Result r = commonAncestorHelper(root, p, q);
+		if (r.isAncestor)
+			return r.node;
+		return null;
+	}
 }
