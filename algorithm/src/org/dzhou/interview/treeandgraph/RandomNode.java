3efe31cc596f534b59d063352764089941f29491
zhou-dong <82224165@qq.com>
2016-01-21 12:19:28 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/treeandgraph/RandomNode.java
@@ -0,0 +1,18 @@
+package org.dzhou.interview.treeandgraph;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * @author DONG ZHOU
+ *
+ *         You are implementing a binary search tree class from scratch, which,
+ *         in addition to insert, find and delete, has a method getRandomNode()
+ *         which returns a random node from the tree. All nodes should be
+ *         equally likely to be chosen. Design and implement an algorithm for
+ *         getRandomNode, and explain how you would implement the rest of the
+ *         methods.
+ *
+ */
+public class RandomNode {
+
+}
