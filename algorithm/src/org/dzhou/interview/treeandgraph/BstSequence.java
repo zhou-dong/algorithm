a97565ef432d8f5a1aa2229dffb72a74f6c535f7
zhou-dong <82224165@qq.com>
2016-01-21 11:19:09 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/treeandgraph/BstSequence.java
@@ -0,0 +1,66 @@
+package org.dzhou.interview.treeandgraph;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * @author DONG ZHOU
+ *
+ *         A binary search tree was created by traversing through an array from
+ *         left to right and inserting each element. Given a binary search tree
+ *         with distinct elements, print all possible arrays that could have led
+ *         to this tree.
+ *
+ */
+public class BstSequence {
+
+	public List<LinkedList<Integer>> allSequences(TreeNode<Integer> node) {
+		if (node == null) {
+			return Collections.emptyList();
+		}
+		List<LinkedList<Integer>> result = new ArrayList<>();
+
+		LinkedList<Integer> prefix = new LinkedList<>();
+		prefix.add(node.getData());
+
+		List<LinkedList<Integer>> leftSeq = allSequences(node.getLeft());
+		List<LinkedList<Integer>> rightSeq = allSequences(node.getRight());
+
+		for (LinkedList<Integer> left : leftSeq) {
+			for (LinkedList<Integer> right : rightSeq) {
+				List<LinkedList<Integer>> weaved = new ArrayList<>();
+				weaveLists(left, right, weaved, prefix);
+				result.addAll(weaved);
+			}
+		}
+
+		return result;
+	}
+
+	void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second,
+			List<LinkedList<Integer>> results, LinkedList<Integer> prefix) {
+		if (first.size() == 0 || second.size() == 0) {
+			LinkedList<Integer> result = new LinkedList<>(prefix);
+			result.addAll(first);
+			result.addAll(second);
+			results.add(result);
+			return;
+		}
+		int headFirst = first.removeFirst();
+		prefix.addLast(headFirst);
+		weaveLists(first, second, results, prefix);
+		prefix.removeLast();
+		first.addFirst(headFirst);
+
+		int headSecond = second.removeFirst();
+		prefix.addLast(headSecond);
+		weaveLists(first, second, results, prefix);
+		prefix.removeLast();
+		second.addFirst(headSecond);
+	}
+
+}
