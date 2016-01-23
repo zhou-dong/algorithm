e5039acde576a75524195a06133fa45b572a80f1
zhou-dong <82224165@qq.com>
2016-01-22 22:49:05 -0600

    update

--- algorithm/src/org/dzhou/interview/treeandgraph/PathWithSum.java
+++ algorithm/src/org/dzhou/interview/treeandgraph/PathWithSum.java
@@ -14,4 +14,25 @@ package org.dzhou.interview.treeandgraph;
  */
 public class PathWithSum {
 
+	int countPathsWithSum(TreeNode<Integer> root, int targetSum) {
+		if (root == null)
+			return 0;
+		int pathsFromRoot = countPathsWithSumFromNode(root, targetSum, 0);
+		int pathsOnLeft = countPathsWithSum(root.getLeft(), targetSum);
+		int pathsOnRight = countPathsWithSum(root.getRight(), targetSum);
+		return pathsFromRoot + pathsOnLeft + pathsOnRight;
+	}
+
+	int countPathsWithSumFromNode(TreeNode<Integer> node, int targetSum, int currentSum) {
+		if (node == null)
+			return 0;
+		currentSum += node.getData();
+		int totalPaths = 0;
+		if (currentSum == targetSum)
+			totalPaths++;
+		totalPaths += countPathsWithSumFromNode(node.getLeft(), targetSum, currentSum);
+		totalPaths += countPathsWithSumFromNode(node.getRight(), targetSum, currentSum);
+		return totalPaths;
+	}
+
 }
