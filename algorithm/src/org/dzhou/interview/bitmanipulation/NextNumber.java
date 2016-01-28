7ca2e558b04a1c9be3a117b5dfb3c86332e502a0
zhou-dong <82224165@qq.com>
2016-01-28 08:29:35 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/bitmanipulation/NextNumber.java
@@ -0,0 +1,14 @@
+package org.dzhou.interview.bitmanipulation;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * Given a positive integer, print the next smallest and next largest number
+ * that have the same number of 1 bits in their binary representation.
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class NextNumber {
+
+}
