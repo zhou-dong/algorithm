884059e5cfa016e27e860cf43dec0db12362e39c
zhou-dong <82224165@qq.com>
2016-01-29 14:54:43 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/lambda/Country.java
@@ -0,0 +1,24 @@
+package org.dzhou.interview.lambda;
+
+public class Country {
+
+	private int population;
+	private String continent;
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	public String getContinent() {
+		return continent;
+	}
+
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+
+}
