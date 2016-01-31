e0e5adf992997686c187473ce2ffefde7feb7bdb
zhou-dong <82224165@qq.com>
2016-01-31 14:06:21 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/preview/Merge.java
@@ -0,0 +1,22 @@
+package org.dzhou.interview.preview;
+
+public class Merge {
+
+	public void merge(int[] a, int[] b, int lastA, int lastB) {
+		int indexA = lastA - 1;
+		int indexB = lastB - 1;
+		int indexMerged = lastA + lastB - 1;
+
+		while (indexB >= 0) {
+			if (indexA >= 0 && a[indexA] > b[indexB]) {
+				a[indexMerged] = a[indexA];
+				indexA--;
+			} else {
+				a[indexMerged] = a[indexB];
+				indexB--;
+			}
+			indexMerged--;
+		}
+	}
+
+}
