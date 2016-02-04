60c46e05db8b0cf9d38e632bf17f55c84c7a7764
zhou-dong <82224165@qq.com>
2016-02-04 10:07:33 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/RobotInGrid.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/RobotInGrid.java
@@ -2,6 +2,9 @@ package org.dzhou.interview.recursiondynamicprogramming;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Practice of "cracking the code interview"
@@ -17,7 +20,7 @@ import java.util.ArrayList;
  */
 public class RobotInGrid {
 
-	public boolean getPath(boolean[][] maze, int row, int col, ArrayList<Point> path) {
+	public boolean getPath(boolean[][] maze, int row, int col, List<Point> path) {
 		if (col < 0 || row < 0 || !maze[row][col])
 			return false;
 		boolean isAtOrigin = (row == 0) && (col == 0);
@@ -27,6 +30,36 @@ public class RobotInGrid {
 			return true;
 		}
 		return false;
+	}
+
+	List<Point> getPath(boolean[][] maze) {
+		if (maze == null || maze.length == 0)
+			return null;
+		List<Point> path = new ArrayList<>();
+		if (getPath(maze, maze.length - 1, maze[0].length - 1, path, new HashMap<>()))
+			return path;
+		else
+			return null;
+	}
+
+	public boolean getPath(boolean[][] maze, int row, int col, List<Point> path,
+			Map<Point, Boolean> cache) {
+		if (col < 0 || row < 0 || !maze[row][col])
+			return false;
+
+		Point p = new Point(row, col);
+		if (cache.containsKey(p))
+			return cache.get(p);
+
+		boolean isAtOrigin = (row == 0) && (col == 0);
+		if (isAtOrigin || getPath(maze, row, col - 1, path) || getPath(maze, row - 1, col, path)) {
+			path.add(p);
+			cache.put(p, true);
+			return true;
+		} else {
+			cache.put(p, false);
+			return false;
+		}
 	}
 
 }
