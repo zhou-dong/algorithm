aafc855caf537989b7fb2c6262ce2d7beec0e172
zhou-dong <82224165@qq.com>
2016-02-04 08:39:07 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/TripleStep.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/TripleStep.java
@@ -1,5 +1,7 @@
 package org.dzhou.interview.recursiondynamicprogramming;
 
+import java.util.Arrays;
+
 /**
  * A child is running up a staircase with n steps and can hop either 1 step, 2
  * steps, or 3 steps at a time. Implement a method to count how many possible
@@ -10,4 +12,30 @@ package org.dzhou.interview.recursiondynamicprogramming;
  */
 public class TripleStep {
 
+	public int countWays(int n) {
+		if (n < 0)
+			return 0;
+		else if (n == 0)
+			return 1;
+		else
+			return countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
+	}
+
+	public int countWays2(int n) {
+		int[] memo = new int[n + 1];
+		Arrays.fill(memo, -1);
+		return countWays(n, memo);
+	}
+
+	private int countWays(int n, int[] memo) {
+		if (n < 0)
+			return 0;
+		else if (n == 0)
+			return 1;
+		else if (memo[n] > 1)
+			return memo[n];
+		else
+			memo[n] = countWays(n - 1, memo) + countWays(n - 2, memo) + countWays(n - 3, memo);
+		return memo[n];
+	}
 }
