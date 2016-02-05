2a11f665babad183c820ea645fbb974f30a47227
zhou-dong <82224165@qq.com>
2016-02-05 00:12:29 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/HanoiTower.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/HanoiTower.java
@@ -10,7 +10,7 @@ import java.util.Stack;
  */
 public class HanoiTower {
 
-	class Tower {
+	static class Tower {
 		private Stack<Integer> disks;
 		private int index;
 
@@ -24,7 +24,7 @@ public class HanoiTower {
 		}
 
 		public boolean add(int data) {
-			if (!disks.isEmpty() && disks.peek() <= d) {
+			if (!disks.isEmpty() && disks.peek() <= data) {
 				return false;
 			} else {
 				disks.push(data);
@@ -36,6 +36,24 @@ public class HanoiTower {
 			int top = disks.pop();
 			t.add(top);
 		}
+
+		public void moveDisks(int n, Tower destination, Tower buffer) {
+			if (n > 0) {
+				moveDisks(n - 1, buffer, destination);
+				moveTopTo(destination);
+				buffer.moveDisks(n - 1, destination, this);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 3;
+		Tower[] towers = new Tower[n];
+		for (int i = 0; i < 3; i++)
+			towers[i] = new Tower(i);
+		for (int i = n - 1; i >= 0; i++)
+			towers[0].add(i);
+		towers[0].moveDisks(n, towers[2], towers[1]);
 	}
 
 }
