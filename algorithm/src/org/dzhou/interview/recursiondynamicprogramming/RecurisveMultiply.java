817d2a1dfeeb09b810ac52971631a2be9fb7c34c
zhou-dong <82224165@qq.com>
2016-02-04 21:51:51 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/RecurisveMultiply.java
@@ -0,0 +1,16 @@
+package org.dzhou.interview.recursiondynamicprogramming;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * @author DONG ZHOU
+ *
+ *         Write a recursive function to multiply two positive integers without
+ *         using the * operator (or / operator). You can use addition,
+ *         subtraction, and bit shifting, but you should minimize the number of
+ *         those operations.
+ *
+ */
+public class RecurisveMultiply {
+
+}
