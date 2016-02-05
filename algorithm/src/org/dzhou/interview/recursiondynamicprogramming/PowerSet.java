486491f7d4373eddd2902942d5bbf330b51c0a6e
zhou-dong <82224165@qq.com>
2016-02-04 21:46:59 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/PowerSet.java
@@ -0,0 +1,36 @@
+package org.dzhou.interview.recursiondynamicprogramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * @author DONG ZHOU
+ *
+ *         Write a method to return all subsets of a set.
+ *
+ */
+public class PowerSet {
+
+	public List<List<Integer>> getSubsets(List<Integer> set, int index) {
+		List<List<Integer>> allsubsets;
+		if (set.size() == index) {
+			allsubsets = new ArrayList<>();
+			allsubsets.add(new ArrayList<>());
+		} else {
+			allsubsets = getSubsets(set, index + 1);
+			int item = set.get(index);
+			List<List<Integer>> moresubsets = new ArrayList<>();
+			for (List<Integer> subset : allsubsets) {
+				List<Integer> newsubset = new ArrayList<>();
+				newsubset.addAll(subset);
+				newsubset.add(item);
+				moresubsets.add(newsubset);
+			}
+			allsubsets.addAll(moresubsets);
+		}
+		return allsubsets;
+	}
+
+}
