d1729768930e0d2ef28a8c2c0470913cd898dbdf
zhou-dong <82224165@qq.com>
2016-02-05 01:16:34 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/HanoiTower.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/HanoiTower.java
@@ -47,10 +47,10 @@ public class HanoiTower {
 
 	public static void main(String[] args) {
 		int n = 3;
-		Tower[] towers = new Tower[n];
+		Tower[] towers = new Tower[3];
 		for (int i = 0; i < 3; i++)
 			towers[i] = new Tower(i);
-		for (int i = n - 1; i >= 0; i++)
+		for (int i = n - 1; i >= 0; i--)
 			towers[0].add(i);
 		towers[0].moveDisks(n, towers[2], towers[1]);
 	}
