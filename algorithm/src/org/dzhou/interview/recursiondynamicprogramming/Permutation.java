bec0fb5931e6e57fd5cf6dfe3621995e57af60cd
zhou-dong <82224165@qq.com>
2016-02-06 16:06:11 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/Permutation.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/Permutation.java
@@ -16,19 +16,19 @@ public class Permutation {
 		if (str == null)
 			return null;
 		ArrayList<String> permutations = new ArrayList<>();
-
 		if (str.length() == 0) {
 			permutations.add("");
 			return permutations;
 		}
-
 		char first = str.charAt(0);
 		String remainder = str.substring(1);
 		ArrayList<String> words = getPerms(remainder);
 		for (String word : words) {
-
+			for (int j = 0; j < word.length(); j++) {
+				String s = insertCharAt(word, first, j);
+				permutations.add(s);
+			}
 		}
-
 		return permutations;
 	}
 
