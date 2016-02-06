83f2083604573de10347eb9881585af6b340c4a6
zhou-dong <82224165@qq.com>
2016-02-06 17:54:35 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/Permutation.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/Permutation.java
@@ -33,10 +33,46 @@ public class Permutation {
 		return permutations;
 	}
 
-	String insertCharAt(String word, char c, int i) {
+	private String insertCharAt(String word, char c, int i) {
 		String start = word.substring(0, i);
 		String end = word.substring(i);
 		return start + c + end;
+	}
+
+	public List<String> getPerms2(String remainder) {
+		int len = remainder.length();
+		List<String> result = new ArrayList<>();
+		if (len == 0) {
+			result.add("");
+			return result;
+		}
+		for (int i = 0; i < len; i++) {
+			String before = remainder.substring(0, i);
+			String after = remainder.substring(i + 1, len);
+			List<String> partials = getPerms2(before + after);
+			for (String s : partials) {
+				result.add(remainder.charAt(i) + s);
+			}
+		}
+		return result;
+	}
+
+	public List<String> getPerms3(String str) {
+		List<String> result = new ArrayList<>();
+		getPerms("", str, result);
+		return result;
+	}
+
+	private void getPerms(String prefix, String remainder, List<String> result) {
+		if (remainder.length() == 0)
+			result.add(prefix);
+		int len = remainder.length();
+		for (int i = 0; i < len; i++) {
+			String before = remainder.substring(0, i);
+			String after = remainder.substring(i + 1, len);
+			char c = remainder.charAt(i);
+			getPerms(prefix + c, before + after, result);
+		}
 	}
 
 }
