9583f8fc5db21cca5dcb66b509af29c405f26485
zhou-dong <82224165@qq.com>
2016-02-06 23:16:02 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/Parens.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/Parens.java
@@ -1,6 +1,8 @@
 package org.dzhou.interview.recursiondynamicprogramming;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -36,11 +38,39 @@ public class Parens {
 		return left + "()" + right;
 	}
 
+	void addParen(List<String> list, int leftRem, int rightRem, char[] str, int count) {
+		if (leftRem < 0 || rightRem < leftRem)
+			return;
+		if (leftRem == 0 && rightRem == 0) {
+			String s = String.copyValueOf(str);
+			list.add(s);
+		} else {
+			if (leftRem > 0) {
+				str[count] = '(';
+				addParen(list, leftRem - 1, rightRem, str, count + 1);
+			}
+			if (rightRem > leftRem) {
+				str[count] = ')';
+				addParen(list, leftRem, rightRem - 1, str, count + 1);
+			}
+		}
+	}
+
+	public List<String> generateParens2(int count) {
+		char[] str = new char[count * 2];
+		List<String> list = new ArrayList<>();
+		addParen(list, count, count, str, 0);
+		return list;
+	}
+
 	public static void main(String[] args) {
 		Parens parens = new Parens();
 		System.out.println(parens.generateParens(1));
 		System.out.println(parens.generateParens(2));
 		System.out.println(parens.generateParens(3));
-		System.out.println(parens.generateParens(4));
+		System.out.println();
+		System.out.println(parens.generateParens2(1));
+		System.out.println(parens.generateParens2(2));
+		System.out.println(parens.generateParens2(3));
 	}
 }
