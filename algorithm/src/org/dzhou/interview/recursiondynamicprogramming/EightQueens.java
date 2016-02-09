b29736bf14dfc4a94fc198f029f3afde39aa1b63
zhou-dong <82224165@qq.com>
2016-02-09 13:56:05 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/EightQueens.java
@@ -0,0 +1,19 @@
+package org.dzhou.interview.recursiondynamicprogramming;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * @author DONG ZHOU
+ *
+ *         Write an algorithm to print all ways of arranging eight queens on an
+ *         8x8 chess board so that none of them share the same row, column or
+ *         diagonal. In this case, "diagonal" means all diagonals, not just the
+ *         two that bisect the board.
+ */
+public class EightQueens {
+
+	private boolean checkValid() {
+
+		return true;
+	}
+}
