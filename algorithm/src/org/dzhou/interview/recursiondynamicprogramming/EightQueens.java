7d1a57212ae82f90d7dcf864fb5f0fb02cbebaa5
zhou-dong <82224165@qq.com>
2016-02-09 14:27:50 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/EightQueens.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/EightQueens.java
@@ -12,8 +12,19 @@ package org.dzhou.interview.recursiondynamicprogramming;
  */
 public class EightQueens {
 
-	private boolean checkValid() {
-
+	private boolean checkValid(Integer[] columns, int row1, int column1) {
+		for (int row2 = 0; row2 < row1; row2++) {
+			int column2 = columns[row2];
+			if (column1 == column2) {
+				return false;
+			}
+			int columnDistance = Math.abs(column2 - column1);
+			int rowDistance = row1 - row2;
+			if (columnDistance == rowDistance) {
+				return false;
+			}
+		}
 		return true;
 	}
+
 }
