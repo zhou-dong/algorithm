528e11b4cf640e78a84aebce8984ee75a41efa8c
zhou-dong <82224165@qq.com>
2016-02-10 10:07:57 -0600

    update

--- algorithm/src/org/dzhou/interview/recursiondynamicprogramming/EightQueens.java
+++ algorithm/src/org/dzhou/interview/recursiondynamicprogramming/EightQueens.java
@@ -1,5 +1,7 @@
 package org.dzhou.interview.recursiondynamicprogramming;
 
+import java.util.List;
+
 /**
  * Practice of "cracking the code interview"
  * 
@@ -9,8 +11,27 @@ package org.dzhou.interview.recursiondynamicprogramming;
  *         8x8 chess board so that none of them share the same row, column or
  *         diagonal. In this case, "diagonal" means all diagonals, not just the
  *         two that bisect the board.
+ * 
+ *         Observe that since each row can only have one queen, we don't need to
+ *         store our board as full 8x8 matrix. We only need a single array where
+ *         column[r]=c indicates that row r has a queen at column c.
  */
 public class EightQueens {
+
+	int GRID_SIZE = 8;
+
+	public void placeQueens(int row, Integer[] columns, List<Integer[]> results) {
+		if (row == GRID_SIZE) {
+			results.add(columns.clone());
+		} else {
+			for (int col = 0; col < GRID_SIZE; col++) {
+				if (checkValid(columns, row, col)) {
+					columns[row] = col;
+					placeQueens(row + 1, columns, results);
+				}
+			}
+		}
+	}
 
 	private boolean checkValid(Integer[] columns, int row1, int column1) {
 		for (int row2 = 0; row2 < row1; row2++) {
