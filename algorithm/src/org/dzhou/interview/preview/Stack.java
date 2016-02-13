dc06dafa66c64d2f54202982a5456693fe96a688
zhou-dong <82224165@qq.com>
2016-02-13 12:25:22 -0600

    update

--- algorithm/src/org/dzhou/interview/preview/Stack.java
+++ algorithm/src/org/dzhou/interview/preview/Stack.java
@@ -1,5 +1,36 @@
 package org.dzhou.interview.preview;
 
-public class Stack {
+public class Stack<T> {
+
+	static class Node<T> {
+		T data;
+		Node<T> next;
+
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+
+	Node<T> top;
+	int size = 0;
+
+	public void push(T data) {
+		Node<T> current = new Node<T>(data);
+		current.next = top;
+		top = current;
+	}
+
+	public T peek() {
+		return top.data;
+	}
+
+	public T pop() {
+		if (top == null) {
+			return null;
+		}
+		T result = top.data;
+		top = top.next;
+		return result;
+	}
 
 }
