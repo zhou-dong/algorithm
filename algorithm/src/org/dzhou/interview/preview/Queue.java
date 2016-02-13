a29d291a9df7877e101e79e94321ba15f9799a7d
zhou-dong <82224165@qq.com>
2016-02-13 12:42:09 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/preview/Queue.java
@@ -0,0 +1,36 @@
+package org.dzhou.interview.preview;
+
+public class Queue<T> {
+
+	static class Node<T> {
+		T data;
+		Node<T> next;
+
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+
+	Node<T> first, last;
+
+	public void enqueue(T data) {
+		Node<T> node = new Node<T>(data);
+		if (first == null) {
+			last = node;
+			first = last;
+		} else {
+			last.next = node;
+			last = node;
+		}
+	}
+
+	public T dequeue() {
+		if (first == null) {
+			return null;
+		}
+		T result = first.data;
+		first = first.next;
+		return result;
+	}
+
+}
