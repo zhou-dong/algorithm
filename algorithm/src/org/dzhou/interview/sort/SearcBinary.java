a79e2fe23ac4b7afe6638b15accc2404c34c5507
zhou-dong <82224165@qq.com>
2016-02-16 00:58:27 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/sort/SearcBinary.java
@@ -0,0 +1,35 @@
+package org.dzhou.interview.sort;
+
+public class SearcBinary {
+
+	public static int binarysearch(int[] array, int x) {
+		int low = 0;
+		int high = array.length - 1;
+		int mid;
+		while (low <= high) {
+			mid = (low + high) / 2;
+			if (array[mid] < x) {
+				low = mid + 1;
+			} else if (array[mid] > x) {
+				high = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+	public static int binarysearchRecurisve(int[] array, int x, int low, int high) {
+		if (low > high)
+			return -1;
+		int mid = (low + high) / 2;
+		if (array[mid] < x) {
+			return binarysearchRecurisve(array, x, mid + 1, high);
+		} else if (array[mid] > x) {
+			return binarysearchRecurisve(array, x, low, mid - 1);
+		} else {
+			return mid;
+		}
+	}
+
+}
