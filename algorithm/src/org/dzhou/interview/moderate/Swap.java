bbfad5efbada0064b12b687cf864e8a3ba175b20
zhou-dong <82224165@qq.com>
2016-02-16 19:17:41 -0600

    update

--- algorithm/src/org/dzhou/interview/moderate/Swap.java
+++ algorithm/src/org/dzhou/interview/moderate/Swap.java
@@ -13,10 +13,16 @@ public class Swap {
 	public static void swap(int a, int b) {
 		System.out.println(a + " " + b);
 		// example a=9, b=4
-		a = a - b; // difference: a=9-4=5
+		a = a - b; // difference: a = 9-4= 5
 		b = b + a; // b = 5+4 = 9
 		a = b - a; // a = 9-5 = 4
 		System.out.println(a + " " + b);
+	}
+
+	public static void swap(int[] array, int a, int b) {
+		array[a] = array[a] - array[b]; // a == difference
+		array[b] = array[b] + array[a]; // update b = b + a (difference) ;
+		array[a] = array[b] - array[a]; // update a = b - a (difference) ;
 	}
 
 }
--- algorithm/src/org/dzhou/interview/moderate/test/TestSwap.java
+++ algorithm/src/org/dzhou/interview/moderate/test/TestSwap.java
@@ -1,13 +1,21 @@
 package org.dzhou.interview.moderate.test;
 
 import org.dzhou.interview.moderate.Swap;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestSwap {
 
-	@Test
 	public void testSwap() {
 		Swap.swap(-1, 8);
+	}
+
+	@Test
+	public void testSwapArray() {
+		int[] origin = { 1, 2 };
+		int[] swaped = { 2, 1 };
+		Swap.swap(origin, 0, 1);
+		Assert.assertArrayEquals(swaped, origin);
 	}
 
 }
