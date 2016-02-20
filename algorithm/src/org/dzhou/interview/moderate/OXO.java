b1dd6ee4a93f09b4e3094c43011118fe2f33cf6b
zhou-dong <82224165@qq.com>
2016-02-19 22:51:18 -0600

    update

--- algorithm/src/org/dzhou/interview/moderate/OXO.java
+++ algorithm/src/org/dzhou/interview/moderate/OXO.java
@@ -1,5 +1,8 @@
 package org.dzhou.interview.moderate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Practice of "cracking the code interview"
  * 
@@ -8,17 +11,31 @@ package org.dzhou.interview.moderate;
  */
 public class OXO {
 
-	// 解法1
-	public boolean hasWin() {
+	public static Map<Integer, Boolean> recordMap = new HashMap<>();
 
-		return false;
+	// 解法1
+	public boolean hasWon(int board) {
+		return recordMap.get(board);
 	}
 
 	public static int convertBoardToInt(char[][] board) {
+		int factor = 1;
 		int sum = 0;
-
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				int v = 0;
+				if (board[i][j] == 'x') {
+					v = 1;
+				} else if (board[i][j] == 'o') {
+					v = 2;
+				}
+				sum += v * factor;
+				factor *= 3;
+			}
+		}
 		return sum;
 	}
+
 	// 解法2
 
 	// 解法3
