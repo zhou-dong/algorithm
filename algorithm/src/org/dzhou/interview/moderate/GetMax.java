273d07f635e7a3643db974bdc749f176ae065ea5
zhou-dong <82224165@qq.com>
2016-02-29 18:25:36 -0600

    updat

--- algorithm/src/org/dzhou/interview/moderate/GetMax.java
+++ algorithm/src/org/dzhou/interview/moderate/GetMax.java
@@ -25,4 +25,22 @@ public class GetMax {
 		return a * k + b * q;
 	}
 
+	public static int getMax(int a, int b) {
+
+		int c = a - b;
+		int sa = sign(a); // if (a>=0) then 1 else 0
+		int sb = sign(b); // if (b>=0) then 1 else 0 ;
+
+		int sc = sign(c);
+
+		// 若a和b，正负号不同，use_sign_of_a == 1 else 0
+		int use_sign_of_a = sa ^ sb;
+		int use_sign_of_c = flip(sa ^ sb);
+
+		int k = use_sign_of_a * sa + use_sign_of_c * sc;
+		int q = flip(k);
+
+		return a * k + b * q;
+	}
+
 }
