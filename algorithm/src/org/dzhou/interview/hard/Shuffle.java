8e0176587c3b965f5f28fffc2c5ad1a2db6fd599
zhou-dong <82224165@qq.com>
2016-03-01 16:56:18 -0600

    update

---
+++ algorithm/src/org/dzhou/interview/hard/Shuffle.java
@@ -0,0 +1,12 @@
+package org.dzhou.interview.hard;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * @author DONG ZHOU
+ *
+ *         编写一个方法，洗一副牌。要求做到完美洗牌，换言之，这副牌52！种排列组合出现的概率相同。假设给定一个完美的随机数生成器。
+ */
+public class Shuffle {
+
+}
