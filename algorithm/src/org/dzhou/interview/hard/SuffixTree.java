54776930ff918a676fc94d1f4d77c475983b0f5e
zhou-dong <82224165@qq.com>
2016-03-06 21:08:02 -0700

    update

---
+++ algorithm/src/org/dzhou/interview/hard/SuffixTree.java
@@ -0,0 +1,12 @@
+package org.dzhou.interview.hard;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * @author DONG ZHOU
+ *
+ *         给定一个字符串s和一个包含较短字符串的数组T，设计一个方法，根据T中的每一个较短字符串，对s进行搜索。
+ */
+public class SuffixTree {
+
+}
