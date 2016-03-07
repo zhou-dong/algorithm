de893fe148368af69b30616e0e70d439dea7022c
zhou-dong <82224165@qq.com>
2016-03-06 21:28:29 -0700

    update

--- algorithm/src/org/dzhou/interview/hard/SuffixTree.java
+++ algorithm/src/org/dzhou/interview/hard/SuffixTree.java
@@ -1,5 +1,9 @@
 package org.dzhou.interview.hard;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Practice of "cracking the code interview"
  * 
@@ -8,5 +12,21 @@ package org.dzhou.interview.hard;
  *         给定一个字符串s和一个包含较短字符串的数组T，设计一个方法，根据T中的每一个较短字符串，对s进行搜索。
  */
 public class SuffixTree {
+
+	class TreeNode {
+		HashMap<Character, TreeNode> children;
+		String text;
+		boolean isWord;
+		List<Integer> indexes;
+
+		public TreeNode() {
+			children = new HashMap<>();
+			indexes = new ArrayList<>();
+		}
+
+		public void insertString(String s, int index) {
+
+		}
+	}
 
 }
