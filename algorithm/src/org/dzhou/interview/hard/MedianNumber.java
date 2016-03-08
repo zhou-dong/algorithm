1988242eaa407e98518ed79fcd81ebb5b780955a
zhou-dong <82224165@qq.com>
2016-03-08 00:53:43 -0700

    update

---
+++ algorithm/src/org/dzhou/interview/hard/MedianNumber.java
@@ -0,0 +1,16 @@
+package org.dzhou.interview.hard;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class MedianNumber {
+
+	public static double getMedian() {
+
+		return 0d;
+	}
+}
