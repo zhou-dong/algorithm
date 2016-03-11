2c23ebe19e59625224a015e83ac4668e5be18ec0
zhou-dong <82224165@qq.com>
2016-03-10 22:00:11 -0700

    update

--- algorithm/src/org/dzhou/interview/hard/ChageWord.java
+++ algorithm/src/org/dzhou/interview/hard/ChageWord.java
@@ -1,5 +1,13 @@
 package org.dzhou.interview.hard;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 /**
  * Practice of "cracking the code interview"
  * 
@@ -7,5 +15,55 @@ package org.dzhou.interview.hard;
  *
  */
 public class ChageWord {
+
+	public static LinkedList<String> transfrom(String startWord, String stopWord,
+			Set<String> dictionary) {
+		startWord = startWord.toUpperCase();
+		stopWord = stopWord.toUpperCase();
+
+		Queue<String> actionQueue = new LinkedList<>();
+		Set<String> visitedSet = new HashSet<>();
+		Map<String, String> backtrackMap = new TreeMap<>();
+
+		actionQueue.add(startWord);
+		visitedSet.add(startWord);
+
+		while (!actionQueue.isEmpty()) {
+			String w = actionQueue.poll();
+			for (String v : getOneEditWords(w)) {
+				if (v.equals(stopWord)) {
+					LinkedList<String> result = new LinkedList<>();
+					result.add(v);
+					while (w != null) {
+						result.add(0, w);
+						w = backtrackMap.get(w);
+					}
+				}
+				if (dictionary.contains(v)) {
+					if (!visitedSet.contains(v)) {
+						actionQueue.add(v);
+						visitedSet.add(v);
+						backtrackMap.put(v, w);
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+
+	static Set<String> getOneEditWords(String word) {
+		Set<String> words = new TreeSet<>();
+		for (int i = 0; i < word.length(); i++) {
+			char[] wordArray = word.toCharArray();
+			for (char c = 'A'; c <= 'Z'; c++) {
+				if (c != word.charAt(i)) {
+					wordArray[i] = c;
+					words.add(new String(wordArray));
+				}
+			}
+		}
+		return words;
+	}
 
 }
