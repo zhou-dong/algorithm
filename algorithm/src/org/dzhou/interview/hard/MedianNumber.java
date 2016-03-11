16c51bd5cd1b9e8b9ee1863018215a9871df3379
zhou-dong <82224165@qq.com>
2016-03-11 07:45:11 -0700

    update

--- algorithm/src/org/dzhou/interview/hard/MedianNumber.java
+++ algorithm/src/org/dzhou/interview/hard/MedianNumber.java
@@ -1,5 +1,8 @@
 package org.dzhou.interview.hard;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Practice of "cracking the code interview"
  * 
@@ -9,12 +12,37 @@ package org.dzhou.interview.hard;
  */
 public class MedianNumber {
 
+	Comparator<Integer> maxHeapComparatpr;
+	Comparator<Integer> minHeapComparator;
+	private PriorityQueue<Integer> maxHeap, minHeap;
+
 	public void addNewNumber(int randomNumber) {
+		if (maxHeap.size() == minHeap.size()) {
+			if (minHeap.peek() != null && randomNumber > minHeap.peek()) {
+				maxHeap.offer(minHeap.poll());
+				minHeap.offer(randomNumber);
+			} else {
+				maxHeap.offer(randomNumber);
+			}
+		} else {
+			if (randomNumber < maxHeap.peek()) {
+				minHeap.offer(maxHeap.poll());
+				maxHeap.offer(randomNumber);
+			} else {
+				minHeap.offer(randomNumber);
+			}
+		}
 	}
 
-	public static double getMedian() {
-
-		return 0d;
+	public double getMedian() {
+		if (maxHeap.isEmpty()) {
+			return 0;
+		}
+		if (maxHeap.size() == minHeap.size()) {
+			return ((double) maxHeap.peek() + (double) minHeap.peek()) / 2;
+		} else {
+			return maxHeap.peek();
+		}
 	}
 
 }
