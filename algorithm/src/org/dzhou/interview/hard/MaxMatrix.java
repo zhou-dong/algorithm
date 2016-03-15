bb119ec55d756a8442d5a8860c68029090521813
zhou-dong <82224165@qq.com>
2016-03-14 21:52:14 -0500

    update

---
+++ algorithm/src/org/dzhou/interview/hard/MaxMatrix.java
@@ -0,0 +1,18 @@
+package org.dzhou.interview.hard;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class MaxMatrix {
+
+	int getMaxMatrix(int[][] original) {
+
+		int maxArea = Integer.MIN_VALUE;
+
+		return maxArea;
+
+	}
+}
