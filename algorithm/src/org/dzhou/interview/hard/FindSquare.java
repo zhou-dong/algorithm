bb323fc35e4009477cf500c991a81e15b263279b
zhou-dong <82224165@qq.com>
2016-03-17 12:21:13 -0500

    update

--- algorithm/src/org/dzhou/interview/hard/FindSquare.java
+++ algorithm/src/org/dzhou/interview/hard/FindSquare.java
@@ -8,15 +8,60 @@ package org.dzhou.interview.hard;
  */
 public class FindSquare {
 
-	void findSquare() {
+	class Subsquare {
+		int row;
+		int col;
+		int size;
 
+		public Subsquare(int row, int col, int size) {
+			this.row = row;
+			this.col = col;
+			this.size = size;
+		}
 	}
 
-	void findSquareWithSize() {
+	Subsquare findSquare(int[][] matrix) {
+		for (int i = matrix.length; i >= 1; i--) {
+			Subsquare subsquare = findSquareWithSize(matrix, i);
+			if (subsquare != null) {
+				return subsquare;
+			}
+		}
+		return null;
+	}
 
+	Subsquare findSquareWithSize(int[][] matrix, int squareSize) {
+		int count = matrix.length - squareSize + 1;
+		for (int row = 0; row < count; row++) {
+			for (int col = 0; col < count; col++) {
+				if (isSquare(matrix, row, col, squareSize)) {
+					return new Subsquare(row, col, squareSize);
+				}
+			}
+		}
+		return null;
 	}
 
 	boolean isSquare(int[][] matrix, int row, int col, int size) {
-		return false;
+		// 检查上边界和下边界
+		for (int j = 0; j < size; j++) {
+			if (matrix[row][col + j] == 1) {
+				return false;
+			}
+			if (matrix[row + size - 1][col + j] == 1) {
+				return false;
+			}
+		}
+		// 检查左边界和右边界
+		for (int i = 1; i < size - 1; i++) {
+			if (matrix[row + i][col] == 1) {
+				return false;
+			}
+			if (matrix[row + i][col + size - 1] == 1) {
+				return false;
+			}
+		}
+		return true;
 	}
+
 }
