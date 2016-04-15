d8b8ff4899efb3e6b956865d9628be7a4917bc75
zhou-dong <82224165@qq.com>
2016-04-15 01:22:29 -0500

    update

--- algorithm/src/org/dzhou/review/linkedlist/LinkedList.java
+++ algorithm/src/org/dzhou/review/linkedlist/LinkedList.java
@@ -1,5 +1,39 @@
 package org.dzhou.review.linkedlist;
 
-public class LinkedList {
+public class LinkedList<T> {
+
+	class Node {
+		T data;
+		Node prev;
+		Node next;
+
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+
+	private Node first;
+	private Node last;
+	private int size;
+
+	public LinkedList() {
+		this.size = 0;
+		this.last = new Node(null);
+		this.first = new Node(null);
+		first.next = last;
+		last.prev = first;
+	}
+
+	public void add(T data) {
+		addLast(data);
+	}
+
+	public void addLast(T data) {
+		Node current = new Node(data);
+	}
+
+	public int getSize() {
+		return size;
+	}
 
 }
