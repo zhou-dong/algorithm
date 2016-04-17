15dc24c281fe5e55d83d8efb4677aaf0175a58e8
zhou-dong <82224165@qq.com>
2016-04-16 21:12:19 -0500

    update

--- algorithm/src/org/dzhou/review/tree/Node.java
+++ algorithm/src/org/dzhou/review/tree/Node.java
@@ -7,4 +7,55 @@ public class Node<T> {
 	private Node<T> right;
 	private Node<T> parent;
 
+	public Node(T data) {
+		this.data = data;
+	}
+
+	public Node(T data, Node<T> parent) {
+		this.data = data;
+		this.parent = parent;
+	}
+
+	public Node<T> addLeftChild(T data) {
+		this.left = new Node<T>(data, this);
+		return this.left;
+	}
+
+	public Node<T> addRightChild(T data) {
+		this.right = new Node<T>(data, this);
+		return this.right;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Node<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node<T> left) {
+		this.left = left;
+	}
+
+	public Node<T> getRight() {
+		return right;
+	}
+
+	public void setRight(Node<T> right) {
+		this.right = right;
+	}
+
+	public Node<T> getParent() {
+		return parent;
+	}
+
+	public void setParent(Node<T> parent) {
+		this.parent = parent;
+	}
+
 }
