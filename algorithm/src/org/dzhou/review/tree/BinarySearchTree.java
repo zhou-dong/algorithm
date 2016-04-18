5e43c850ca44b8aee7f23fa7c6bbb5d8283ed5fb
zhou-dong <82224165@qq.com>
2016-04-18 01:07:37 -0500

    update

--- algorithm/src/org/dzhou/review/tree/BinarySearchTree.java
+++ algorithm/src/org/dzhou/review/tree/BinarySearchTree.java
@@ -2,4 +2,54 @@ package org.dzhou.review.tree;
 
 public class BinarySearchTree<T extends Comparable<T>> {
 
+	class Node {
+		T object;
+		Node left;
+		Node right;
+
+		public Node(T object) {
+			this.object = object;
+		}
+	}
+
+	Node root;
+
+	public T search(T object) {
+		Node current = root;
+		while (current != null) {
+			int compare = current.object.compareTo(object);
+			if (compare == 0)
+				return current.object;
+			else if (compare < 0)
+				current = current.left;
+			else
+				current = current.right;
+		}
+		return null;
+	}
+
+	public void insert(T object) {
+		if (root == null) {
+			root = new Node(object);
+			return;
+		}
+		Node current = root;
+		Node parent = null;
+		int compare = 0;
+		while (current != null) {
+			compare = current.object.compareTo(object);
+			if (compare == 0)
+				throw new IllegalArgumentException();
+			parent = current;
+			if (compare < 0)
+				current = current.left;
+			else
+				current = current.right;
+		}
+		if (compare < 0)
+			parent.left = new Node(object);
+		else
+			parent.right = new Node(object);
+	}
+
 }
