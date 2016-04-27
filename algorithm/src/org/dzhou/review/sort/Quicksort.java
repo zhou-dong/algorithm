5156f8d0124e916659db657a7cdfac9a8df1bd71
zhou-dong <82224165@qq.com>
2016-04-27 15:48:50 -0500

    update

--- algorithm/src/org/dzhou/review/sort/Quicksort.java
+++ algorithm/src/org/dzhou/review/sort/Quicksort.java
@@ -3,6 +3,15 @@ package org.dzhou.review.sort;
 /**
  * @author DONG ZHOU
  */
-public class Quicksort {
+public class Quicksort<T extends Comparable<T>> {
+
+	private static <T extends Comparable<T>> void sort(T[] array, int lowIndex, int highIndex) {
+	}
+
+	public static <T extends Comparable<T>> void sort(T[] array) {
+		if (array.length < 2)
+			return;
+		sort(array, 0, array.length - 1);
+	}
 
 }
