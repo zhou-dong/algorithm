475d716043ca21412d843fc63be2420f76b7c9f4
zhou-dong <82224165@qq.com>
2016-05-04 01:33:47 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/ReverseString.java
+++ algorithm/src/org/dzhou/practice/easy/ReverseString.java
@@ -1,7 +1,10 @@
 package org.dzhou.practice.easy;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
- * 
  * Write a function that takes a string as input and returns the string
  * reversed.
  * 
@@ -12,9 +15,84 @@ package org.dzhou.practice.easy;
  */
 public class ReverseString {
 
-	// TODO
-	public String reverseString(String s) {
-		return null;
+	// method one
+	public static String reverseString(String s) {
+		char[] result = new char[s.length()];
+		for (int i = 0; i < result.length; i++)
+			result[i] = s.charAt(result.length - 1 - i);
+		return new String(result);
+	}
+
+	// method two
+	public static String reverseStringFromBothSide(String s) {
+		char[] result = s.toCharArray();
+		for (int left = 0, right = result.length - 1; left < right; left++, right--)
+			swap(result, left, right);
+		return new String(result);
+	}
+
+	private static void swap(char[] chars, int x, int y) {
+		char temp = chars[x];
+		chars[x] = chars[y];
+		chars[y] = temp;
+	}
+
+	// method three
+	public static String reverseStringByStack(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (char c : s.toCharArray())
+			stack.add(c);
+		char[] result = new char[stack.size()];
+		for (int i = 0; i < result.length; i++)
+			result[i] = stack.pop();
+		return new String(result);
+	}
+
+	// method four
+	public static String reverseStringByRecursionFrontEnd(String s) {
+		char[] input = s.toCharArray();
+		char[] result = new char[input.length];
+		reverseStringByRecursionFrontEnd(input, result, 0);
+		return new String(result);
+	}
+
+	private static void reverseStringByRecursionFrontEnd(char[] input, char[] result, int index) {
+		if (index < input.length) {
+			result[index] = input[input.length - 1 - index];
+			reverseStringByRecursionFrontEnd(input, result, index + 1);
+		}
+	}
+
+	// method five
+	public static String reverseStringByRecursionEndFront(String s) {
+		char[] input = s.toCharArray();
+		char[] result = new char[input.length];
+		reverseStringByRecursionEndFront(input, result, 0);
+		return new String(result);
+	}
+
+	private static void reverseStringByRecursionEndFront(char[] input, char[] result, int index) {
+		if (index < input.length) {
+			reverseStringByRecursionEndFront(input, result, index + 1);
+			result[result.length - 1 - index] = input[index];
+		}
+	}
+
+	// method six
+	public static String reverseStringByStringBuilder(String s) {
+		return new StringBuilder(s).reverse().toString();
+	}
+
+	// method seven
+	public static String reverseStringByLinkedList(String s) {
+		LinkedList<Character> list = new LinkedList<>();
+		for (char c : s.toCharArray())
+			list.add(c);
+		Collections.reverse(list);
+		char[] result = new char[list.size()];
+		for (int i = 0; i < result.length; i++)
+			result[i] = list.get(i);
+		return new String(result);
 	}
 
 }
---
+++ algorithm/src/org/dzhou/practice/easy/test/ReverseStringTest.java
@@ -0,0 +1,54 @@
+package org.dzhou.practice.easy.test;
+
+import org.dzhou.practice.easy.ReverseString;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseStringTest {
+
+	String input = "hello";
+	String expected = "olleh";
+
+	@Test
+	public void testReverseString() {
+		String answer = ReverseString.reverseString(input);
+		Assert.assertEquals(expected, answer);
+	}
+
+	@Test
+	public void testReverseStringFromBothSide() {
+		String answer = ReverseString.reverseStringFromBothSide(input);
+		Assert.assertEquals(expected, answer);
+	}
+
+	@Test
+	public void testReverseStringByStack() {
+		String answer = ReverseString.reverseStringByStack(input);
+		Assert.assertEquals(expected, answer);
+	}
+
+	@Test
+	public void testReverseStringByRecursionFrontEnd() {
+		String answer = ReverseString.reverseStringByRecursionFrontEnd(input);
+		Assert.assertEquals(expected, answer);
+	}
+
+	@Test
+	public void testReverseStringByRecursionEndFront() {
+		String answer = ReverseString.reverseStringByRecursionEndFront(input);
+		Assert.assertEquals(expected, answer);
+	}
+
+	@Test
+	public void testReverseStringByStringBuilder() {
+		String answer = ReverseString.reverseStringByStringBuilder(input);
+		Assert.assertEquals(expected, answer);
+	}
+
+	@Test
+	public void testReverseStringByLinkedList() {
+		String answer = ReverseString.reverseStringByLinkedList(input);
+		Assert.assertEquals(expected, answer);
+	}
+
+}
