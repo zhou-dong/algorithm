54919b90b1c52608e491872fc1f63509cc6e59d6
zhou-dong <82224165@qq.com>
2016-05-03 19:54:55 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/ReverseString.java
@@ -0,0 +1,20 @@
+package org.dzhou.practice.easy;
+
+/**
+ * 
+ * Write a function that takes a string as input and returns the string
+ * reversed.
+ * 
+ * Example: Given s = "hello", return "olleh".
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class ReverseString {
+
+	// TODO
+	public String reverseString(String s) {
+		return null;
+	}
+
+}
