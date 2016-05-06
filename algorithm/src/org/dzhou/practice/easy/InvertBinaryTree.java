4e631d954638ff0e4f7a01fcac4ac5c8932c306a
zhou-dong <82224165@qq.com>
2016-05-06 17:14:19 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/InvertBinaryTree.java
@@ -0,0 +1,24 @@
+package org.dzhou.practice.easy;
+
+/**
+ * @author DONG ZHOU
+ */
+public class InvertBinaryTree {
+
+	// Definition for a binary tree node.
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public TreeNode invertTree(TreeNode root) {
+
+		return null;
+	}
+
+}
