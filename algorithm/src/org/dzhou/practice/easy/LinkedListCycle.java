2bc4b8b9840df17d6d26f3ed541a85138d965813
zhou-dong <82224165@qq.com>
2016-05-09 11:14:18 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/LinkedListCycle.java
@@ -0,0 +1,29 @@
+package org.dzhou.practice.easy;
+
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ * 
+ * Follow up: Can you solve it without using extra space?
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class LinkedListCycle {
+
+	// Definition for singly-linked list.
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	public boolean hasCycle(ListNode head) {
+
+		return true;
+	}
+
+}
