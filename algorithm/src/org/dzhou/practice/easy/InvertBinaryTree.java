2ae645319b7065e3efce35584cb0c166bd478cb7
zhou-dong <82224165@qq.com>
2016-05-08 19:15:12 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/InvertBinaryTree.java
+++ algorithm/src/org/dzhou/practice/easy/InvertBinaryTree.java
@@ -1,5 +1,8 @@
 package org.dzhou.practice.easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author DONG ZHOU
  */
@@ -17,8 +20,31 @@ public class InvertBinaryTree {
 	}
 
 	public TreeNode invertTree(TreeNode root) {
+		if (root == null)
+			return null;
+		TreeNode left = invertTree(root.left);
+		TreeNode right = invertTree(root.right);
+		root.left = right;
+		root.right = left;
+		return root;
+	}
 
-		return null;
+	public TreeNode invertTree2(TreeNode root) {
+		if (root == null)
+			return null;
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			TreeNode current = queue.poll();
+			TreeNode temp = current.left;
+			current.left = current.right;
+			current.right = temp;
+			if (current.left != null)
+				queue.add(current.left);
+			if (current.right != null)
+				queue.add(current.right);
+		}
+		return root;
 	}
 
 }
