f15aa06210dcc81251003800582a714a09367d90
周动 <zhoudong@zhoudongdeAir.att.net>
2016-05-10 17:39:04 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,30 @@
+package org.dzhou.practice.easy;
+
+/**
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the root
+ * node down to the farthest leaf node.
+ * 
+ * @author zhoudong
+ *
+ */
+public class MaximumDepthOfBinaryTree {
+
+	// Definition for a binary tree node.
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public int maxDepth(TreeNode root) {
+
+		return 0;
+	}
+
+}
