ad3fe6fadb466d7a97607ca64e39fb51963e02d7
zhou-dong <82224165@qq.com>
2016-05-11 00:08:43 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/ContainsDuplicate.java
+++ algorithm/src/org/dzhou/practice/easy/ContainsDuplicate.java
@@ -26,8 +26,8 @@ public class ContainsDuplicate {
 
 	public boolean containsDuplicate1(int[] nums) {
 		Arrays.sort(nums);
-		for (int i = 0, j = 1; i < nums.length - 1; i++, j++) {
-			if (nums[i] == nums[j])
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == nums[i + 1])
 				return true;
 		}
 		return false;
---
+++ algorithm/src/org/dzhou/practice/easy/ExcelSheetColumnNumber.java
@@ -0,0 +1,49 @@
+package org.dzhou.practice.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Related to question Excel Sheet Column Title
+ * 
+ * Given a column title as appear in an Excel sheet, return its corresponding
+ * column number.
+ * 
+ * For example:<br>
+ * 
+ * A -> 1 <br>
+ * B -> 2 <br>
+ * C -> 3 <br>
+ * ... <br>
+ * Z -> 26 <br>
+ * AA -> 27 <br>
+ * AB -> 28 <br>
+ * 
+ * @author zhoudong
+ * 
+ *         实现26进制
+ */
+public class ExcelSheetColumnNumber {
+
+	public int titleToNumber(String s) {
+		if (s == null || s.length() == 0)
+			return 0;
+		int result = 0;
+		Map<Character, Integer> vocabulary = createVocabulary();
+		for (int i = 0; i < s.length(); i++) {
+			int val = vocabulary.get(s.charAt(s.length() - 1 - i));
+			result += (val * Math.pow(26, i));
+		}
+		return result;
+	}
+
+	private Map<Character, Integer> createVocabulary() {
+		char[] chars = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+				'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+		Map<Character, Integer> vocabulary = new HashMap<>();
+		for (int i = 0; i < chars.length; i++)
+			vocabulary.put(chars[i], i + 1);
+		return vocabulary;
+	}
+
+}
