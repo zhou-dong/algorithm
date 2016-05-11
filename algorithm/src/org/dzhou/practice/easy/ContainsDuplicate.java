5da0080e74b12539512ef2ce123fd9517a881bc6
zhou-dong <82224165@qq.com>
2016-05-10 22:57:24 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/ContainsDuplicate.java
@@ -0,0 +1,36 @@
+package org.dzhou.practice.easy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an array of integers, find if the array contains any duplicates. Your
+ * function should return true if any value appears at least twice in the array,
+ * and it should return false if every element is distinct.
+ * 
+ * @author zhoudong
+ *
+ */
+public class ContainsDuplicate {
+
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> set = new HashSet<>();
+		for (int i : nums) {
+			if (set.contains(i))
+				return true;
+			set.add(i);
+		}
+		return false;
+	}
+
+	public boolean containsDuplicate1(int[] nums) {
+		Arrays.sort(nums);
+		for (int i = 0, j = 1; i < nums.length - 1; i++, j++) {
+			if (nums[i] == nums[j])
+				return true;
+		}
+		return false;
+	}
+
+}
