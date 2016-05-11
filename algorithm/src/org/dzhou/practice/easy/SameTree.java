b58827660b878ab6bfe497f40b33dbbcc3fa25d4
zhou-dong <82224165@qq.com>
2016-05-10 21:55:26 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/SameTree.java
@@ -0,0 +1,30 @@
+package org.dzhou.practice.easy;
+
+/**
+ * Given two binary trees, write a function to check if they are equal or not.
+ * 
+ * Two binary trees are considered equal if they are structurally identical and
+ * the nodes have the same value.
+ * 
+ * @author zhoudong
+ *
+ */
+public class SameTree {
+
+	// Definition for a binary tree node.
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+
+		return true;
+	}
+
+}
