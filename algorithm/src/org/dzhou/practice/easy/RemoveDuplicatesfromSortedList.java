94d94790a52a67fd77ba6a9cfab4f48d8232af6f
zhou-dong <82224165@qq.com>
2016-05-12 20:56:31 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,17 @@
+package org.dzhou.practice.easy;
+
+/**
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ * 
+ * For example,
+ * 
+ * Given 1->1->2, return 1->2.<br>
+ * Given 1->1->2->3->3, return 1->2->3.
+ * 
+ * @author zhoudong
+ *
+ */
+public class RemoveDuplicatesfromSortedList {
+
+}
