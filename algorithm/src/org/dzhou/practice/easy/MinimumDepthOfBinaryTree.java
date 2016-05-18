e7be8c43895757497c5ed4b90792f1f9b8b403cf
zhou-dong <82224165@qq.com>
2016-05-18 01:19:55 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/MinimumDepthOfBinaryTree.java
+++ algorithm/src/org/dzhou/practice/easy/MinimumDepthOfBinaryTree.java
@@ -1,5 +1,8 @@
 package org.dzhou.practice.easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given a binary tree, find its minimum depth.
  * 
@@ -22,15 +25,41 @@ public class MinimumDepthOfBinaryTree {
 		}
 	}
 
-	public int minDepth(TreeNode root) {
-		if (root == null)
-			return 0;
-		int minLeft = minDepth(root.left);
-		int minRight = minDepth(root.right);
-		if (minLeft == 0 || minRight == 0) {
-			return minLeft > minRight ? minLeft + 1 : minRight + 1;
+	class Solution {
+		public int minDepth(TreeNode root) {
+			if (root == null)
+				return 0;
+			int minLeft = minDepth(root.left);
+			int minRight = minDepth(root.right);
+			if (minLeft == 0 || minRight == 0) {
+				return minLeft > minRight ? minLeft + 1 : minRight + 1;
+			}
+			return Math.min(minLeft, minRight) + 1;
 		}
-		return Math.min(minLeft, minRight) + 1;
+	}
+
+	class Solution1 {
+		public int minDepth(TreeNode root) {
+			if (root == null)
+				return 0;
+			Queue<TreeNode> queue = new LinkedList<>();
+			queue.add(root);
+			int depth = 1;
+			while (!queue.isEmpty()) {
+				int size = queue.size();
+				for (int i = 0; i < size; i++) {
+					TreeNode current = queue.poll();
+					if (current.left == null && current.right == null)
+						return depth;
+					if (current.left != null)
+						queue.add(current.left);
+					if (current.right != null)
+						queue.add(current.right);
+				}
+				depth++;
+			}
+			return depth;
+		}
 	}
 
 }
