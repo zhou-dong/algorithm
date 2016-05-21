e9c7c27290ece125eca7993b49677282b69f56b1
zhou-dong <82224165@qq.com>
2016-05-20 19:49:58 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/PalindromeLinkedList.java
@@ -0,0 +1,30 @@
+package org.dzhou.practice.easy;
+
+/**
+ * Given a singly linked list, determine if it is a palindrome.
+ * 
+ * Follow up: Could you do it in O(n) time and O(1) space?
+ * 
+ * @author zhoudong
+ *
+ */
+public class PalindromeLinkedList {
+
+	// Definition for singly-linked list.
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	class Solution {
+		public boolean isPalindrome(ListNode head) {
+
+			return true;
+		}
+	}
+
+}
