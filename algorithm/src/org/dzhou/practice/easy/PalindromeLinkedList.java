1011a37204045f055f22502335d723e37997873b
zhou-dong <82224165@qq.com>
2016-05-21 17:11:04 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/PalindromeLinkedList.java
+++ algorithm/src/org/dzhou/practice/easy/PalindromeLinkedList.java
@@ -20,22 +20,41 @@ public class PalindromeLinkedList {
 		}
 	}
 
-	public boolean isPalindrome(ListNode head) {
-
-		return true;
-	}
-
-	private ListNode reverse(ListNode head) {
-		ListNode prev = null;
-
-		while (head != null) {
-			ListNode temp = head.next;
-			head.next = prev;
-			prev = head;
-			head = temp;
+	class Solution {
+		public boolean isPalindrome(ListNode head) {
+			if (head == null)
+				return true;
+			ListNode middle = findMiddle(head);
+			middle.next = reverse(middle.next);
+			ListNode p1 = head, p2 = middle.next;
+			while (p1 != null && p2 != null && p1.val == p2.val) {
+				p1 = p1.next;
+				p2 = p2.next;
+			}
+			return p2 == null;
 		}
 
-		return prev;
+		private ListNode findMiddle(ListNode head) {
+			if (head == null)
+				return null;
+			ListNode slow = head, fast = head.next;
+			while (fast != null && fast.next != null) {
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+			return slow;
+		}
+
+		private ListNode reverse(ListNode head) {
+			ListNode prev = null;
+			while (head != null) {
+				ListNode temp = head.next;
+				head.next = prev;
+				prev = head;
+				head = temp;
+			}
+			return prev;
+		}
 	}
 
 }
