b266023a79d0845f2bd0a6e721a72e126f2a9c46
zhou-dong <82224165@qq.com>
2016-05-22 00:48:31 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/CountPrimes.java
+++ algorithm/src/org/dzhou/practice/easy/CountPrimes.java
@@ -15,14 +15,19 @@ public class CountPrimes {
 	public int countPrimes(int n) {
 		boolean[] prime = new boolean[n];
 		Arrays.fill(prime, true);
-		for (int i = 2; i < n; i++) {
 
-		}
-		int count = 0;
-		for (int i = 1; i < n; i++) {
+		for (int i = 2; i < n; i++) {
 			if (prime[i]) {
-				count++;
+				for (int j = i * 2; j < n; j = j + i) {
+					prime[j] = false;
+				}
 			}
+		}
+
+		int count = 0;
+		for (int i = 2; i < n; i++) {
+			if (prime[i])
+				count++;
 		}
 		return count;
 	}
