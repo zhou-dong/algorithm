d0dd3d4e440cd9efe6f46f1a1fe077e4cbf94b18
zhou-dong <82224165@qq.com>
2016-05-22 20:50:35 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/LongestCommonPrefix.java
+++ algorithm/src/org/dzhou/practice/easy/LongestCommonPrefix.java
@@ -9,9 +9,42 @@ package org.dzhou.practice.easy;
  */
 public class LongestCommonPrefix {
 
-	public String longestCommonPrefix(String[] strs) {
+	public class Solution {
+		public String longestCommonPrefix(String[] strs) {
+			if (strs == null || strs.length == 0)
+				return "";
+			int len = minLen(strs);
+			StringBuilder result = new StringBuilder();
+			for (int i = 0; i < len; i++) {
+				for (int j = 1; j < strs.length; j++) {
+					if (strs[0].charAt(i) != strs[j].charAt(i))
+						return result.toString();
+				}
+				result.append(strs[0].charAt(i));
+			}
+			return result.toString();
+		}
 
-		return "";
+		private int minLen(String[] strs) {
+			int min = Integer.MAX_VALUE;
+			for (int i = 0; i < strs.length; i++)
+				min = Math.min(min, strs[i].length());
+			return min;
+		}
+	}
+
+	public class Solution1 {
+		public String longestCommonPrefix(String[] strs) {
+			if (strs == null || strs.length == 0)
+				return "";
+			for (int i = 0; i < strs[0].length(); i++) {
+				for (int j = 1; j < strs.length; j++) {
+					if (strs[j].length() == i || strs[0].charAt(i) != strs[j].charAt(i))
+						return strs[0].substring(0, i);
+				}
+			}
+			return strs[0];
+		}
 	}
 
 }
