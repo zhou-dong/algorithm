47887a35ce2e8d089fc37497a2ff176bd28ced3c
zhou-dong <82224165@qq.com>
2016-05-24 02:26:43 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/ZigZagConversion.java
+++ algorithm/src/org/dzhou/practice/easy/ZigZagConversion.java
@@ -8,9 +8,20 @@ package org.dzhou.practice.easy;
 public class ZigZagConversion {
 
 	public String convert(String s, int numRows) {
-		StringBuilder result = new StringBuilder();
-
-		return result.toString();
+		if (numRows < 2 || s == null || s.length() == 0) {
+			return s;
+		}
+		int size = 2 * (numRows - 1);
+		StringBuilder rst = new StringBuilder();
+		for (int i = 0; i < numRows; i++) {
+			for (int j = i; j < s.length(); j += size) {
+				rst.append(s.charAt(j));
+				if (i != 0 && i != numRows - 1 && j + size - 2 * i < s.length()) {
+					rst.append(s.charAt(j + size - 2 * i));
+				}
+			}
+		}
+		return rst.toString();
 	}
 
 }
