c815b83839810385650a7ef1b3c87e3c6785afde
zhou-dong <82224165@qq.com>
2016-05-25 01:07:42 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/PascalTriangleII.java
+++ algorithm/src/org/dzhou/practice/easy/PascalTriangleII.java
@@ -17,12 +17,7 @@ public class PascalTriangleII {
 
 	public List<Integer> getRow(int rowIndex) {
 		Integer[] result = new Integer[rowIndex + 1];
-		for (int i = 0; i < result.length; i++) {
-			if (i == 0 || i == rowIndex)
-				result[i] = 1;
-			else
-				result[i] = 0;
-		}
+		result[0] = 1;
 		for (int i = 1; i < rowIndex + 1; i++) {
 			result[i] = 1;
 			for (int j = i - 1; j > 0; j--) {
