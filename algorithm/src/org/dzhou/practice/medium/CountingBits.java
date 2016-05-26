97d548101a3d92bab5bd8553358eb056ea211bfb
zhou-dong <82224165@qq.com>
2016-05-26 03:25:05 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/CountingBits.java
+++ algorithm/src/org/dzhou/practice/medium/CountingBits.java
@@ -1,10 +1,59 @@
 package org.dzhou.practice.medium;
 
 /**
+ * Given a non negative integer number num. For every numbers i in the range 0 ≤
+ * i ≤ num calculate the number of 1's in their binary representation and return
+ * them as an array.
+ * 
+ * Example:<br>
+ * For num = 5 you should return [0,1,1,2,1,2].<br>
+ * 
+ * Follow up:<br>
+ * 
+ * It is very easy to come up with a solution with run time
+ * O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a
+ * single pass?<br>
+ * Space complexity should be O(n).<br>
+ * Can you do it like a boss? Do it without using any builtin function like
+ * __builtin_popcount in c++ or in any other language.
+ * 
+ * Hint:<br>
+ * 
+ * You should make use of what you have produced already.<br>
+ * Divide the numbers in ranges like [2-3], [4-7], [8-15] and so on. And try to
+ * generate new range from previous.<br>
+ * Or does the odd/even status of the number help you in calculating the number
+ * of 1s?<br>
+ * 
+ * 0 0000 0<br>
+ * -------------<br>
+ * 1 0001 1<br>
+ * -------------<br>
+ * 2 0010 1<br>
+ * 3 0011 2<br>
+ * -------------<br>
+ * 4 0100 1<br>
+ * 5 0101 2<br>
+ * 6 0110 2<br>
+ * 7 0111 3<br>
+ * -------------<br>
+ * 8 1000 1<br>
+ * 9 1001 2<br>
+ * 10 1010 2<br>
+ * 11 1011 3<br>
+ * 12 1100 2<br>
+ * 13 1101 3<br>
+ * 14 1110 3<br>
+ * 15 1111 4<br>
  * 
  * @author zhoudong
  *
  */
 public class CountingBits {
+
+	public int[] countBits(int num) {
+
+		return null;
+	}
 
 }
