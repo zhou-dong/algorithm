948f13dd2bd8dc378a7d0b2f4d877c9fcae1036d
zhou-dong <82224165@qq.com>
2016-06-04 19:25:28 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/ProductOfArrayExceptSelf.java
+++ algorithm/src/org/dzhou/practice/medium/ProductOfArrayExceptSelf.java
@@ -42,7 +42,17 @@ public class ProductOfArrayExceptSelf {
 			for (int i = 1; i < nums.length; i++) {
 				pSeq[i] = pSeq[i - 1] * nums[i - 1];
 			}
-			return null;
+
+			nSeq[nums.length - 1] = 1;
+			for (int i = nums.length - 2; i >= 0; i--) {
+				nSeq[i] = nSeq[i + 1] * nums[i + 1];
+			}
+
+			for (int i = 0; i < nums.length; i++) {
+				pSeq[i] = pSeq[i] * nSeq[i];
+			}
+
+			return pSeq;
 		}
 
 	}
