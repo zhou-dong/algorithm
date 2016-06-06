aa96bc1fefce129caaa1e594f4b2cb4272e8be1b
zhou-dong <82224165@qq.com>
2016-06-06 01:20:24 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/medium/IntegerBreak.java
@@ -0,0 +1,50 @@
+package org.dzhou.practice.medium;
+
+/**
+ * Given a positive integer n, break it into the sum of at least two positive
+ * integers and maximize the product of those integers. Return the maximum
+ * product you can get.
+ * 
+ * For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 =
+ * 3 + 3 + 4).
+ * 
+ * Note: you may assume that n is not less than 2.
+ * 
+ * Hint:
+ * 
+ * 1. There is a simple O(n) solution to this problem.
+ * 
+ * 2. You may check the breaking results of n ranging from 7 to 10 to discover
+ * the regularities.
+ * 
+ * @author zhoudong
+ *
+ */
+public class IntegerBreak {
+
+	public class Solution {
+
+		public int integerBreak(int n) {
+
+			if (n == 2)
+				return 1;
+			if (n == 3)
+				return 2;
+
+			int result = 1;
+			while (n > 2) {
+				result *= 3;
+				n -= 3;
+			}
+
+			if (n == 1)
+				return (result / 3) * 4;// 余1，把其中的一个3加1变为4再相乘
+			if (n == 2)
+				return result * 2; // 余2,则可直接把2与res相乘
+
+			return result;
+		}
+
+	}
+
+}
