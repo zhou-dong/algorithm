d15a611cedd9681b3c483d0fd2b2cd6b37df7b09
zhou-dong <82224165@qq.com>
2016-06-06 14:28:52 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/MissingNumber.java
+++ algorithm/src/org/dzhou/practice/medium/MissingNumber.java
@@ -1,5 +1,7 @@
 package org.dzhou.practice.medium;
 
+import java.util.Arrays;
+
 /**
  * Given an array containing n distinct numbers taken from [0, 1, 2, ..., n],
  * find the one that is missing from the array.
@@ -16,15 +18,40 @@ public class MissingNumber {
 
 	// 用等差数列的求和公式求出0到n之间所有的数字之和，然后再遍历数组算出给定数字的累积和，然后做减法，差值就是丢失的那个数字
 	public class Solution {
-
 		public int missingNumber(int[] nums) {
 			int sum = 0;
 			for (int n : nums)
 				sum += n;
-			sum *= 2;
-			return (nums.length * (nums.length + 1) - sum) / 2;
+			return (nums.length * (nums.length + 1) - (sum * 2)) / 2;
 		}
+	}
 
+	// 使用位操作Bit Manipulation来解的，用到了异或操作的特性
+	public class Solution1 {
+		public int missingNumber(int[] nums) {
+			int result = 0;
+			for (int n : nums)
+				result ^= n;
+			for (int i = 0; i <= nums.length; i++)
+				result ^= i;
+			return result;
+		}
+	}
+
+	// 用二分查找法来做
+	public class Solution2 {
+		public int missingNumber(int[] nums) {
+			Arrays.sort(nums);
+			int left = 0, right = nums.length;
+			while (left < right) {
+				int mid = (left + right) / 2;
+				if (nums[mid] > mid)
+					right = mid;
+				else
+					left = mid + 1;
+			}
+			return right;
+		}
 	}
 
 }
