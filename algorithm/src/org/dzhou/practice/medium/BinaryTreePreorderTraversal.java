e38e2cbd09cbb497eab2cac10b8c52d3bf2fb13b
zhou-dong <82224165@qq.com>
2016-06-06 15:36:43 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/medium/BinaryTreePreorderTraversal.java
@@ -0,0 +1,61 @@
+package org.dzhou.practice.medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * 
+ * For example:
+ * 
+ * Given binary tree {1,#,2,3},
+ * 
+ * 1<br>
+ * \<br>
+ * 2<br>
+ * /<br>
+ * 3<br>
+ * 
+ * return [1,2,3].
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ * 
+ * 
+ * @author zhoudong
+ *
+ */
+public class BinaryTreePreorderTraversal {
+
+	// Definition for a binary tree node.
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public class Solution {
+
+		public List<Integer> preorderTraversal(TreeNode root) {
+			if (root == null)
+				return Collections.emptyList();
+			List<Integer> result = new ArrayList<>();
+			helper(root, result);
+			return result;
+		}
+
+		private void helper(TreeNode node, List<Integer> list) {
+			if (node == null)
+				return;
+			list.add(node.val);
+			helper(node.left, list);
+			helper(node.right, list);
+		}
+
+	}
+
+}
