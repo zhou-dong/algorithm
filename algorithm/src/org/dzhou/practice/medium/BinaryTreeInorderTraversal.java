aa48664c37e30bb43ceac190628f5c32ae36e97d
zhou-dong <82224165@qq.com>
2016-06-06 16:42:14 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/BinaryTreeInorderTraversal.java
+++ algorithm/src/org/dzhou/practice/medium/BinaryTreeInorderTraversal.java
@@ -6,20 +6,20 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * Given a binary tree, return the preorder traversal of its nodes' values.
+ * Given a binary tree, return the inorder traversal of its nodes' values.
  * 
  * For example:
  * 
- * Given binary tree {1,#,2,3},
+ * Given binary tree [1,null,2,3], return [1,3,2].
  * 
- * return [1,2,3].
+ * Note:
  * 
- * Note: Recursive solution is trivial, could you do it iteratively?
+ * Recursive solution is trivial, could you do it iteratively?
  * 
  * @author zhoudong
  *
  */
-public class BinaryTreePreorderTraversal {
+public class BinaryTreeInorderTraversal {
 
 	// Definition for a binary tree node.
 	public class TreeNode {
@@ -35,7 +35,7 @@ public class BinaryTreePreorderTraversal {
 	// 递归做法
 	public class Solution {
 
-		public List<Integer> preorderTraversal(TreeNode root) {
+		public List<Integer> inorderTraversal(TreeNode root) {
 			if (root == null)
 				return Collections.emptyList();
 			List<Integer> result = new ArrayList<>();
@@ -46,8 +46,8 @@ public class BinaryTreePreorderTraversal {
 		private void helper(TreeNode node, List<Integer> list) {
 			if (node == null)
 				return;
-			list.add(node.val);
 			helper(node.left, list);
+			list.add(node.val);
 			helper(node.right, list);
 		}
 
@@ -56,24 +56,22 @@ public class BinaryTreePreorderTraversal {
 	// 非递归方法
 	public class Solution1 {
 
-		public List<Integer> preorderTraversal(TreeNode root) {
+		public List<Integer> inorderTraversal(TreeNode root) {
 			if (root == null)
 				return Collections.emptyList();
-
 			List<Integer> result = new ArrayList<>();
 			Stack<TreeNode> stack = new Stack<>();
 
 			while (root != null || !stack.isEmpty()) {
 				if (root != null) {
 					stack.push(root);
-					result.add(root.val);
 					root = root.left;
 				} else {
 					root = stack.pop();
+					result.add(root.val);
 					root = root.right;
 				}
 			}
-
 			return result;
 		}
 
