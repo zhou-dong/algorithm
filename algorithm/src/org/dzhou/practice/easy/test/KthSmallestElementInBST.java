dd74d7d8cbfed2f095ca6a0749cfe021d8472e97
zhou-dong <82224165@qq.com>
2016-06-07 01:07:10 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/test/KthSmallestElementInBST.java
@@ -0,0 +1,25 @@
+package org.dzhou.practice.easy.test;
+
+/**
+ * Given a binary search tree, write a function kthSmallest to find the kth
+ * smallest element in it.
+ * 
+ * Note:<br>
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ * 
+ * Follow up:<br>
+ * What if the BST is modified (insert/delete operations) often and you need to
+ * find the kth smallest frequently? How would you optimize the kthSmallest
+ * routine?
+ * 
+ * Hint:<br>
+ * Try to utilize the property of a BST.<br>
+ * What if you could modify the BST node's structure?<br>
+ * The optimal runtime complexity is O(height of BST).<br>
+ * 
+ * @author zhoudong
+ *
+ */
+public class KthSmallestElementInBST {
+
+}
