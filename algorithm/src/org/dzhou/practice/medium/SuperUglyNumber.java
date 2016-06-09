c2484609dc0b49ad72a201c5aa7ac61bcca4383d
zhou-dong <82224165@qq.com>
2016-06-09 13:46:34 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/medium/SuperUglyNumber.java
@@ -0,0 +1,27 @@
+package org.dzhou.practice.medium;
+
+/**
+ * Write a program to find the nth super ugly number.
+ * 
+ * Super ugly numbers are positive numbers whose all prime factors are in the
+ * given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16,
+ * 19, 26, 28, 32] is the sequence of the first 12 super ugly numbers given
+ * primes = [2, 7, 13, 19] of size 4.
+ * 
+ * Note:<br>
+ * (1) 1 is a super ugly number for any given primes.<br>
+ * (2) The given numbers in primes are in ascending order.<br>
+ * (3) 0 < k ≤ 100, 0 < n ≤ 106, 0 < primes[i] < 1000.*
+ * 
+ * @author zhoudong
+ *
+ */
+public class SuperUglyNumber {
+
+	public int nthSuperUglyNumber(int n, int[] primes) {
+
+		return 0;
+
+	}
+
+}
