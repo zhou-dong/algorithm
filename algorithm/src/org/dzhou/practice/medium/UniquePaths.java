fc4345a29e42fed32bd9313b7463a482fee000e2
zhou-dong <82224165@qq.com>
2016-06-10 15:29:06 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/UniquePaths.java
+++ algorithm/src/org/dzhou/practice/medium/UniquePaths.java
@@ -29,7 +29,6 @@ public class UniquePaths {
 		int[][] dp = new int[m][n];
 		for (int i = 0; i < n; i++)
 			dp[0][i] = 1;
-
 		for (int j = 0; j < m; j++)
 			dp[j][0] = 1;
 
@@ -38,6 +37,7 @@ public class UniquePaths {
 				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 			}
 		}
+
 		return dp[m - 1][n - 1];
 
 	}
