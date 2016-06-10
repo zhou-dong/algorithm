41b57afe5b770d1d24aba83c088251b5686aed0e
zhou-dong <82224165@qq.com>
2016-06-10 14:54:45 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/UniquePaths.java
+++ algorithm/src/org/dzhou/practice/medium/UniquePaths.java
@@ -20,7 +20,26 @@ package org.dzhou.practice.medium;
 public class UniquePaths {
 
 	public int uniquePaths(int m, int n) {
-		return 0;
+
+		if (m == 0 || n == 0)
+			return 0;
+		if (m == 1 || n == 1)
+			return 1;
+
+		int[][] dp = new int[m][n];
+		for (int i = 0; i < n; i++)
+			dp[0][i] = 1;
+
+		for (int j = 0; j < m; j++)
+			dp[j][0] = 1;
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+		return dp[m - 1][n - 1];
+
 	}
 
 }
