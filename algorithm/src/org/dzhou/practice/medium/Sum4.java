54ad17db0e8158f66c5f27ad29bcfff35d25218e
zhou-dong <82224165@qq.com>
2016-06-16 18:29:59 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/Sum4.java
+++ algorithm/src/org/dzhou/practice/medium/Sum4.java
@@ -2,7 +2,6 @@ package org.dzhou.practice.medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
