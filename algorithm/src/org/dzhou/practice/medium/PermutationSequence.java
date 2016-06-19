41138ccb98ab21496574e7ece56fca2aaac2881e
zhou-dong <82224165@qq.com>
2016-06-19 01:19:21 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/medium/PermutationSequence.java
@@ -0,0 +1,34 @@
+package org.dzhou.practice.medium;
+
+/**
+ * The set [1,2,3,…,n] contains a total of n! unique permutations.
+ * 
+ * By listing and labeling all of the permutations in order, We get the
+ * following sequence (ie, for n = 3):
+ * 
+ * 1. "123"<br>
+ * 2. "132"<br>
+ * 3. "213"<br>
+ * 4. "231"<br>
+ * 5. "312"<br>
+ * 6. "321"<br>
+ * 
+ * Given n and k, return the kth permutation sequence.
+ * 
+ * Note: Given n will be between 1 and 9 inclusive.
+ * 
+ * @author zhoudong
+ *
+ */
+public class PermutationSequence {
+
+	public class Solution {
+
+		public String getPermutation(int n, int k) {
+
+			return "";
+		}
+
+	}
+
+}
