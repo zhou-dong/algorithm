f0176ed18c8d0c960f554c0bfd9ea370574e195d
zhou-dong <82224165@qq.com>
2016-06-20 14:54:52 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/medium/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,57 @@
+package org.dzhou.practice.medium;
+
+/**
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list.
+ * 
+ * For example,
+ * 
+ * Given 1->2->3->3->4->4->5, return 1->2->5.
+ * 
+ * Given 1->1->1->2->3, return 2->3.
+ * 
+ * @author zhoudong
+ *
+ */
+public class RemoveDuplicatesFromSortedListII {
+
+	// Definition for singly-linked list.
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public class Solution {
+
+		// Given 1->2->3->3->4->4->5, return 1->2->5.
+		// Given 1->1->1->2->3, return 2->3.
+		public ListNode deleteDuplicates(ListNode head) {
+
+			if (head == null || head.next == null)
+				return head;
+
+			ListNode dummy = new ListNode(0);
+			dummy.next = head;
+			head = dummy;
+
+			while (head.next != null && head.next.next != null) {
+				if (head.next.val == head.next.next.val) {
+					int val = head.next.val;
+					while (head.next != null && head.next.val == val) {
+						head.next = head.next.next;
+					}
+				} else {
+					head = head.next;
+				}
+			}
+
+			return dummy.next;
+		}
+
+	}
+
+}
