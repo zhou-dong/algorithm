b4014e1982efc2b6d09fa49a1ffae333ffe68d01
zhou-dong <82224165@qq.com>
2016-06-22 02:07:52 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/WordBreak.java
+++ algorithm/src/org/dzhou/practice/medium/WordBreak.java
@@ -19,7 +19,26 @@ public class WordBreak {
 
 		public boolean wordBreak(String s, Set<String> wordDict) {
 
-			return true;
+			if (s == null || s.length() == 0)
+				return true;
+
+			boolean[] dp = new boolean[s.length() + 1];
+			dp[0] = true;
+
+			for (int i = 0; i < s.length(); i++) {
+
+				StringBuilder str = new StringBuilder(s.substring(0, i + 1));
+				for (int j = 0; j <= i; j++) {
+					if (dp[j] && wordDict.contains(str.toString())) {
+						dp[i + 1] = true;
+						break;
+					}
+					str.deleteCharAt(0);
+				}
+
+			}
+
+			return dp[s.length()];
 		}
 
 	}
