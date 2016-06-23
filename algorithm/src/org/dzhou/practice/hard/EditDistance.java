7371dd93185823fbefdbeca3a6a4df1ae19779b9
zhou-dong <82224165@qq.com>
2016-06-23 02:03:13 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/EditDistance.java
+++ algorithm/src/org/dzhou/practice/hard/EditDistance.java
@@ -21,9 +21,35 @@ public class EditDistance {
 
 		public int minDistance(String word1, String word2) {
 
-			int count = 0;
+			int len1 = word1.length();
+			int len2 = word2.length();
 
-			return count;
+			int[][] dp = new int[len1 + 1][len2 + 1];
+
+			for (int i = 0; i < len1 + 1; i++) {
+				dp[i][0] = i;
+			}
+
+			for (int j = 0; j < len2 + 1; j++) {
+				dp[0][j] = j;
+			}
+
+			for (int i = 1; i < len1 + 1; i++) {
+				char c1 = word1.charAt(i - 1);
+				for (int j = 1; j < len2 + 1; j++) {
+					char c2 = word2.charAt(j - 1);
+					if (c1 == c2) {
+						dp[i][j] = dp[i - 1][j - 1];
+					} else {
+						int replace = dp[i - 1][j - 1] + 1;
+						int insert = dp[i - 1][j] + 1;
+						int delete = dp[i][j - 1] + 1;
+						dp[i][j] = Math.min(Math.min(replace, insert), delete);
+					}
+				}
+			}
+
+			return dp[len1][len2];
 
 		}
 
