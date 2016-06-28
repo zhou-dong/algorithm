863698b9451833b1ef17ed9d4f5895b6236fb37d
zhou-dong <82224165@qq.com>
2016-06-28 00:38:20 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/PopulatingNextRightPointersInEachNodeII.java
+++ algorithm/src/org/dzhou/practice/hard/PopulatingNextRightPointersInEachNodeII.java
@@ -1,8 +1,5 @@
 package org.dzhou.practice.hard;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * Follow up for problem "Populating Next Right Pointers in Each Node".
  * 
@@ -49,27 +46,37 @@ public class PopulatingNextRightPointersInEachNodeII {
 		public void connect(TreeLinkNode root) {
 			if (root == null)
 				return;
-			Queue<TreeLinkNode> queue = new LinkedList<>();
-			queue.add(root);
-			while (!queue.isEmpty()) {
-				int size = queue.size();
-				for (int i = 0; i < size; i++) {
-					TreeLinkNode curr = queue.poll();
-					if (curr.left != null) {
-						curr.left.next = curr.right;
-						queue.add(curr.left);
-					}
-					TreeLinkNode right = curr.right;
-					if (right == null)
-						continue;
-					if (i < size - 1)
-						right.next = queue.peek().left;
-					else
-						right.next = null;
-					queue.add(right);
+
+			TreeLinkNode p = root.next;
+
+			// 因此，这道题目首要是找到右孩子的第一个有效的next链接节点，然后再处理左孩子。然后依次递归处理右孩子，左孩子
+			while (p != null) {
+				if (p.left != null) {
+					p = p.left;
+					break;
 				}
+				if (p.right != null) {
+					p = p.right;
+					break;
+				}
+				p = p.next;
 			}
+
+			if (root.right != null) {
+				root.right.next = p;
+			}
+
+			if (root.left != null) {
+				if (root.right != null)
+					root.left.next = root.right;
+				else
+					root.left.next = p;
+			}
+
+			connect(root.right);
+			connect(root.left);
 		}
 
 	}
+
 }
