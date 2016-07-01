b4e510c31b9bf36a485d29bb48f09f55edcbe555
zhou-dong <82224165@qq.com>
2016-06-30 23:58:02 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/SumOfTwoIntegers.java
+++ algorithm/src/org/dzhou/practice/easy/SumOfTwoIntegers.java
@@ -13,13 +13,14 @@ public class SumOfTwoIntegers {
 
 	// 异或 + 移位
 	public class Solution {
-
 		public int getSum(int a, int b) {
-			int ans = 0;
-
-			return ans;
+			while (b != 0) {
+				int c = a ^ b;// 所有不需要进位的，都保持原样，需要进位的为0
+				b = (a & b) << 1;// 所有需要进位的为1，其它位为0, 然后向左移动一位
+				a = c;
+			}
+			return a;
 		}
-
 	}
 
 }
