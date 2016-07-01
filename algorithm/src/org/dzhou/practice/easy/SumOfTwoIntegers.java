2a88a5154cae422df8fc4bc55bc1e9e1cae2c9e5
zhou-dong <82224165@qq.com>
2016-06-30 23:28:22 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/easy/SumOfTwoIntegers.java
@@ -0,0 +1,25 @@
+package org.dzhou.practice.easy;
+
+/**
+ * Calculate the sum of two integers a and b, but you are not allowed to use the
+ * operator + and -.
+ * 
+ * Example: Given a = 1 and b = 2, return 3.
+ * 
+ * @author zhoudong
+ *
+ */
+public class SumOfTwoIntegers {
+
+	// 异或 + 移位
+	public class Solution {
+
+		public int getSum(int a, int b) {
+			int ans = 0;
+
+			return ans;
+		}
+
+	}
+
+}
---
+++ algorithm/src/org/dzhou/practice/medium/LongestIncreasingSubsequence.java
@@ -0,0 +1,12 @@
+package org.dzhou.practice.medium;
+
+/**
+ * Given an unsorted array of integers, find the length of longest increasing
+ * subsequence.
+ * 
+ * @author zhoudong
+ *
+ */
+public class LongestIncreasingSubsequence {
+
+}
