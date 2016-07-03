8fef37f704d3cba55ddfc5f7ddb54eabf66350b6
zhou-dong <82224165@qq.com>
2016-07-02 22:50:06 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/MaximumProductSubarray.java
+++ algorithm/src/org/dzhou/practice/medium/MaximumProductSubarray.java
@@ -16,8 +16,22 @@ public class MaximumProductSubarray {
 	public class Solution {
 
 		public int maxProduct(int[] nums) {
+			if (nums == null || nums.length == 0)
+				return 0;
 
-			return 0;
+			int max = nums[0];
+			int max_temp = nums[0];
+			int min_temp = nums[0];
+
+			for (int i = 1; i < nums.length; i++) {
+				int a = nums[i] * max_temp;
+				int b = nums[i] * min_temp;
+				max_temp = Math.max(Math.max(a, b), nums[i]);
+				min_temp = Math.min(Math.min(a, b), nums[i]);
+				max = Math.max(max_temp, max);
+			}
+
+			return max;
 		}
 
 	}
