d765bac1cf50b06e5493bfef2d5dab93570c8785
zhou-dong <82224165@qq.com>
2016-07-02 22:21:16 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/medium/MaximumProductSubarray.java
@@ -0,0 +1,25 @@
+package org.dzhou.practice.medium;
+
+/**
+ * Find the contiguous subarray within an array (containing at least one number)
+ * which has the largest product.
+ * 
+ * For example, given the array [2,3,-2,4],
+ * 
+ * the contiguous subarray [2,3] has the largest product = 6.
+ * 
+ * @author zhoudong
+ *
+ */
+public class MaximumProductSubarray {
+
+	public class Solution {
+
+		public int maxProduct(int[] nums) {
+
+			return 0;
+		}
+
+	}
+
+}
