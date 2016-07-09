d418e679543efe2665fe60535a6a9668cc59d1a7
zhou-dong <82224165@qq.com>
2016-07-09 14:07:57 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/medium/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,32 @@
+package org.dzhou.practice.medium;
+
+/**
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes
+ * in the tree.
+ * 
+ * @author zhoudong
+ *
+ */
+public class LowestCommonAncestorOfABinaryTree {
+
+	// Definition for a binary tree node.
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public class Solution {
+
+		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+			return null;
+		}
+
+	}
+
+}
