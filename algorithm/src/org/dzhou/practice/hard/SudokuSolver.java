bcd80cbd08e506633cae9a108f20e44ac3d9ccf7
zhou-dong <82224165@qq.com>
2016-07-08 23:56:43 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/SudokuSolver.java
+++ algorithm/src/org/dzhou/practice/hard/SudokuSolver.java
@@ -20,7 +20,25 @@ public class SudokuSolver {
 			helper(board);
 		}
 
-		private void helper(char[][] board) {
+		private boolean helper(char[][] board) {
+
+			for (int i = 0; i < board.length; i++) {
+				for (int j = 0; j < board[0].length; j++) {
+					if (board[i][j] != '.')
+						continue;
+					for (char c = '1'; c <= '9'; c++) {
+						if (!isValid(board, i, j, c))
+							continue;
+						board[i][j] = c;
+						if (helper(board))
+							return true;
+						board[i][j] = '.';
+					}
+					return false;
+				}
+			}
+
+			return true;
 		}
 
 		private boolean isValid(char[][] board, int i, int j, char val) {
