d124ee3d72030e5880020229398509e772960e64
zhou-dong <82224165@qq.com>
2016-07-08 22:54:53 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/hard/LargestRectangleInHistogram.java
@@ -0,0 +1,22 @@
+package org.dzhou.practice.hard;
+
+/**
+ * Given n non-negative integers representing the histogram's bar height where
+ * the width of each bar is 1, find the area of largest rectangle in the
+ * histogram.
+ * 
+ * @author zhoudong
+ *
+ */
+public class LargestRectangleInHistogram {
+
+	public class Solution {
+
+		public int largestRectangleArea(int[] heights) {
+
+			return 0;
+		}
+
+	}
+
+}
---
+++ algorithm/src/org/dzhou/practice/hard/SudokuSolver.java
@@ -0,0 +1,23 @@
+package org.dzhou.practice.hard;
+
+/**
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * 
+ * Empty cells are indicated by the character '.'.
+ * 
+ * You may assume that there will be only one unique solution.
+ * 
+ * @author zhoudong
+ *
+ */
+public class SudokuSolver {
+
+	public class Solution {
+
+		public void solveSudoku(char[][] board) {
+
+		}
+
+	}
+
+}
