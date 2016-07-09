cb859a90c017301cf256cfe8d66383a04e49e915
zhou-dong <82224165@qq.com>
2016-07-09 16:47:47 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/LowestCommonAncestorOfABinaryTree.java
+++ algorithm/src/org/dzhou/practice/medium/LowestCommonAncestorOfABinaryTree.java
@@ -6,6 +6,8 @@ package org.dzhou.practice.medium;
  * 
  * @author zhoudong
  *
+ *         对于二叉搜索树，公共祖先的值一定大于等于较小的节点，小于等于较大的节点。换言之，在遍历树的时候，如果当前结点大于两个节点，
+ *         则结果在当前结点的左子树里，如果当前结点小于两个节点，则结果在当前节点的右子树里。
  */
 public class LowestCommonAncestorOfABinaryTree {
 
@@ -23,8 +25,13 @@ public class LowestCommonAncestorOfABinaryTree {
 	public class Solution {
 
 		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-			return null;
+			if (root == null || root == p || root == q)
+				return root;
+			TreeNode left = lowestCommonAncestor(root.left, p, q);
+			TreeNode right = lowestCommonAncestor(root.right, p, q);
+			if (left != null && right != null)
+				return root;
+			return left == null ? right : left;
 		}
 
 	}
