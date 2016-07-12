7bbcb9f4e32fe1edbadd7d46f2489687a7ce54c3
zhou-dong <82224165@qq.com>
2016-07-12 16:44:51 -0500

    update

---
+++ algorithm/src/org/dzhou/research/cci/arrays/OneAway.java
@@ -0,0 +1,50 @@
+package org.dzhou.research.cci.arrays;
+
+/**
+ * There are three types of edits that can be performed on strings" insert a
+ * character, remove a character, or replace a character. Given two strings,
+ * writes a function to check if they are one edit (or zero edit) away.
+ * 
+ * @author zhoudong
+ *
+ */
+public class OneAway {
+
+	boolean oneEditAway(String first, String second) {
+		if (first.length() == second.length())
+			return oneEditReplace(first, second);
+		else if (first.length() + 1 == second.length())
+			return oneEditInsert(first, second);
+		else if (first.length() - 1 == second.length())
+			return oneEditInsert(second, first);
+		return false;
+	}
+
+	boolean oneEditReplace(String s1, String s2) {
+		boolean foundDifference = false;
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1.charAt(i) != s2.charAt(i)) {
+				if (foundDifference)
+					return false;
+				foundDifference = true;
+			}
+		}
+		return true;
+	}
+
+	boolean oneEditInsert(String s1, String s2) {
+		int index1 = 0, index2 = 0;
+		while (index1 < s1.length() && index2 < s2.length()) {
+			if (s1.charAt(index1) != s2.charAt(index2)) {
+				if (index1 != index2)
+					return false;
+				index2++;
+			} else {
+				index1++;
+				index2++;
+			}
+		}
+		return true;
+	}
+
+}
