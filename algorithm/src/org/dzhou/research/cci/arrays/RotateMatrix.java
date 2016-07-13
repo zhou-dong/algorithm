11df39b0b98d11c1f6febebb87e45aa7730b30b2
zhou-dong <82224165@qq.com>
2016-07-13 01:55:01 -0500

    update

---
+++ algorithm/src/org/dzhou/research/cci/arrays/RotateMatrix.java
@@ -0,0 +1,41 @@
+package org.dzhou.research.cci.arrays;
+
+/**
+ * Given an image represented by an NxN matrix, where each pixel in the image is
+ * 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in
+ * place?
+ * 
+ * @author zhoudong
+ *
+ *         swap index by index
+ *
+ */
+public class RotateMatrix {
+
+	public class Solution {
+
+		void rotate(int[][] matrix, int n) {
+
+			for (int layer = 0; layer < n / 2; layer++) {
+				int first = layer;
+				int last = n - 1 - layer;
+				for (int i = first; i < last; i++) {
+					int offset = i - first;
+					// save top
+					int top = matrix[first][i];
+					// left -> top
+					matrix[first][i] = matrix[last - offset][first];
+					// bottom -> left
+					matrix[last - offset][first] = matrix[last][last - offset];
+					// right -> bottom
+					matrix[last][last - offset] = matrix[i][last];
+					// top -> right
+					matrix[i][last] = top;
+				}
+			}
+
+		}
+
+	}
+
+}
