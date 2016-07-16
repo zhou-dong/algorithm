50f7e89fda47822aab1219c819a1cbc22f5f8637
zhou-dong <82224165@qq.com>
2016-07-16 17:35:07 -0500

    update

--- algorithm/src/org/dzhou/research/cci/treegraph/CheckBalanced.java
+++ algorithm/src/org/dzhou/research/cci/treegraph/CheckBalanced.java
@@ -12,18 +12,18 @@ package org.dzhou.research.cci.treegraph;
  */
 public class CheckBalanced {
 
-	class TreeNode<T> {
-		T value;
-		TreeNode<T> left;
-		TreeNode<T> right;
+	class TreeNode {
+		int value;
+		TreeNode left;
+		TreeNode right;
 
-		TreeNode(T value) {
+		TreeNode(int value) {
 			this.value = value;
 		}
 	}
 
-	public class Solution<T> {
-		boolean isBalanced(TreeNode<T> root) {
+	public class Solution {
+		boolean isBalanced(TreeNode root) {
 			if (root == null)
 				return true;
 			if (getHeightDiff(root) > 1)
@@ -31,14 +31,72 @@ public class CheckBalanced {
 			return isBalanced(root.left) && isBalanced(root.right);
 		}
 
-		private int getHeightDiff(TreeNode<T> root) {
+		private int getHeightDiff(TreeNode root) {
 			return Math.abs(getHeight(root.left) - getHeight(root.right));
 		}
 
-		int getHeight(TreeNode<T> root) {
+		int getHeight(TreeNode root) {
 			if (root == null)
 				return -1;
 			return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+		}
+	}
+
+	public class Solution1 {
+		boolean isBalanced(TreeNode root) {
+			return checkHeight(root) != Integer.MIN_VALUE;
+		}
+
+		int checkHeight(TreeNode root) {
+			if (root == null)
+				return -1;
+			int leftHeight = checkHeight(root.left);
+			if (leftHeight == Integer.MIN_VALUE)
+				return Integer.MIN_VALUE;
+			int rightHeight = checkHeight(root.right);
+			if (rightHeight == Integer.MIN_VALUE)
+				return Integer.MIN_VALUE;
+			if (checkHeightDiff(leftHeight, rightHeight) > 1)
+				return Integer.MIN_VALUE;
+			return Math.max(leftHeight, rightHeight) + 1;
+		}
+
+		private int checkHeightDiff(int leftHeight, int rightHeight) {
+			return Math.abs(leftHeight - rightHeight);
+		}
+	}
+
+	/**
+	 * This is error solution!
+	 * 
+	 * for example:<br>
+	 * Input: [1,null,3,2]<br>
+	 * Output: true<br>
+	 * Expected: false
+	 */
+	public class ErrorSolution {
+		public boolean isBalanced(TreeNode root) {
+			if (root == null)
+				return true;
+			if (getHeightDiff(root) > 1)
+				return false;
+			return isBalanced(root.left) && isBalanced(root.right);
+		}
+
+		private int getHeightDiff(TreeNode node) {
+			return Math.abs(getLeftHeight(node) - getRightHeight(node));
+		}
+
+		private int getLeftHeight(TreeNode node) {
+			if (node == null)
+				return -1;
+			return getLeftHeight(node.left) + 1;
+		}
+
+		private int getRightHeight(TreeNode node) {
+			if (node == null)
+				return -1;
+			return getRightHeight(node.right) + 1;
 		}
 	}
 
