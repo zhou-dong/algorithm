35cdee3547d44284d89b4fcc369cc676ddecf2cd
zhou-dong <82224165@qq.com>
2016-07-20 01:52:51 -0500

    update

---
+++ algorithm/src/org/dzhou/research/cci/dynamicprogramming/TripleStep.java
@@ -0,0 +1,20 @@
+package org.dzhou.research.cci.dynamicprogramming;
+
+/**
+ * Practice of "cracking the code interview"
+ * 
+ * A child is running up a staircase with n steps and can hop either 1 step, 2
+ * steps, or 3 steps at a time. Implement a method to count how many possible
+ * ways the child can run up the stairs.
+ * 
+ * @author DONG ZHOU
+ *
+ */
+public class TripleStep {
+
+	public int countWays(int n) {
+
+		return 0;
+	}
+
+}
