fe14991391a24e8bc94517136ef660817b8653c1
zhou-dong <82224165@qq.com>
2016-07-21 00:15:38 -0500

    update

--- algorithm/src/org/dzhou/research/cci/dynamicprogramming/RobotInGrid.java
+++ algorithm/src/org/dzhou/research/cci/dynamicprogramming/RobotInGrid.java
@@ -1,7 +1,11 @@
 package org.dzhou.research.cci.dynamicprogramming;
 
 import java.awt.Point;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Practice of "cracking the code interview"
@@ -17,9 +21,82 @@ import java.util.List;
  */
 public class RobotInGrid {
 
-	List<Point> getPath(boolean[][] maze) {
+	class Solution {
+		List<Point> getPath(boolean[][] maze) {
+			if (maze == null || maze.length == 0 || maze[0].length == 0)
+				return Collections.emptyList();
+			List<Point> path = new LinkedList<>();
+			Map<Point, Boolean> cache = new HashMap<>();
+			if (getPath(maze, path, maze.length - 1, maze[0].length - 1, cache))
+				return path;
+			return Collections.emptyList();
+		}
 
-		return null;
+		private boolean getPath(boolean[][] maze, List<Point> path, int row, int col, Map<Point, Boolean> cache) {
+			if (row < 0 || col < 0 || maze[row][col] == false)
+				return false;
+			Point p = new Point(row, col);
+			if (cache.containsKey(p))
+				return cache.get(p);
+			boolean isAtOrigin = (row == 0) && (col == 0);
+			boolean success = false;
+			if (isAtOrigin || getPath(maze, path, row, col - 1, cache) || getPath(maze, path, row - 1, col, cache)) {
+				path.add(new Point(row, col));
+				success = true;
+			}
+			cache.put(p, success);
+			return success;
+		}
+	}
+
+	class Solution1 {
+		List<Point> getPath(boolean[][] maze) {
+			if (maze == null || maze.length == 0 || maze[0].length == 0)
+				return Collections.emptyList();
+			List<Point> path = new LinkedList<>();
+			if (getPath(maze, path, maze.length - 1, maze[0].length - 1))
+				return path;
+			return Collections.emptyList();
+		}
+
+		private boolean getPath(boolean[][] maze, List<Point> path, int row, int col) {
+			if (row < 0 || col < 0 || maze[row][col] == false)
+				return false;
+			boolean isAtOrigin = (row == 0) && (col == 0);
+			// 1. 先add最后一个point(0,0)
+			// 2. 回溯，如果是return true，则把point加入到path中
+			if (isAtOrigin || getPath(maze, path, row, col - 1) || getPath(maze, path, row - 1, col)) {
+				path.add(new Point(row, col));
+				return true;
+			}
+			return false;
+		}
+	}
+
+	class Solution2 {
+		List<Point> getPath(boolean[][] maze) {
+			if (maze == null || maze.length == 0 || maze[0].length == 0)
+				return Collections.emptyList();
+			List<List<Point>> paths = new LinkedList<>();
+			getPaths(maze, new LinkedList<>(), paths, maze.length - 1, maze[0].length - 1);
+			if (paths.size() == 0)
+				return Collections.emptyList();
+			return paths.get(0);
+		}
+
+		private void getPaths(boolean[][] maze, List<Point> path, List<List<Point>> paths, int row, int col) {
+			if (row < 0 || col < 0 || maze[row][col] == false)
+				return;
+			if (row == 0 && col == 0) {
+				paths.add(new LinkedList<>(path));
+				return;
+			}
+			Point point = new Point(row, col);
+			path.add(point);
+			getPaths(maze, path, paths, row - 1, col);
+			getPaths(maze, path, paths, row, col - 1);
+			path.remove(point);
+		}
 	}
 
 }
