df043cf7c831d18d164e7b6e7571849fba88b468
zhou-dong <82224165@qq.com>
2016-07-24 04:00:40 -0500

    update

--- algorithm/src/org/dzhou/again/regularexpressionmatching/RegularExpressionMatching.java
+++ algorithm/src/org/dzhou/again/regularexpressionmatching/RegularExpressionMatching.java
@@ -20,7 +20,6 @@ package org.dzhou.again.regularexpressionmatching;
  * isMatch("ab", ".*") → true<br>
  * isMatch("aab", "c*a*b") → true
  * 
- *
  * references:
  * 
  * http://articles.leetcode.com/regular-expression-matching
@@ -28,7 +27,6 @@ package org.dzhou.again.regularexpressionmatching;
  * http://harrifeng.github.io/algo/leetcode/regular-expression-matching.html
  * 
  * @author zhoudong
- * 
  * 
  *         "匹配"这个问题,非常容易转换成"匹配了一部分",整个匹配不匹配,要看"剩下的匹配"情况.<br>
  *         这就很好的把一个大的问题转换成了规模较小的问题:递归
@@ -39,19 +37,23 @@ public class RegularExpressionMatching {
 	public class Solution {
 
 		public boolean isMatch(String s, String p) {
+
 			// recursive return condition
 			if (p.length() == 0)
 				return s.length() == 0;
 			if (p.length() == 1)
 				return s.length() == 1 && isMatch(s, p, 0);
-			// main part
-			if (p.charAt(1) != '*') { // normal compare
+
+			if (p.charAt(1) != '*') // normal compare
 				return s.length() > 0 && isMatch(s, p, 0) && isMatch(s.substring(1), p.substring(1));
-			} else {
+
+			// next char is *
+			while (s.length() > 0 && isMatch(s, p, 0)) {
 				if (isMatch(s, p.substring(2)))
 					return true;
-				return s.length() > 0 && isMatch(s, p, 0) && isMatch(s.substring(1), p);
+				s = s.substring(1);
 			}
+			return isMatch(s, p.substring(2));
 		}
 
 		private boolean isMatch(String s, String p, int index) {
