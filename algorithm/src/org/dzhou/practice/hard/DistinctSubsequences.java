f8a3b2944c058c27d18b4d3d685251a6882729e2
zhou-dong <82224165@qq.com>
2016-07-31 23:33:28 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/DistinctSubsequences.java
+++ algorithm/src/org/dzhou/practice/hard/DistinctSubsequences.java
@@ -13,8 +13,22 @@ package org.dzhou.practice.hard;
  * 
  * Return 3.
  * 
+ * 
  * @author zhoudong
  *
+ *         “When you see string problem that is about subsequence or matching,
+ *         dynamic programming method should come to your mind naturally. ”
+ * 
+ *         reference: http://blog.csdn.net/abcbc/article/details/8978146
+ * 
+ *         - - r a b b b i t <br>
+ *         - 1 1 1 1 1 1 1 1 <br>
+ *         r 0 1 1 1 1 1 1 1 <br>
+ *         a 0 0 1 1 1 1 1 1 <br>
+ *         b 0 0 0 1 2 3 3 3 <br>
+ *         b 0 0 0 0 1 3 3 3 <br>
+ *         i 0 0 0 0 0 0 3 3 <br>
+ *         t 0 0 0 0 0 0 0 3 <br>
  */
 public class DistinctSubsequences {
 
