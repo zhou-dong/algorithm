7dd09ba1c1013e0589118169cbc417877ff8775c
zhou-dong <82224165@qq.com>
2016-08-01 03:12:08 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/SubstringWithConcatenationOfAllWords.java
+++ algorithm/src/org/dzhou/practice/hard/SubstringWithConcatenationOfAllWords.java
@@ -1,7 +1,10 @@
 package org.dzhou.practice.hard;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * You are given a string, s, and a list of words, words, that are all of the
@@ -23,17 +26,64 @@ public class SubstringWithConcatenationOfAllWords {
 	public class Solution {
 
 		public List<Integer> findSubstring(String s, String[] words) {
-
 			if (!isInputValid(s, words))
 				return Collections.emptyList();
+			Map<String, Integer> wordsWithCount = wordsWithCount(words);
+			return findSubstring(s, wordsWithCount, words[0].length());
+		}
 
-			return null;
+		private List<Integer> findSubstring(String s, Map<String, Integer> wordsWithCount, int wordLength) {
+			List<Integer> result = new ArrayList<>();
+			for (int i = 0; i < s.length(); i++) {
+				String str = s.substring(i);
+				if (isSubString(0, wordLength, new HashMap<>(wordsWithCount), str)) {
+					result.add(i);
+				}
+			}
+			return result;
+		}
+
+		private boolean isSubString(int start, int wordLength, Map<String, Integer> words, String str) {
+			while (start + wordLength <= str.length()) {
+				String current = str.substring(start, start + wordLength);
+				if (!words.containsKey(current))
+					return false;
+				if (words.get(current) == 1)
+					words.remove(current);
+				else
+					words.put(current, words.get(current) - 1);
+				if (words.size() == 0)
+					return true;
+				start += wordLength;
+			}
+			return false;
+		}
+
+		private Map<String, Integer> wordsWithCount(String[] words) {
+			Map<String, Integer> map = new HashMap<>();
+			for (String word : words) {
+				if (map.containsKey(word))
+					map.put(word, map.get(word) + 1);
+				else
+					map.put(word, 1);
+			}
+			return map;
 		}
 
 		private boolean isInputValid(String s, String words[]) {
 			return s != null && words != null && s.length() > 0 && words.length > 0;
 		}
 
+	}
+
+	private void test(String str, String[] words) {
+		Solution s = new Solution();
+		System.out.println(s.findSubstring(str, words));
+	}
+
+	public static void main(String[] args) {
+		SubstringWithConcatenationOfAllWords s = new SubstringWithConcatenationOfAllWords();
+		s.test("wordgoodgoodgoodbestword", new String[] { "word", "good", "best", "good" });
 	}
 
 }
