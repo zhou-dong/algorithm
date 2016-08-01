92f16538a70baba8c9268e4b268180db43cdaa7e
zhou-dong <82224165@qq.com>
2016-07-31 22:55:47 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/DistinctSubsequences.java
+++ algorithm/src/org/dzhou/practice/hard/DistinctSubsequences.java
@@ -1,10 +1,30 @@
 package org.dzhou.practice.hard;
 
 /**
+ * Given a string S and a string T, count the number of distinct subsequences of
+ * T in S.
+ * 
+ * A subsequence of a string is a new string which is formed from the original
+ * string by deleting some (can be none) of the characters without disturbing
+ * the relative positions of the remaining characters. (ie, "ACE" is a
+ * subsequence of "ABCDE" while "AEC" is not).
+ * 
+ * Here is an example: S = "rabbbit", T = "rabbit"
+ * 
+ * Return 3.
  * 
  * @author zhoudong
  *
  */
 public class DistinctSubsequences {
+
+	public class Solution {
+
+		public int numDistinct(String s, String t) {
+
+			return 0;
+		}
+
+	}
 
 }
