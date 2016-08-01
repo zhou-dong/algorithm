f7842ea2b675bb107f77d2bc2514a074f4181a09
zhou-dong <82224165@qq.com>
2016-08-01 16:42:34 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/hard/LongestConsecutiveSequence.java
@@ -0,0 +1,25 @@
+package org.dzhou.practice.hard;
+
+/**
+ * Given an unsorted array of integers, find the length of the longest
+ * consecutive elements sequence.
+ * 
+ * For example, <br>
+ * Given [100, 4, 200, 1, 3, 2], <br>
+ * The longest consecutive elements sequence is [1, 2, 3, 4]. <br>
+ * Return its length: 4.
+ * 
+ * Your algorithm should run in O(n) complexity.
+ * 
+ * @author zhoudong
+ *
+ */
+public class LongestConsecutiveSequence {
+
+	public class Solution {
+		public int longestConsecutive(int[] nums) {
+
+			return 0;
+		}
+	}
+}
