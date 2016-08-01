5b7e21b789e7a0436eac1cb5112157da06bcae0b
zhou-dong <82224165@qq.com>
2016-08-01 03:27:11 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/SubstringWithConcatenationOfAllWords.java
+++ algorithm/src/org/dzhou/practice/hard/SubstringWithConcatenationOfAllWords.java
@@ -86,4 +86,41 @@ public class SubstringWithConcatenationOfAllWords {
 		s.test("wordgoodgoodgoodbestword", new String[] { "word", "good", "best", "good" });
 	}
 
+	public class Solution1 {
+
+		public List<Integer> findSubstring(String S, String[] L) {
+			List<Integer> result = new ArrayList<Integer>();
+			HashMap<String, Integer> toFind = new HashMap<String, Integer>();
+			HashMap<String, Integer> found = new HashMap<String, Integer>();
+			int m = L.length, n = L[0].length();
+			for (int i = 0; i < m; i++) {
+				if (!toFind.containsKey(L[i])) {
+					toFind.put(L[i], 1);
+				} else {
+					toFind.put(L[i], toFind.get(L[i]) + 1);
+				}
+			}
+			for (int i = 0; i <= S.length() - n * m; i++) {
+				found.clear();
+				int j;
+				for (j = 0; j < m; j++) {
+					int k = i + j * n;
+					String stub = S.substring(k, k + n);
+					if (!toFind.containsKey(stub))
+						break;
+					if (!found.containsKey(stub)) {
+						found.put(stub, 1);
+					} else {
+						found.put(stub, found.get(stub) + 1);
+					}
+					if (found.get(stub) > toFind.get(stub))
+						break;
+				}
+				if (j == m)
+					result.add(i);
+			}
+			return result;
+		}
+	}
+
 }
