f04d91474b578cc21b90122146652d45a56bcfa1
zhou-dong <82224165@qq.com>
2016-08-03 00:01:36 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/LargestRectangleInHistogram.java
+++ algorithm/src/org/dzhou/practice/hard/LargestRectangleInHistogram.java
@@ -1,5 +1,8 @@
 package org.dzhou.practice.hard;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * Given n non-negative integers representing the histogram's bar height where
  * the width of each bar is 1, find the area of largest rectangle in the
@@ -14,9 +17,46 @@ public class LargestRectangleInHistogram {
 
 	public class Solution {
 
-		public int largestRectangleArea(int[] heights) {
+		public int largestRectangleArea(int[] height) {
+			int area = 0;
+			Stack<Integer> stack = new Stack<Integer>();
+			for (int i = 0; i < height.length; i++) {
+				if (stack.empty() || height[stack.peek()] < height[i]) {
+					stack.push(i);
+				} else {
+					int start = stack.pop();
+					int width = stack.empty() ? i : i - stack.peek() - 1;
+					area = Math.max(area, height[start] * width);
+					i--;
+				}
+			}
+			while (!stack.empty()) {
+				int start = stack.pop();
+				int width = stack.empty() ? height.length : height.length - stack.peek() - 1;
+				area = Math.max(area, height[start] * width);
+			}
+			return area;
+		}
 
-			return 0;
+	}
+
+	public class Solution1 {
+
+		public int largestRectangleArea(int[] height) {
+			Stack<Integer> stack = new Stack<Integer>();
+			int i = 0;
+			int maxArea = 0;
+			int[] h = new int[height.length + 1];
+			h = Arrays.copyOf(height, height.length + 1);
+			while (i < h.length) {
+				if (stack.isEmpty() || h[stack.peek()] <= h[i]) {
+					stack.push(i++);
+				} else {
+					int t = stack.pop();
+					maxArea = Math.max(maxArea, h[t] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+				}
+			}
+			return maxArea;
 		}
 
 	}
