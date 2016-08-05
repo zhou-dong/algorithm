165272251b4e8af0806c0d67da8d4ce255698d84
zhou-dong <82224165@qq.com>
2016-08-04 22:43:22 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/hard/CopyListWithRandomPointer.java
@@ -0,0 +1,77 @@
+package org.dzhou.practice.hard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Copy List with Random Pointer
+ * 
+ * A linked list is given such that each node contains an additional random
+ * pointer which could point to any node in the list or null.
+ * 
+ * Return a deep copy of the list.
+ * 
+ * 
+ * @author zhoudong
+ *
+ */
+public class CopyListWithRandomPointer {
+
+	// Definition for singly-linked list with a random pointer.
+	class RandomListNode {
+		int label;
+		RandomListNode next, random;
+
+		RandomListNode(int x) {
+			this.label = x;
+		}
+	};
+
+	// Solution with hashMap
+	public class Solution {
+		public RandomListNode copyRandomList(RandomListNode head) {
+			if (head == null)
+				return null;
+			Map<RandomListNode, RandomListNode> map = createMap(head);
+			addRelation(head, map);
+			return map.get(head);
+		}
+
+		private Map<RandomListNode, RandomListNode> createMap(RandomListNode head) {
+			Map<RandomListNode, RandomListNode> map = new HashMap<>();
+			while (head != null) {
+				map.put(head, new RandomListNode(head.label));
+				head = head.next;
+			}
+			return map;
+		}
+
+		private void addRelation(RandomListNode head, Map<RandomListNode, RandomListNode> map) {
+			while (head != null) {
+				if (head.next != null)
+					map.get(head).next = map.get(head.next);
+				if (head.random != null)
+					map.get(head).random = map.get(head.random);
+				head = head.next;
+			}
+		}
+	}
+
+	void test() {
+		Solution s = new Solution();
+		RandomListNode head = new RandomListNode(-1);
+		head.next = new RandomListNode(-1);
+		RandomListNode curr = s.copyRandomList(head);
+		while (curr != null) {
+			System.out.println(curr.label);
+			curr = curr.next;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		CopyListWithRandomPointer c = new CopyListWithRandomPointer();
+		c.test();
+	}
+
+}
