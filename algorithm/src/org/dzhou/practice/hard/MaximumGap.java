c928ccbb3a7a21694755bedc71214b679d103a42
zhou-dong <82224165@qq.com>
2016-08-05 04:28:24 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/MaximumGap.java
+++ algorithm/src/org/dzhou/practice/hard/MaximumGap.java
@@ -49,6 +49,13 @@ public class MaximumGap {
 			return findMaxGap(buckets);
 		}
 
+		private Bucket[] createBuckets(int length) {
+			Bucket[] buckets = new Bucket[length];
+			for (int i = 0; i < buckets.length; i++)
+				buckets[i] = new Bucket();
+			return buckets;
+		}
+
 		private void insertNumsIntoBuckets(Bucket[] buckets, int[] nums) {
 			int min = findMin(nums);
 			int max = findMax(nums);
@@ -84,13 +91,6 @@ public class MaximumGap {
 				prevHigh = buckets[index].high;
 			}
 			return result;
-		}
-
-		private Bucket[] createBuckets(int length) {
-			Bucket[] buckets = new Bucket[length];
-			for (int i = 0; i < buckets.length; i++)
-				buckets[i] = new Bucket();
-			return buckets;
 		}
 
 		private int findMin(int[] nums) {
