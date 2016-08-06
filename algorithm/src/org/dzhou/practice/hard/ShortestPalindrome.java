8fcf967591c6ce0299f6dbce0ba4466440abe4d6
zhou-dong <82224165@qq.com>
2016-08-06 00:13:39 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/hard/ShortestPalindrome.java
@@ -0,0 +1,25 @@
+package org.dzhou.practice.hard;
+
+/**
+ * Given a string S, you are allowed to convert it to a palindrome by adding
+ * characters in front of it. Find and return the shortest palindrome you can
+ * find by performing this transformation.
+ * 
+ * For example:
+ * 
+ * Given "aacecaaa", return "aaacecaaa".
+ * 
+ * Given "abcd", return "dcbabcd".
+ * 
+ * @author zhoudong
+ *
+ */
+public class ShortestPalindrome {
+
+	public class Solution {
+		public String shortestPalindrome(String s) {
+
+			return null;
+		}
+	}
+}
