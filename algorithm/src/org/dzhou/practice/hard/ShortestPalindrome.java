d32ddf6f0280a76d8f00f6adc1c69094daa150be
zhou-dong <82224165@qq.com>
2016-08-06 20:11:32 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/ShortestPalindrome.java
+++ algorithm/src/org/dzhou/practice/hard/ShortestPalindrome.java
@@ -28,8 +28,48 @@ public class ShortestPalindrome {
 	}
 
 	void test() {
-		Solution1 s = new Solution1();
-		System.out.println(s.shortestPalindrome("aacecaaa"));
+		Solution sl = new Solution();
+		System.out.println(sl.shortestPalindrome("aacecaaa"));
+	}
+
+	public class Solution {
+		public String shortestPalindrome(String s) {
+			if (s == null || s.length() == 0)
+				return "";
+			if (isPalindrome(s))
+				return s;
+			String reverse = new StringBuilder(s).reverse().toString();
+			int[] kmpTable = kmpPartialTable(s + "#" + reverse);
+			return reverse.substring(0, s.length() - kmpTable[kmpTable.length - 1]) + s;
+		}
+
+		private int[] kmpPartialTable(String s) {
+			int[] table = new int[s.length()];
+			for (int i = 1; i < s.length(); i++) {
+
+				int j = table[i - 1];
+
+				while (j > 0 && s.charAt(i) != s.charAt(j))
+					j = table[j - 1];
+				if (s.charAt(i) == s.charAt(j)) {
+					table[i] = j + 1;
+				}
+			}
+			return table;
+		}
+
+		private boolean isPalindrome(String s) {
+			if (s.length() < 2)
+				return true;
+			int low = 0, high = s.length() - 1;
+			while (low < high) {
+				if (s.charAt(low) != s.charAt(high))
+					return false;
+				low++;
+				high--;
+			}
+			return true;
+		}
 	}
 
 	public class Solution1 {
@@ -37,9 +77,54 @@ public class ShortestPalindrome {
 		public String shortestPalindrome(String s) {
 			if (s == null || s.length() == 0)
 				return "";
-			return null;
+			if (isPalindrome(s))
+				return s;
+			String reverse = new StringBuilder(s).reverse().toString();
+			String join = s + "#" + reverse;
+			Set<String> properPrefix = properPrefix(join);
+			Set<String> properSuffix = properSuffix(join);
+			int end = longestSamePrefixSuffix(properPrefix, properSuffix);
+			return reverse.substring(0, s.length() - end) + s;
 		}
 
+		private int longestSamePrefixSuffix(Set<String> prefix, Set<String> suffix) {
+			int max = 0;
+			for (String item : prefix) {
+				if (!suffix.contains(item))
+					continue;
+				max = Math.max(max, item.length());
+			}
+			return max;
+		}
+
+		private Set<String> properPrefix(String s) {
+			Set<String> result = new HashSet<>();
+			for (int i = 0; i < s.length() - 1; i++) {
+				result.add(s.substring(0, i + 1));
+			}
+			return result;
+		}
+
+		private Set<String> properSuffix(String s) {
+			Set<String> result = new HashSet<>();
+			for (int i = 1; i < s.length(); i++) {
+				result.add(s.substring(i));
+			}
+			return result;
+		}
+
+		private boolean isPalindrome(String s) {
+			if (s.length() < 2)
+				return true;
+			int low = 0, high = s.length() - 1;
+			while (low < high) {
+				if (s.charAt(low) != s.charAt(high))
+					return false;
+				low++;
+				high--;
+			}
+			return true;
+		}
 	}
 
 }
