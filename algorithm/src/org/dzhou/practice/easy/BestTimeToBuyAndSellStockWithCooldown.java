11801f7acb32e4714c30b66cfcbd2c8461c53944
zhou-dong <82224165@qq.com>
2016-08-07 20:51:09 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/BestTimeToBuyAndSellStockWithCooldown.java
+++ algorithm/src/org/dzhou/practice/easy/BestTimeToBuyAndSellStockWithCooldown.java
@@ -16,9 +16,41 @@ package org.dzhou.practice.easy;
  * 
  * @author zhoudong
  *
+ *         reference:
+ * 
+ *         https://www.hrwhisper.me/leetcode-best-time-to-buy-and-sell-stock-
+ *         with-cooldown/
+ * 
+ *         状态转移方程:
+ * 
+ *         buy[i] = max(buy[i-1] , sell[i-2] – prices[i])
+ * 
+ *         sell[i] = max(sell[i-1], buy[i-1] + prices[i])
  */
 public class BestTimeToBuyAndSellStockWithCooldown {
 
-	private void todo() {
+	public class Solution {
+
+		public int maxProfit(int[] prices) {
+
+			if (prices == null || prices.length < 2)
+				return 0;
+
+			int[] buy = new int[prices.length];
+			int[] sell = new int[prices.length];
+
+			buy[0] = -prices[0];
+			buy[1] = Math.max(-prices[0], -prices[1]);
+			sell[1] = Math.max(0, prices[1] - prices[0]);
+
+			for (int i = 2; i < prices.length; i++) {
+				buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);
+				sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+			}
+
+			return sell[prices.length - 1];
+		}
+
 	}
+
 }
