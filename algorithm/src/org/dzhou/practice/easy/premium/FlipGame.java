4bed53dff6f841704ab52b78e3432c0433c2be15
zhou-dong <82224165@qq.com>
2016-08-09 05:38:45 -0500

    update

--- algorithm/src/org/dzhou/practice/easy/premium/FlipGame.java
+++ algorithm/src/org/dzhou/practice/easy/premium/FlipGame.java
@@ -28,15 +28,20 @@ import java.util.List;
 public class FlipGame {
 
 	public class Solution {
+
 		public List<String> generatePossibleNextMoves(String s) {
+
 			List<String> result = new ArrayList<>();
+
 			for (int i = 0; i < s.length() - 1; i++) {
 				if (s.charAt(i) == '-' || s.charAt(i + 1) == '-')
 					continue;
 				result.add(s.substring(0, i) + "--" + s.substring(i + 2));
 			}
+
 			return result;
 		}
+
 	}
 
 }
