3767f5f8f7ce1fa6410b84de4f88edec5a69425e
zhou-dong <82224165@qq.com>
2016-08-10 07:14:15 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/premium/ShortestWordDistanceIII.java
+++ algorithm/src/org/dzhou/practice/medium/premium/ShortestWordDistanceIII.java
@@ -52,4 +52,55 @@ public class ShortestWordDistanceIII {
 
 	}
 
+	public class Solution1 {
+
+		public int shortestWordDistance(String[] words, String word1, String word2) {
+			if (word1.equals(word2))
+				return sameWord(words, word1);
+			else
+				return differentWords(words, word1, word2);
+		}
+
+		private int differentWords(String[] words, String word1, String word2) {
+			int index1 = -1, index2 = -1;
+			int min = words.length;
+			for (int i = 0; i < words.length; i++) {
+				String word = words[i];
+				if (word.equals(word1))
+					index1 = i;
+				else if (word.equals(word2))
+					index2 = i;
+				else
+					continue;
+				min = updateMin(min, index1, index2);
+			}
+			return min;
+
+		}
+
+		private int sameWord(String[] words, String word) {
+			int index1 = -1, index2 = -1;
+			int min = words.length;
+			for (int i = 0; i < words.length; i++) {
+				if (!word.equals(words[i]))
+					continue;
+				if (index1 > index2)
+					index2 = i;
+				else
+					index1 = i;
+				min = updateMin(min, index1, index2);
+			}
+			return min;
+		}
+
+		private int updateMin(int min, int index1, int index2) {
+			if (index1 == -1 || index2 == -1)
+				return min;
+			return min(min, index1, index2);
+		}
+
+		private int min(int min, int index1, int index2) {
+			return Math.min(min, Math.abs(index1 - index2));
+		}
+	}
 }
