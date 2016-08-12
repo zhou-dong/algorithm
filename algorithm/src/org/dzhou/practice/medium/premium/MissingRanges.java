0b921b47ad2113b1a5f603cccbfa72b9b5cddee4
zhou-dong <82224165@qq.com>
2016-08-12 15:54:16 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/premium/MissingRanges.java
+++ algorithm/src/org/dzhou/practice/medium/premium/MissingRanges.java
@@ -20,30 +20,29 @@ public class MissingRanges {
 	public class Solution {
 
 		public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-
 			List<String> result = new LinkedList<String>();
-
+			// edge check
 			if (nums == null || nums.length == 0) {
 				result.add(range(lower, upper));
 				return result;
 			}
-
+			// lower than nums[0]
 			if (lower < nums[0])
 				result.add(range(lower, nums[0] - 1));
-
+			// from nums[0]
 			for (int i = 0; i < nums.length; i++) {
 				int to = (i == nums.length - 1) ? upper : nums[i + 1] - 1;
 				if (nums[i] < to) {
 					result.add(range(nums[i] + 1, to));
 				}
 			}
-
 			return result;
 		}
 
 		private String range(int from, int to) {
 			return from == to ? String.valueOf(from) : from + "->" + to;
 		}
+
 	}
 
 }
