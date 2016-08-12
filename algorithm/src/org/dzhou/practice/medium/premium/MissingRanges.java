7976acc215dc17fb533cf6c26769e04e88bcd49c
zhou-dong <82224165@qq.com>
2016-08-12 15:48:27 -0500

    update

---
+++ algorithm/src/org/dzhou/practice/medium/premium/MissingRanges.java
@@ -0,0 +1,49 @@
+package org.dzhou.practice.medium.premium;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Given a sorted integer array where the range of elements are [lower, upper]
+ * inclusive, return its missing ranges.
+ * 
+ * For example, <br>
+ * given [0, 1, 3, 50, 75], <br>
+ * lower = 0 and upper = 99,<br>
+ * return ["2", "4->49", "51->74", "76->99"].
+ * 
+ * @author zhoudong
+ *
+ */
+public class MissingRanges {
+
+	public class Solution {
+
+		public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+
+			List<String> result = new LinkedList<String>();
+
+			if (nums == null || nums.length == 0) {
+				result.add(range(lower, upper));
+				return result;
+			}
+
+			if (lower < nums[0])
+				result.add(range(lower, nums[0] - 1));
+
+			for (int i = 0; i < nums.length; i++) {
+				int to = (i == nums.length - 1) ? upper : nums[i + 1] - 1;
+				if (nums[i] < to) {
+					result.add(range(nums[i] + 1, to));
+				}
+			}
+
+			return result;
+		}
+
+		private String range(int from, int to) {
+			return from == to ? String.valueOf(from) : from + "->" + to;
+		}
+	}
+
+}
