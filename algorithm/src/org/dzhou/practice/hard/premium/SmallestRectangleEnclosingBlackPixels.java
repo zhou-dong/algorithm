422f763b70594a56c1c2ba1f61ecf11e67a96491
zhou-dong <82224165@qq.com>
2016-08-15 02:18:48 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/premium/SmallestRectangleEnclosingBlackPixels.java
+++ algorithm/src/org/dzhou/practice/hard/premium/SmallestRectangleEnclosingBlackPixels.java
@@ -32,28 +32,49 @@ public class SmallestRectangleEnclosingBlackPixels {
 	void test() {
 		char[] one = new char[] { '0', '0', '1', '0' };
 		char[] two = new char[] { '0', '1', '1', '0' };
-		char[] thr = new char[] { '0', '1', '0', '0' };
+		char[] thr = new char[] { '1', '1', '0', '0' };
 		int area = new Solution().minArea(new char[][] { one, two, thr }, 0, 2);
 		System.out.println(area);
 	}
 
 	public class Solution {
-
 		public int minArea(char[][] image, int x, int y) {
+			if (image == null || image.length == 0) {
+				return 0;
+			}
+			int rowNum = image.length, colNum = image[0].length;
+			int left = binarySearch(image, 0, y, 0, rowNum, true, true);
+			int right = binarySearch(image, y + 1, colNum, 0, rowNum, true, false);
+			int top = binarySearch(image, 0, x, left, right, false, true);
+			int bot = binarySearch(image, x + 1, rowNum, left, right, false, false);
 
-			int top = 0;
-			int bottom = 0;
-			int left = 0;
-			int right = 0;
+			return (right - left) * (bot - top);
+		}
 
-			return (right - left + 1) * (bottom - top + 1);
+		private int binarySearch(char[][] image, int lo, int hi, int min, int max, boolean searchHorizontal,
+				boolean searchLo) {
+			while (lo < hi) {
+				int mid = lo + (hi - lo) / 2;
+				boolean hasBlackPixel = false;
+				for (int i = min; i < max; i++) {
+					if ((searchHorizontal ? image[i][mid] : image[mid][i]) == '1') {
+						hasBlackPixel = true;
+						break;
+					}
+				}
+				if (hasBlackPixel == searchLo) {
+					hi = mid;
+				} else {
+					lo = mid + 1;
+				}
+			}
+			return lo;
 		}
 
 	}
 
 	// Time Limit Exceeded
 	public class Solution1 {
-
 		class Point {
 			int row;
 			int col;
@@ -67,43 +88,38 @@ public class SmallestRectangleEnclosingBlackPixels {
 		public int minArea(char[][] image, int x, int y) {
 			int top = x, bottom = x;
 			int left = y, right = y;
-
-			boolean[][] visited = new boolean[image.length][image[0].length];
 			Queue<Point> queue = new LinkedList<>();
 			queue.add(new Point(x, y));
-
 			while (!queue.isEmpty()) {
 				Point current = queue.poll();
-				visited[current.row][current.col] = true;
+				image[current.row][current.col] = 0;
 				top = Math.min(top, current.row);
 				bottom = Math.max(bottom, current.row);
 				left = Math.min(left, current.col);
 				right = Math.max(right, current.col);
-				addPointToQueue(queue, image, new Point(current.row - 1, current.col), visited);
-				addPointToQueue(queue, image, new Point(current.row + 1, current.col), visited);
-				addPointToQueue(queue, image, new Point(current.row, current.col - 1), visited);
-				addPointToQueue(queue, image, new Point(current.row, current.col + 1), visited);
+				addPointToQueue(queue, image, new Point(current.row - 1, current.col));
+				addPointToQueue(queue, image, new Point(current.row + 1, current.col));
+				addPointToQueue(queue, image, new Point(current.row, current.col - 1));
+				addPointToQueue(queue, image, new Point(current.row, current.col + 1));
 			}
-
 			return (right - left + 1) * (bottom - top + 1);
 		}
 
-		private void addPointToQueue(Queue<Point> queue, char[][] image, Point point, boolean[][] visited) {
-			if (isValid(image, point, visited)) {
+		private void addPointToQueue(Queue<Point> queue, char[][] image, Point point) {
+			if (isValid(image, point)) {
 				queue.add(point);
 			}
 		}
 
-		private boolean isValid(char[][] image, Point point, boolean[][] visited) {
-			return isValid(image, point.row, point.col, visited);
+		private boolean isValid(char[][] image, Point point) {
+			return isValid(image, point.row, point.col);
 		}
 
-		private boolean isValid(char[][] image, int row, int col, boolean[][] visited) {
+		private boolean isValid(char[][] image, int row, int col) {
 			if (row < 0 || col < 0 || row >= image.length || col >= image[row].length)
 				return false;
-			return visited[row][col] == false && image[row][col] == '1';
+			return image[row][col] == '1';
 		}
-
 	}
 
 }
