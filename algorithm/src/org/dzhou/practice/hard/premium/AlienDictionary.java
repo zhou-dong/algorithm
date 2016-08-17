c30d5fedb84f6b60ef56913105e59c06ccbe0bbf
zhou-dong <82224165@qq.com>
2016-08-17 14:00:24 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/premium/AlienDictionary.java
+++ algorithm/src/org/dzhou/practice/hard/premium/AlienDictionary.java
@@ -2,7 +2,9 @@ package org.dzhou.practice.hard.premium;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 
@@ -38,9 +40,75 @@ public class AlienDictionary {
 	public class Solution {
 
 		public String alienOrder(String[] words) {
+			Map<Character, Set<Character>> map = new HashMap<Character, Set<Character>>();
+			Map<Character, Integer> degree = new HashMap<Character, Integer>();
+			String result = "";
+			if (words == null || words.length == 0)
+				return result;
+			for (String s : words) {
+				for (char c : s.toCharArray()) {
+					degree.put(c, 0);
+				}
+			}
+			for (int i = 0; i < words.length - 1; i++) {
+				String cur = words[i];
+				String next = words[i + 1];
+				int length = Math.min(cur.length(), next.length());
+				for (int j = 0; j < length; j++) {
+					char c1 = cur.charAt(j);
+					char c2 = next.charAt(j);
+					if (c1 != c2) {
+						Set<Character> set = new HashSet<Character>();
+						if (map.containsKey(c1))
+							set = map.get(c1);
+						if (!set.contains(c2)) {
+							set.add(c2);
+							map.put(c1, set);
+							degree.put(c2, degree.get(c2) + 1);
+						}
+						break;
+					}
+				}
+			}
+			Queue<Character> q = new LinkedList<Character>();
+			for (char c : degree.keySet()) {
+				if (degree.get(c) == 0)
+					q.add(c);
+			}
+			while (!q.isEmpty()) {
+				char c = q.remove();
+				result += c;
+				if (map.containsKey(c)) {
+					for (char c2 : map.get(c)) {
+						degree.put(c2, degree.get(c2) - 1);
+						if (degree.get(c2) == 0)
+							q.add(c2);
+					}
+				}
+			}
+			if (result.length() != degree.size())
+				return "";
+			return result;
+		}
+	}
+
+	// wrong answer
+	public class Solution1 {
+
+		public String alienOrder(String[] words) {
 			Map<Character, Set<Character>> graph = createGraph(words);
 			Stack<Character> stack = new TopologicalSort().sort(graph);
-			return stringResult(stack);
+			return charCount(words) != stack.size() ? "" : stringResult(stack);
+		}
+
+		private int charCount(String[] words) {
+			Set<Character> uniqueChar = new HashSet<>();
+			for (String word : words) {
+				for (char c : word.toCharArray()) {
+					uniqueChar.add(word.charAt(c));
+				}
+			}
+			return uniqueChar.size();
 		}
 
 		private String stringResult(Stack<Character> stack) {
