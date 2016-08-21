e6a06f248cb6eb815ab2ad1a7e9101f7d58c2929
zhou-dong <82224165@qq.com>
2016-08-21 17:15:35 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/WaterAndJugProblem.java
+++ algorithm/src/org/dzhou/practice/medium/WaterAndJugProblem.java
@@ -28,11 +28,11 @@ package org.dzhou.practice.medium;
  *
  *         题意: 给定两个杯子，它们分别能装x和y升水，求它们是否能恰好测量出z升的水。
  * 
+ *         求最大公约数GCD（Greatest Common Divisor)。
+ * 
  *         题目可以表示为如下等式: <br>
  *         mx + ny = z <br>
  *         若m,n>0说明往里面加水，若m,n <0则说明清空
- * 
- *         可以参考 ： http://www.math.tamu.edu/~dallen/hollywood/diehard/diehard.htm
  * 
  *         本题中，若mx + ny = z 有解，则一定有z为GCD(x,y) 的整数倍，否则无解。
  * 
@@ -46,6 +46,7 @@ public class WaterAndJugProblem {
 			return x + y == z || (x + y > z) && z % gcd(x, y) == 0;
 		}
 
+		// Greatest Common Divisor
 		private int gcd(int a, int b) {
 			return b == 0 ? a : gcd(b, a % b);
 		}
