f738cca0df3a67874b3c4708b71e4de178398edd
zhou-dong <82224165@qq.com>
2016-08-26 00:45:34 -0500

    update

--- algorithm/src/org/dzhou/practice/hard/premium/WordPatternII.java
+++ algorithm/src/org/dzhou/practice/hard/premium/WordPatternII.java
@@ -26,49 +26,38 @@ public class WordPatternII {
 
 	public class Solution {
 
-		Map<Character, String> map;
-		Set<String> set;
-		boolean res;
+		Map<Character, String> map = null;
+		Set<String> set = null;
+		boolean res = false;
 
 		public boolean wordPatternMatch(String pattern, String str) {
-			// 和I中一样，Map用来记录字符和字符串的映射关系
 			map = new HashMap<Character, String>();
-			// Set用来记录哪些字符串被映射了，防止多对一映射
 			set = new HashSet<String>();
 			res = false;
-			// 递归回溯
 			helper(pattern, str, 0, 0);
 			return res;
 		}
 
 		public void helper(String pattern, String str, int i, int j) {
-			// 如果pattern匹配完了而且str也正好匹配完了，说明有解
 			if (i == pattern.length() && j == str.length()) {
 				res = true;
 				return;
 			}
-			// 如果某个匹配超界了，则结束递归
 			if (i >= pattern.length() || j >= str.length()) {
 				return;
 			}
 			char c = pattern.charAt(i);
-			// 尝试从当前位置到结尾的所有划分方式
 			for (int cut = j + 1; cut <= str.length(); cut++) {
-				// 拆出一个子串
 				String substr = str.substring(j, cut);
-				// 如果这个子串没有被映射过，而且当前pattern的字符也没有产生过映射
-				// 则新建一对映射，并且继续递归求解
 				if (!set.contains(substr) && !map.containsKey(c)) {
 					map.put(c, substr);
 					set.add(substr);
 					helper(pattern, str, i + 1, cut);
 					map.remove(c);
 					set.remove(substr);
-					// 如果已经有映射了，但是是匹配的，也继续求解
 				} else if (map.containsKey(c) && map.get(c).equals(substr)) {
 					helper(pattern, str, i + 1, cut);
 				}
-				// 否则跳过该子串，尝试下一种拆分
 			}
 		}
 	}
