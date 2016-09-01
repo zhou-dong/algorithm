ac58ab1b31d391737af52e27cf40b237758e9d2a
zhou-dong <82224165@qq.com>
2016-09-01 02:35:33 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/LongestAbsoluteFilePath.java
+++ algorithm/src/org/dzhou/practice/medium/LongestAbsoluteFilePath.java
@@ -9,37 +9,75 @@ import java.util.Stack;
  */
 public class LongestAbsoluteFilePath {
 
-	// "\t": tab
-	// "\n": 回车
-	public class Solution {
-
-		public int lengthLongestPath(String input) {
-			if (input == null || input.length() == 0 || !input.contains("."))
-				return 0;
-
-			String[] list = input.split("\n");
-			Stack<Integer> stack = new Stack<>();
-			int curLength = 0;
-			int maxLength = -1;
-			for (String s : list) {
-				int tabs = 0;
-				while (s.charAt(tabs) == '\t') {
-					tabs++;
-				}
-				while (tabs < stack.size()) {
-					curLength -= stack.pop();
-				}
-				s = s.substring(tabs);
-				if (s.contains(".")) {
-					maxLength = Math.max(maxLength, curLength + s.length());
-				} else {
-					curLength += s.length() + 1;
-					stack.push(s.length() + 1);
-				}
-			}
-			return maxLength;
-		}
-
+	public static void main(String[] args) {
+		String input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+		int i = new LongestAbsoluteFilePath().lengthLongestPath(input);
+		System.out.println(i);
 	}
 
+	// "\t": tab
+	// "\n": 回车
+	public int lengthLongestPath(String input) {
+		if (input == null || input.length() == 0 || !input.contains("."))
+			return 0;
+
+		String[] strs = input.split("\n");
+		Stack<Integer> stack = new Stack<>();
+		int max = 0;
+
+		for (String s : strs) {
+			int tabs = countTab(s);
+			while (tabs < stack.size()) {
+				stack.pop();
+			}
+			int chars = s.length() - tabs;
+			int preLength = stack.isEmpty() ? 0 : stack.peek();
+			if (s.contains(".")) {
+				max = Math.max(max, preLength + chars);
+			} else {
+				stack.push(preLength + chars + 1);
+			}
+		}
+
+		return max;
+	}
+
+	private int countTab(String s) {
+		int count = 0;
+		for (char c : s.toCharArray()) {
+			if (c == '\t') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		return count;
+	}
+
+	int before(String input) {
+
+		if (input == null || input.length() == 0 || !input.contains("."))
+			return 0;
+
+		String[] list = input.split("\n");
+		Stack<Integer> stack = new Stack<>();
+		int current = 0, max = 0;
+
+		for (String s : list) {
+			int tabs = countTab(s);
+			while (tabs < stack.size()) {
+				current -= stack.pop();
+			}
+
+			int chars = s.length() - tabs;
+			if (s.contains(".")) {
+				max = Math.max(max, current + chars);
+			} else {
+				current += chars + 1;
+				stack.push(chars + 1);
+			}
+		}
+		return max;
+
+	}
 }
