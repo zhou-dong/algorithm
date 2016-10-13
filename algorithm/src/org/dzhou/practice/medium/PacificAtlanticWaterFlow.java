164d8d922de91fd75e71dd1cd1bdfe9560643c1b
zhou-dong <82224165@qq.com>
2016-10-13 16:03:17 -0500

    update

--- algorithm/src/org/dzhou/practice/medium/PacificAtlanticWaterFlow.java
+++ algorithm/src/org/dzhou/practice/medium/PacificAtlanticWaterFlow.java
@@ -1,6 +1,26 @@
 package org.dzhou.practice.medium;
 
 /**
+ * 417. Pacific Atlantic Water Flow
+ * 
+ * Difficulty: Medium
+ * 
+ * Given an m x n matrix of non-negative integers representing the height of
+ * each unit cell in a continent, the "Pacific ocean" touches the left and top
+ * edges of the matrix and the "Atlantic ocean" touches the right and bottom
+ * edges.
+ * 
+ * Water can only flow in four directions (up, down, left, or right) from a cell
+ * to another one with height equal or lower.
+ * 
+ * Find the list of grid coordinates where water can flow to both the Pacific
+ * and Atlantic ocean.
+ * 
+ * Note:
+ * 
+ * The order of returned grid coordinates does not matter.
+ * 
+ * Both m and n are less than 150.
  * 
  * @author zhoudong
  *
