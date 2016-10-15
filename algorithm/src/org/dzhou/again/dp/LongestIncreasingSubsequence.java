f4c96f0b6f36577b85354babd320b14594d86a80
zhou-dong <82224165@qq.com>
2016-10-14 23:51:52 -0500

    update

--- algorithm/src/org/dzhou/again/dp/LongestIncreasingSubsequence.java
+++ algorithm/src/org/dzhou/again/dp/LongestIncreasingSubsequence.java
@@ -32,7 +32,13 @@ public class LongestIncreasingSubsequence {
 	private int dpHelper(int[] nums) {
 		int[] dp = new int[nums.length];
 		Arrays.fill(dp, 1);
-
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[i] > nums[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+		}
 		return max(dp);
 	}
 
