a766da485e55382bbd4ff5dab9373a300871077f
zhou-dong <82224165@qq.com>
2016-10-14 21:01:41 -0500

    update

---
+++ algorithm/src/org/dzhou/again/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,28 @@
+package org.dzhou.again.dp;
+
+/**
+ * 300. Longest Increasing Subsequence
+ * 
+ * Difficulty: Medium
+ * 
+ * Contributors: Admin
+ * 
+ * Given an unsorted array of integers, find the length of longest increasing
+ * subsequence.
+ * 
+ * For example, Given [10, 9, 2, 5, 3, 7, 101, 18],
+ * 
+ * The longest increasing subsequence is [2, 3, 7, 101], therefore the length is
+ * 4. Note that there may be more than one LIS combination, it is only necessary
+ * for you to return the length.
+ * 
+ * Your algorithm should run in O(n2) complexity.
+ * 
+ * Follow up: Could you improve it to O(n log n) time complexity?
+ * 
+ * @author zhoudong
+ *
+ */
+public class LongestIncreasingSubsequence {
+
+}
