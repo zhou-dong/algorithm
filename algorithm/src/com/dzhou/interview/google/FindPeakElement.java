741c57377a3bad710d1decb93479e3f6d613a0bf
zhou-dong <82224165@qq.com>
2016-10-18 20:57:24 -0500

    update

---
+++ algorithm/src/com/dzhou/interview/google/FindPeakElement.java
@@ -0,0 +1,67 @@
+package com.dzhou.interview.google;
+
+/**
+ * 162. Find Peak Element
+ * 
+ * Difficulty: Medium
+ * 
+ * A peak element is an element that is greater than its neighbors.
+ * 
+ * Given an input array where num[i] ≠ num[i+1], find a peak element and return
+ * its index.
+ * 
+ * The array may contain multiple peaks, in that case return the index to any
+ * one of the peaks is fine.
+ * 
+ * You may imagine that num[-1] = num[n] = -∞.
+ * 
+ * For example, in array [1, 2, 3, 1], 3 is a peak element and your function
+ * should return the index number 2.
+ * 
+ * Note: Your solution should be in logarithmic complexity.
+ * 
+ * @author zhoudong
+ *
+ */
+public class FindPeakElement {
+
+	class BruteForce_Solution {
+		public int findPeakElement(int[] nums) {
+			if (nums == null || nums.length == 0)
+				return -1;
+			if (nums.length == 1)
+				return 0;
+			for (int i = 0; i < nums.length; i++) {
+				if (isPeak(nums, i)) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		private boolean isPeak(int[] nums, int index) {
+			int left = (index == 0) ? Integer.MIN_VALUE : nums[index - 1];
+			int right = (index == (nums.length - 1)) ? Integer.MIN_VALUE : nums[index + 1];
+			return nums[index] > left && nums[index] > right;
+		}
+	}
+
+	class BinarySearch_Solution {
+		public int findPeakElement(int[] nums) {
+			int left = 0, right = nums.length - 1;
+			while (left <= right) {
+				if (left == right)
+					return left;
+				int mid = (left + right) / 2;
+				if (nums[mid] < nums[mid + 1]) {
+					left = mid + 1;
+				} else {
+					right = mid;
+				}
+			}
+			return left;
+		}
+
+	}
+
+}
