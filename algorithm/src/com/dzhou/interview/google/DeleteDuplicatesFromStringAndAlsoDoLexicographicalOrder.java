3ba33b437ffafb5a9cb8f12d3da9b568fe081202
zhou-dong <82224165@qq.com>
2016-10-20 18:56:51 -0500

    update

--- algorithm/src/com/dzhou/interview/google/DeleteDuplicatesFromStringAndAlsoDoLexicographicalOrder.java
+++ algorithm/src/com/dzhou/interview/google/DeleteDuplicatesFromStringAndAlsoDoLexicographicalOrder.java
@@ -1,5 +1,11 @@
 package com.dzhou.interview.google;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * Given a string which only contains lowercase. You need delete the repeated
  * letters only leave one, and try to make the lexicographical order of new
@@ -19,5 +25,53 @@ package com.dzhou.interview.google;
  *
  */
 public class DeleteDuplicatesFromStringAndAlsoDoLexicographicalOrder {
+
+	public String removeDuplicateLetters(String s) {
+		Map<Character, Integer> charCount = createHashTable(s);
+		Stack<Character> stack = new Stack<>();
+		Set<Character> visited = new HashSet<>();
+		for (char c : s.toCharArray()) {
+			if (visited.contains(c)) {
+				subtractCount(charCount, c);
+				continue;
+			}
+			while (!stack.isEmpty() && stack.peek() > c && charCount.get(stack.peek()) > 0) {
+				visited.remove(stack.peek());
+				stack.pop();
+			}
+			addCharacter(stack, visited, charCount, c);
+		}
+		return readFromStack(stack);
+	}
+
+	private void addCharacter(Stack<Character> stack, Set<Character> visited, Map<Character, Integer> charCount,
+			char c) {
+		stack.push(c);
+		visited.add(c);
+		subtractCount(charCount, c);
+	}
+
+	private String readFromStack(Stack<Character> stack) {
+		StringBuilder result = new StringBuilder();
+		while (!stack.isEmpty())
+			result.append(stack.pop());
+		return result.reverse().toString();
+	}
+
+	private void subtractCount(Map<Character, Integer> map, char key) {
+		map.put(key, map.get(key) - 1);
+	}
+
+	private Map<Character, Integer> createHashTable(String s) {
+		Map<Character, Integer> map = new HashMap<>();
+		for (char c : s.toCharArray()) {
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+		}
+		return map;
+	}
 
 }
