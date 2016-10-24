15dd6c324aba9b49f5c522a390091856fbe22142
zhou-dong <82224165@qq.com>
2016-10-24 05:38:45 -0500

    update

---
+++ algorithm/src/com/dzhou/interview/google/PalindromePartitioning.java
@@ -0,0 +1,35 @@
+package com.dzhou.interview.google;
+
+/**
+ * 131. Palindrome Partitioning
+ * 
+ * Difficulty: Medium
+ * 
+ * Given a string s, partition s such that every substring of the partition is a
+ * palindrome.
+ * 
+ * Return all possible palindrome partitioning of s.
+ * 
+ * For example, given s = "aab",
+ * 
+ * Return
+ * 
+ * [ <br>
+ * --["aa","b"], <br>
+ * --["a","a","b"] <br>
+ * ] <br>
+ * 
+ * @author zhoudong
+ *
+ */
+public class PalindromePartitioning {
+
+	public class Backtracking_Solution {
+
+	}
+
+	public class DynamicProgramming_Solution {
+
+	}
+
+}
