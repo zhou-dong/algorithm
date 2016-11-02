c8927b8afb96cc72258fea91bc0e0111a94fef22
zhou-dong <82224165@qq.com>
2016-11-02 18:52:43 -0500

    update

---
+++ algorithm/src/com/dzhou/interview/google/MaximumXORofTwoNumbersInAnArray.java
@@ -0,0 +1,32 @@
+package com.dzhou.interview.google;
+
+/**
+ * 421. Maximum XOR of Two Numbers in an Array
+ * 
+ * Difficulty: Medium
+ * 
+ * Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 231.
+ * 
+ * Find the maximum result of ai XOR aj, where 0 ≤ i, j < n.
+ * 
+ * Could you do this in O(n) runtime?
+ * 
+ * Example:
+ * 
+ * Input: [3, 10, 5, 25, 2, 8]
+ * 
+ * Output: 28
+ * 
+ * Explanation: The maximum result is 5 ^ 25 = 28.
+ * 
+ * @author zhoudong
+ *
+ */
+public class MaximumXORofTwoNumbersInAnArray {
+
+	public static void main(String[] args) {
+
+		System.out.println(2 ^ 25);
+
+	}
+}
