d6a3fac8b647b661e1ffa91f492c648406aa0a14
zhou-dong <82224165@qq.com>
2016-11-03 16:51:02 -0500

    update

--- algorithm/src/com/dzhou/interview/google/MaximumXORofTwoNumbersInAnArray.java
+++ algorithm/src/com/dzhou/interview/google/MaximumXORofTwoNumbersInAnArray.java
@@ -21,29 +21,78 @@ package com.dzhou.interview.google;
  * 
  * @author zhoudong
  *
- *         a ^ b = c -> <br>
- *         a ^ c = b -> <br>
- *         b ^ c = a
+ *         我不会用hashtable，只会用Trie
+ * 
  */
 public class MaximumXORofTwoNumbersInAnArray {
 
-	public static void main(String[] args) {
+	class TrieNode {
+		TrieNode[] children = new TrieNode[2];
 
-		System.out.println(2 ^ 25);
-		System.out.println(3 ^ 10);
-		System.out.println(3 ^ 5);
-		System.out.println(3 ^ 25);
-		System.out.println(3 ^ 2);
-		System.out.println(3 ^ 8);
+		TrieNode addChild(int child) {
+			if (children[child] == null)
+				children[child] = new TrieNode();
+			return children[child];
+		}
 
-		System.out.println();
-
-		System.out.println(Integer.toBinaryString(3));
-		System.out.println(Integer.toBinaryString(10));
-		System.out.println(Integer.toBinaryString(5));
-		System.out.println(Integer.toBinaryString(25));
-		System.out.println(Integer.toBinaryString(2));
-		System.out.println(Integer.toBinaryString(8));
-
+		boolean contains(int bit) {
+			return children[bit] != null;
+		}
 	}
+
+	class Trie {
+		TrieNode root = new TrieNode();
+
+		Trie(int[] nums) {
+			for (int num : nums) {
+				insert(num);
+			}
+		}
+
+		void insert(int num) {
+			TrieNode current = root;
+			for (int i = 31; i >= 0; i--) {
+				int bit = getBit(num, i);
+				current = current.addChild(bit);
+			}
+		}
+	}
+
+	private int getBit(int num, int index) {
+		int mask = 1 << index;
+		return (num & mask) == 0 ? 0 : 1;
+	}
+
+	public int findMaximumXOR(int[] nums) {
+
+		Trie trie = new Trie(nums);
+		int max = Integer.MIN_VALUE;
+
+		for (int num : nums) {
+			TrieNode node = trie.root;
+			int value = 0;
+			for (int i = 31; i >= 0; i--) {
+				int bit = getBit(num, i);
+				if (bit == 1) {
+					if (node.contains(0)) {
+						value |= 1 << i;
+						node = node.children[0];
+					} else {
+						node = node.children[1];
+					}
+				} else {
+					if (node.contains(1)) {
+						value |= 1 << i;
+						node = node.children[1];
+					} else {
+						node = node.children[0];
+					}
+				}
+				max = Math.max(max, value);
+			}
+		}
+
+		return max;
+	}
+
 }
