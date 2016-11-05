f0c271f5d444b3983ff144a3c4c9bafa20c402be
zhou-dong <82224165@qq.com>
2016-11-05 18:16:58 -0500

    update

--- algorithm/src/com/dzhou/interview/google/GeneralizedAbbreviation.java
+++ algorithm/src/com/dzhou/interview/google/GeneralizedAbbreviation.java
@@ -23,8 +23,19 @@ public class GeneralizedAbbreviation {
 
 	public List<String> generateAbbreviations(String word) {
 		List<String> res = new ArrayList<String>();
-
+		backtrack(res, word, 0, "", 0);
 		return res;
+	}
+
+	private void backtrack(List<String> res, String word, int pos, String cur, int count) {
+		if (pos == word.length()) {
+			if (count > 0)
+				cur += count;
+			res.add(cur);
+		} else {
+			backtrack(res, word, pos + 1, cur, count + 1);
+			backtrack(res, word, pos + 1, cur + (count > 0 ? count : "") + word.charAt(pos), 0);
+		}
 	}
 
 }
