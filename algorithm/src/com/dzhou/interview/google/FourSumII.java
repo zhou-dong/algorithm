bc59a8f2f08cbc52ddb5f6f86a731f8befadebf8
zhou-dong <82224165@qq.com>
2016-11-17 14:15:34 -0600

    update

---
+++ algorithm/src/com/dzhou/interview/google/FourSumII.java
@@ -0,0 +1,35 @@
+package com.dzhou.interview.google;
+
+/**
+ * 454. 4Sum II
+ * 
+ * Difficulty: Medium
+ * 
+ * Given four lists A, B, C, D of integer values, compute how many tuples (i, j,
+ * k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+ * 
+ * To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤
+ * N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is
+ * guaranteed to be at most 231 - 1.
+ * 
+ * Example:
+ * 
+ * Input: <br>
+ * A = [ 1, 2] <br>
+ * B = [-2,-1] <br>
+ * C = [-1, 2] <br>
+ * D = [ 0, 2] <br>
+ * 
+ * Output: 2
+ * 
+ * Explanation: <br>
+ * The two tuples are: <br>
+ * 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0 <br>
+ * 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0 <br>
+ * 
+ * @author zhoudong
+ *
+ */
+public class FourSumII {
+
+}
