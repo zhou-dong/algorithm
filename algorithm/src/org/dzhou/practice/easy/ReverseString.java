a1bdfa028fb3a09d0e909d391e1ed413af7910a8
zhou-dong <82224165@qq.com>
2016-11-27 16:26:56 -0600

    update reverse string

--- algorithm/src/org/dzhou/practice/easy/ReverseString.java
+++ algorithm/src/org/dzhou/practice/easy/ReverseString.java
@@ -16,123 +16,153 @@ import java.util.Stack;
 public class ReverseString {
 
 	// method one
-	public static String reverseString(String s) {
-		char[] result = new char[s.length()];
-		for (int i = 0; i < result.length; i++)
-			result[i] = s.charAt(result.length - 1 - i);
-		return new String(result);
+	class CopyFromBackToFrontSolution {
+		public String reverseString(String s) {
+			char[] result = new char[s.length()];
+			for (int i = 0; i < result.length; i++)
+				result[i] = s.charAt(result.length - 1 - i);
+			return new String(result);
+		}
 	}
 
 	// method two
-	public static String reverseStringFromBothSide(String s) {
-		char[] result = s.toCharArray();
-		for (int left = 0, right = result.length - 1; left < right; left++, right--)
-			swap(result, left, right);
-		return new String(result);
-	}
+	class TwoPointerSolution {
+		public String reverseString(String s) {
+			char[] result = s.toCharArray();
+			for (int left = 0, right = result.length - 1; left < right; left++, right--)
+				swap(result, left, right);
+			return new String(result);
+		}
 
-	private static void swap(char[] chars, int x, int y) {
-		char temp = chars[x];
-		chars[x] = chars[y];
-		chars[y] = temp;
+		private void swap(char[] chars, int x, int y) {
+			char temp = chars[x];
+			chars[x] = chars[y];
+			chars[y] = temp;
+		}
 	}
 
 	// method three
-	public static String reverseStringByStack(String s) {
-		Stack<Character> stack = new Stack<>();
-		for (char c : s.toCharArray())
-			stack.add(c);
-		char[] result = new char[stack.size()];
-		for (int i = 0; i < result.length; i++)
-			result[i] = stack.pop();
-		return new String(result);
-	}
-
-	// method four
-	public static String reverseStringByRecursionFrontEnd(String s) {
-		char[] input = s.toCharArray();
-		char[] result = new char[input.length];
-		reverseStringByRecursionFrontEnd(input, result, 0);
-		return new String(result);
-	}
-
-	private static void reverseStringByRecursionFrontEnd(char[] input, char[] result, int index) {
-		if (index < input.length) {
-			result[index] = input[input.length - 1 - index];
-			reverseStringByRecursionFrontEnd(input, result, index + 1);
+	class StackSolution {
+		public String reverseString(String s) {
+			Stack<Character> stack = new Stack<>();
+			for (char c : s.toCharArray())
+				stack.add(c);
+			char[] result = new char[stack.size()];
+			for (int i = 0; i < result.length; i++)
+				result[i] = stack.pop();
+			return new String(result);
 		}
 	}
 
-	// method five
-	public static String reverseStringByRecursionEndFront(String s) {
-		char[] input = s.toCharArray();
-		char[] result = new char[input.length];
-		reverseStringByRecursionEndFront(input, result, 0);
-		return new String(result);
-	}
+	class RecursionFrontToEndSolution {
 
-	private static void reverseStringByRecursionEndFront(char[] input, char[] result, int index) {
-		if (index < input.length) {
-			reverseStringByRecursionEndFront(input, result, index + 1);
-			result[result.length - 1 - index] = input[index];
+		public String reverseString(String s) {
+			char[] result = new char[s.length()];
+			dfs(s, result, 0);
+			return new String(result);
 		}
+
+		private void dfs(String s, char[] result, int index) {
+			if (index < result.length) {
+				result[index] = s.charAt(s.length() - 1 - index);
+				dfs(s, result, index + 1);
+			}
+		}
+
 	}
 
-	// method six
-	public static String reverseStringByRecursionFromHalf(String s) {
-		char[] input = s.toCharArray();
-		reverseStringByRecursionFromHalf(input, 0, input.length / 2);
-		return new String(input);
+	class RecursionEndToFrontSolution {
+
+		public String reverseString(String s) {
+			char[] result = new char[s.length()];
+			dfs(s, result, 0);
+			return new String(result);
+		}
+
+		private void dfs(String s, char[] result, int index) {
+			if (index < result.length) {
+				dfs(s, result, index + 1);
+				result[result.length - 1 - index] = s.charAt(index);
+			}
+		}
+
 	}
 
-	private static void reverseStringByRecursionFromHalf(char[] input, int index, int half) {
-		if (index >= input.length)
-			return;
-		reverseStringByRecursionFromHalf(input, index + 1, half);
-		if (index > half)
-			swap(input, index, input.length - index - 1);
+	class RecursionFromMiddleSolution {
+
+		public String reverseString(String s) {
+			char[] result = s.toCharArray();
+			dfs(result, result.length / 2);
+			return new String(result);
+		}
+
+		private void dfs(char[] result, int index) {
+			if (index < result.length) {
+				dfs(result, index + 1);
+				swap(result, index, result.length - index - 1);
+			}
+		}
+
+		private void swap(char[] chars, int x, int y) {
+			char temp = chars[x];
+			chars[x] = chars[y];
+			chars[y] = temp;
+		}
+
 	}
 
 	// method seven
-	public static String reverseStringByLinkedList(String s) {
-		LinkedList<Character> list = new LinkedList<>();
-		for (char c : s.toCharArray())
-			list.add(c);
-		char[] result = new char[list.size()];
-		for (int i = 0; i < result.length; i++)
-			result[i] = list.pollLast();
-		return new String(result);
+	class LinkedListAsStackSolution {
+		public String reverseString(String s) {
+			LinkedList<Character> list = new LinkedList<>();
+			for (char c : s.toCharArray())
+				list.add(c);
+			char[] result = new char[list.size()];
+			for (int i = 0; i < result.length; i++)
+				result[i] = list.pollLast();
+			return new String(result);
+		}
 	}
 
 	// method eight
-	public static String reverseStringByLinkedList2(String s) {
-		LinkedList<Character> list = new LinkedList<>();
-		for (char c : s.toCharArray())
-			list.add(c);
-		Collections.reverse(list);
-		char[] result = new char[list.size()];
-		for (int i = 0; i < result.length; i++)
-			result[i] = list.get(i);
-		return new String(result);
+	class LinkedListSolution {
+		public String reverseString(String s) {
+			LinkedList<Character> list = new LinkedList<>();
+			for (char c : s.toCharArray())
+				list.add(c);
+			Collections.reverse(list);
+			char[] result = new char[list.size()];
+			for (int i = 0; i < result.length; i++)
+				result[i] = list.get(i);
+			return new String(result);
+		}
 	}
 
-	// method nine
-	public static String reverseStringByStringBuilder(String s) {
-		return new StringBuilder(s).reverse().toString();
+	class StringBuilderSolution {
+		public String reverseString(String s) {
+			return new StringBuilder(s).reverse().toString();
+		}
 	}
 
-	// method ten
-	public static String reverseStringByRecursionFromHalf2(String s) {
-		char[] input = s.toCharArray();
-		reverseStringByRecursionFromHalf2(input, 0, input.length / 2);
-		return new String(input);
-	}
+	class RecursionAndSwapSolution {
 
-	private static void reverseStringByRecursionFromHalf2(char[] input, int index, int half) {
-		if (index >= half)
-			return;
-		swap(input, index, input.length - index - 1);
-		reverseStringByRecursionFromHalf2(input, index + 1, half);
-	}
+		public String reverseString(String s) {
+			char[] input = s.toCharArray();
+			dfs(input, 0);
+			return new String(input);
+		}
 
+		private void dfs(char[] input, int index) {
+			if (index < input.length / 2) {
+				swap(input, index, input.length - index - 1);
+				dfs(input, index + 1);
+			}
+		}
+
+		private void swap(char[] chars, int x, int y) {
+			char temp = chars[x];
+			chars[x] = chars[y];
+			chars[y] = temp;
+		}
+	}
 }
