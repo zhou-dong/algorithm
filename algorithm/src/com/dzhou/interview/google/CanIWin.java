f45eac3178301e6b13cf7dfa3feb5e3b049d0945
zhou-dong <82224165@qq.com>
2016-12-08 12:43:40 -0600

    update

--- algorithm/src/com/dzhou/interview/google/CanIWin.java
+++ algorithm/src/com/dzhou/interview/google/CanIWin.java
@@ -1,5 +1,8 @@
 package com.dzhou.interview.google;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 464. Can I Win
  * 
@@ -43,9 +46,54 @@ package com.dzhou.interview.google;
  */
 public class CanIWin {
 
-	public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+	Map<Integer, Boolean> map;
+	boolean[] used;
 
-		return false;
+	public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+		int sum = (1 + maxChoosableInteger) * maxChoosableInteger / 2;
+		if (sum < desiredTotal)
+			return false;
+		if (desiredTotal <= 0)
+			return true;
+
+		map = new HashMap<>();
+		used = new boolean[maxChoosableInteger + 1];
+		return helper(desiredTotal);
+	}
+
+	public boolean helper(int desiredTotal) {
+		if (desiredTotal <= 0)
+			return false;
+		int key = format(used);
+		if (!map.containsKey(key)) {
+			// try every unchosen number as next step
+			for (int i = 1; i < used.length; i++) {
+				if (!used[i]) {
+					used[i] = true;
+					// check whether this lead to a win (i.e. the other player
+					// lose)
+					if (!helper(desiredTotal - i)) {
+						map.put(key, true);
+						used[i] = false;
+						return true;
+					}
+					used[i] = false;
+				}
+			}
+			map.put(key, false);
+		}
+		return map.get(key);
+	}
+
+	// transfer boolean[] to an Integer
+	public int format(boolean[] used) {
+		int num = 0;
+		for (boolean b : used) {
+			num <<= 1;
+			if (b)
+				num |= 1;
+		}
+		return num;
 	}
 
 }
