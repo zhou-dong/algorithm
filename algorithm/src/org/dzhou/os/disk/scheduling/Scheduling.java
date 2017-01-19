e4eb1d860096f7d4aa13067444998daa478766bf
zhou-dong <82224165@qq.com>
2017-01-18 22:07:55 -0600

    update

--- algorithm/src/org/dzhou/os/disk/scheduling/Scheduling.java
+++ algorithm/src/org/dzhou/os/disk/scheduling/Scheduling.java
@@ -4,6 +4,71 @@ import java.util.Arrays;
 
 public interface Scheduling {
 
+	public static class ListNode {
+		ListNode prev;
+		ListNode next;
+		int value;
+
+		ListNode(int value) {
+			this.value = value;
+		}
+	}
+
+	public static class List {
+		ListNode head;
+		ListNode tail;
+		int size;
+
+		List() {
+			this.size = 0;
+			this.head = new ListNode(-1);
+			this.tail = new ListNode(-1);
+			head.next = tail;
+			tail.prev = head;
+		}
+
+		List(int[] nums) {
+			this();
+			for (int num : nums) {
+				this.append(num);
+			}
+		}
+
+		ListNode append(int value) {
+			ListNode node = new ListNode(value);
+			node.next = tail;
+			node.prev = tail.prev;
+			tail.prev.next = node;
+			tail.prev = node;
+			this.size++;
+			return node;
+		}
+
+		void remove(ListNode node) {
+			node.prev.next = node.next;
+			node.next.prev = node.prev;
+			this.size--;
+		}
+
+		ListNode near(ListNode node) {
+			if (this.size == 0)
+				return null;
+			if (this.size == 1)
+				return node;
+			if (node.prev == head)
+				return node.next;
+			if (node.next == tail)
+				return node.prev;
+			return near(node, node.prev, node.next);
+		}
+
+		ListNode near(ListNode node, ListNode prev, ListNode next) {
+			int prevInterval = Math.abs(node.value - prev.value);
+			int nextInterval = Math.abs(node.value - next.value);
+			return prevInterval < nextInterval ? prev : next;
+		}
+	}
+
 	public abstract int execute(int[] range, int[] cylinders, int header, int previous);
 
 	public static class FCFS implements Scheduling {
@@ -22,68 +87,10 @@ public interface Scheduling {
 
 	public static class SSTF implements Scheduling {
 
-		private class ListNode {
-			ListNode prev;
-			ListNode next;
-			int value;
-
-			ListNode(int value) {
-				this.value = value;
-			}
-		}
-
-		private class List {
-			ListNode head;
-			ListNode tail;
-			int size;
-
-			List() {
-				this.size = 0;
-				this.head = new ListNode(-1);
-				this.tail = new ListNode(-1);
-				head.next = tail;
-				tail.prev = head;
-			}
-
-			ListNode append(int value) {
-				ListNode node = new ListNode(value);
-				node.next = tail;
-				node.prev = tail.prev;
-				tail.prev.next = node;
-				tail.prev = node;
-				this.size++;
-				return node;
-			}
-
-			void remove(ListNode node) {
-				node.prev.next = node.next;
-				node.next.prev = node.prev;
-				this.size--;
-			}
-
-			ListNode near(ListNode node) {
-				if (this.size == 0)
-					return null;
-				if (this.size == 1)
-					return node;
-				if (node.prev == head)
-					return node.next;
-				if (node.next == tail)
-					return node.prev;
-				return near(node, node.prev, node.next);
-			}
-
-			ListNode near(ListNode node, ListNode prev, ListNode next) {
-				int prevInterval = Math.abs(node.value - prev.value);
-				int nextInterval = Math.abs(node.value - next.value);
-				return prevInterval < nextInterval ? prev : next;
-			}
-		}
-
 		@Override
 		public int execute(int[] range, int[] cylinders, int header, int previous) {
 			Arrays.sort(cylinders);
-			List list = createList(cylinders);
+			List list = new List(cylinders);
 			ListNode current = startNode(list, header);
 			int total = 0, prev = header;
 			while (list.size > 0) {
@@ -94,14 +101,6 @@ public interface Scheduling {
 				current = next;
 			}
 			return total;
-		}
-
-		private List createList(int[] cylinders) {
-			List list = new List();
-			for (int i = 0; i < cylinders.length; i++) {
-				list.append(cylinders[i]);
-			}
-			return list;
 		}
 
 		private ListNode startNode(List list, int header) {
@@ -121,7 +120,13 @@ public interface Scheduling {
 
 	}
 
-	public static class SCAN {
+	public static class SCAN implements Scheduling {
+
+		@Override
+		public int execute(int[] range, int[] cylinders, int header, int previous) {
+
+			return 0;
+		}
 
 	}
 
