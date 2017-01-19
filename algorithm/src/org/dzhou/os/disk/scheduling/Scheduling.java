0296a3b6e7423733e71c908b81405076be261e9e
zhou-dong <82224165@qq.com>
2017-01-18 19:07:02 -0600

    update

---
+++ algorithm/src/org/dzhou/os/disk/scheduling/Scheduling.java
@@ -0,0 +1,5 @@
+package org.dzhou.os.disk.scheduling;
+
+public class Scheduling {
+
+}
---
+++ algorithm/src/org/dzhou/os/disk/scheduling/test/TestScheduling.java
@@ -0,0 +1,6 @@
+package org.dzhou.os.disk.scheduling.test;
+
+public class TestScheduling {
+
+	int[] cylinders = { 86, 1470, 913, 1779, 918, 1509, 1022, 1750, 130 };
+}
