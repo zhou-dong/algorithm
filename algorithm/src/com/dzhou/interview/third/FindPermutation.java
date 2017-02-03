32b1b9c4811bffc1131aa6383cef639b09c7b2d4
zhou-dong <82224165@qq.com>
2017-02-02 23:50:39 -0600

    find permutation

--- algorithm/src/com/dzhou/interview/third/FindPermutation.java
+++ algorithm/src/com/dzhou/interview/third/FindPermutation.java
@@ -37,5 +37,28 @@ package com.dzhou.interview.third;
  *
  */
 public class FindPermutation {
+	public int[] findPermutation(String s) {
+		int n = s.length(), arr[] = new int[n + 1];
+		for (int i = 0; i <= n; i++)
+			arr[i] = i + 1; // sorted
+		for (int h = 0; h < n; h++) {
+			if (s.charAt(h) == 'D') {
+				int l = h;
+				while (h < n && s.charAt(h) == 'D')
+					h++;
+				reverse(arr, l, h);
+			}
+		}
+		return arr;
+	}
 
+	void reverse(int[] arr, int l, int h) {
+		while (l < h) {
+			arr[l] ^= arr[h];
+			arr[h] ^= arr[l];
+			arr[l] ^= arr[h];
+			l++;
+			h--;
+		}
+	}
 }
