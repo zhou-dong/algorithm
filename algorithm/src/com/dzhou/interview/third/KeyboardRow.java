857f96b0359725fa841b31225b77adbf4180aeaa
zhou-dong <82224165@qq.com>
2017-02-08 22:48:32 -0600

    update

--- algorithm/src/com/dzhou/interview/third/KeyboardRow.java
+++ algorithm/src/com/dzhou/interview/third/KeyboardRow.java
@@ -1,5 +1,10 @@
 package com.dzhou.interview.third;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 500. Keyboard Row
  * 
@@ -23,7 +28,37 @@ public class KeyboardRow {
 
 	public String[] findWords(String[] words) {
 
-		return null;
+		String[] strs = { "QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM" };
+		Map<Character, Integer> map = new HashMap<>();
+		for (int i = 0; i < strs.length; i++) {
+			for (char ch : strs[i].toCharArray()) {
+				map.put(ch, i);
+			}
+		}
+
+		List<String> result = new LinkedList<>();
+
+		for (String word : words) {
+			if (word == null || word.length() == 0) {
+				continue;
+			}
+			char[] chars = word.toUpperCase().toCharArray();
+			int index = map.getOrDefault(chars[0], -1);
+			if (index == -1)
+				continue;
+			boolean flag = true;
+			for (char ch : chars) {
+				if (map.getOrDefault(ch, -1) != index) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) {
+				result.add(word);
+			}
+		}
+
+		return result.toArray(new String[0]);
 	}
 
 }
