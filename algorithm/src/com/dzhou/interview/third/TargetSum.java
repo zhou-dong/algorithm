71a60e1fe1ee039ed888bdf0a3e03e0fc906c426
zhou-dong <82224165@qq.com>
2017-02-17 15:22:19 -0600

    update

--- algorithm/src/com/dzhou/interview/third/TargetSum.java
+++ algorithm/src/com/dzhou/interview/third/TargetSum.java
@@ -16,4 +16,33 @@ package com.dzhou.interview.third;
  */
 public class TargetSum {
 
+	int result = 0;
+
+	public int findTargetSumWays(int[] nums, int S) {
+		if (nums == null || nums.length == 0)
+			return result;
+
+		int n = nums.length;
+		int[] sums = new int[n];
+		sums[n - 1] = nums[n - 1];
+		for (int i = n - 2; i >= 0; i--)
+			sums[i] = sums[i + 1] + nums[i];
+
+		helper(nums, sums, S, 0);
+		return result;
+	}
+
+	public void helper(int[] nums, int[] sums, int target, int pos) {
+		if (pos == nums.length) {
+			if (target == 0)
+				result++;
+			return;
+		}
+
+		if (sums[pos] < Math.abs(target))
+			return;
+
+		helper(nums, sums, target + nums[pos], pos + 1);
+		helper(nums, sums, target - nums[pos], pos + 1);
+	}
 }
