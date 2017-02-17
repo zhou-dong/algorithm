dfc765fd5e56083dfba1d21518a6ff819bd7a815
zhou-dong <82224165@qq.com>
2017-02-16 23:35:57 -0600

    update

---
+++ algorithm/src/com/dzhou/interview/third/TargetSum.java
@@ -0,0 +1,19 @@
+package com.dzhou.interview.third;
+
+/**
+ * 494. Target Sum
+ * 
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a target,
+ * S. Now you have 2 symbols + and -. For each integer, you should choose one
+ * from + and - as its new symbol.
+ * 
+ * Find out how many ways to assign symbols to make sum of integers equal to
+ * target S.
+ * 
+ * 
+ * @author zhoudong
+ *
+ */
+public class TargetSum {
+
+}
